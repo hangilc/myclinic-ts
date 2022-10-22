f860173b1d4df3cbd9675bec9d589b3e9aef174d
hangilc <hangil@chang.jp>
2022-10-22 12:34:55 +0900

    added nter xp

--- src/practice/exam/record/conduct/EnterInjectWidget.svelte
+++ src/practice/exam/record/conduct/EnterInjectWidget.svelte
@@ -9,11 +9,15 @@
     widget.open();
   }
 
+  async function doEnter() {
+    
+  }
+
 </script>
 
 <Widget title="注射処置入力" let:close={close} bind:this={widget}>
   <svelte:fragment slot="commands">
-    <button>入力</button>
+    <button on:click={doEnter}>入力</button>
     <button on:click={close}>キャンセル</button>
   </svelte:fragment>
 </Widget>
--- src/practice/exam/record/conduct/EnterXpWidget.svelte
+++ src/practice/exam/record/conduct/EnterXpWidget.svelte
@@ -1,27 +1,44 @@
 <script lang="ts">
-  import type { VisitEx } from "@/lib/model";
+  import { ConductKind, type VisitEx } from "@/lib/model";
   import Widget from "@/lib/Widget.svelte"
+  import { enter } from "../shinryou//helper"
 
-  let widget: Widget;
   export let visit: VisitEx;
+  let widget: Widget;
+  let label: string;
+  let film: string;
 
   export function open(): void {
     widget.open();
   }
 
+  async function doEnter(close: () => void) {
+    const c = {
+      kind: ConductKind.Gazou,
+      labelOption: label,
+      shinryou: ["単純撮影", "単純撮影診断"],
+      kizai: [{
+        code: film,
+        amount: 1
+      }]
+    };
+    await enter(visit, [], [c]);
+    close();
+  }
+
 </script>
 
 <Widget title="Ｘ線検査入力" let:close={close} bind:this={widget}>
-  <select>
+  <select bind:value={label}>
     <option selected>胸部単純Ｘ線</option>
     <option>腹部単純Ｘ線</option>
   </select>
-  <select>
+  <select bind:value={film}>
     <option selected>大角</option>
     <option>四ツ切</option>
   </select>
   <svelte:fragment slot="commands">
-    <button>入力</button>
+    <button on:click={doEnter(close)}>入力</button>
     <button on:click={close}>キャンセル</button>
   </svelte:fragment>
 </Widget>
