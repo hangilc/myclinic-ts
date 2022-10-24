ed602cdf9a52846176a6e21dd6e4c8850ae7e81c
hangilc <hangil@chang.jp>
2022-10-24 15:59:31 +0900

    added edit payment ui

--- src/practice/exam/record/payment/Edit.svelte
+++ src/practice/exam/record/payment/Edit.svelte
@@ -1,10 +1,42 @@
 <script lang="ts">
   import type { VisitEx } from "@/lib/model";
-    import RightBox from "../../RightBox.svelte";
+  import RightBox from "../../RightBox.svelte";
 
   export let visit: VisitEx;
+  export let onClose: () => void;
+
+  export function open(): void {
+    show = true;
+  }
+
+  function close(){
+    show = false;
+    onClose();
+  }
+  let show = false;
 </script>
 
+{#if show}
+<!-- svelte-ignore a11y-invalid-attribute -->
 <RightBox title="請求額の変更">
-
+  <div class="commands">
+    <a href="javascript:void(0)">未収に</a>
+    <a href="javascript:void(0)">領収書PDF</a>
+    <button>入力</button>
+    <button on:click={close}>キャンセル</button>
+  </div>
 </RightBox>
+{/if}
+
+<style>
+  .commands {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+  }
+
+  .commands :global(a),
+  .commands :global(button) {
+    margin-left: 4px;
+  }
+</style>
--- src/practice/exam/record/payment/Payment.svelte
+++ src/practice/exam/record/payment/Payment.svelte
@@ -1,30 +1,40 @@
 <script lang="ts">
   import type { VisitEx } from "@/lib/model";
-    import PaymentStatus from "./PaymentStatus.svelte";
+  import Edit from "./Edit.svelte";
+  import PaymentStatus from "./PaymentStatus.svelte";
 
   export let visit: VisitEx;
   let mode = "disp";
+  let edit: Edit;
 
   function paymentRep(visit: VisitEx): string {
     const charge = visit.chargeOption;
-    if( charge == null ){
-      return "（未請求）"
+    if (charge == null) {
+      return "（未請求）";
     } else {
       return `請求額：${charge.charge.toLocaleString()}円`;
     }
   }
 
   function doDispClick(): void {
-    
+    if (visit.chargeOption != null) {
+      mode = "edit";
+      edit.open();
+    }
   }
 </script>
 
 {#if mode === "disp"}
-<div class="disp" class:has-charge={visit.chargeOption != null} 
-    on:click={doDispClick}>
-  {paymentRep(visit)} <PaymentStatus {visit} />
-</div>
+  <div
+    class="disp"
+    class:has-charge={visit.chargeOption != null}
+    on:click={doDispClick}
+  >
+    {paymentRep(visit)}
+    <PaymentStatus {visit} />
+  </div>
 {/if}
+<Edit onClose={() => (mode = "disp")} {visit} bind:this={edit} />
 
 <style>
   .disp.has-charge {
