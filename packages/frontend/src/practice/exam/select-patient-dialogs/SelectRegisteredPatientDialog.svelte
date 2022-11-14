36a9497afed8bffcbb04e8cd5fe1afe774584155
hangilc <hangil@chang.jp>
2022-11-14 14:17:22 +0900

    fixed select from registered patients

--- packages/frontend/src/practice/exam/select-patient-dialogs/SelectRegisteredPatientDialog.svelte
+++ packages/frontend/src/practice/exam/select-patient-dialogs/SelectRegisteredPatientDialog.svelte
@@ -3,44 +3,54 @@
   import SelectItem from "../../../lib/SelectItem.svelte";
   import { writable, type Writable } from "svelte/store";
   import api from "../../../lib/api";
-  import type * as m from "myclinic-model";
+  import {
+    WqueueState,
+    type Patient,
+    type Visit,
+    type Wqueue,
+  } from "myclinic-model";
 
-  export let onEnter: (patient: m.Patient, visitId: number | null) => void;
+  let entries: [Wqueue, Visit, Patient][] = [];
+
+  export let onEnter: (patient: Patient, visitId: number | null) => void;
   let dialog: Dialog;
-  export function open(): void {
+
+  export async function open() {
+    const list = await api.listWqueueFull();
+    entries = list.filter((d) => {
+      const state = d[0].waitState;
+      return state == WqueueState.WaitExam || state == WqueueState.WaitReExam;
+    });
     dialog.open();
   }
 
-  let selected: Writable<[m.Wqueue, m.Visit, m.Patient] | null> = writable(null);
+  let selected: Writable<[Wqueue, Visit, Patient] | null> =
+    writable(null);
 
   function enter(close: () => void) {
-    if( $selected ){
+    if ($selected) {
       const [_, visit, patient] = $selected;
       onEnter(patient, visit.visitId);
       close();
     }
   }
-
 </script>
 
-<Dialog let:close={close} bind:this={dialog}>
+<Dialog let:close bind:this={dialog}>
   <span slot="title" class="title">受付患者選択</span>
-  {#await api.listWqueueFull()}
-    <div>Loading...</div>
-  {:then dataList}
-    <div class="select">
-      {#each dataList as data}
-        <SelectItem data={data} selected={selected}>
-          {@const [_wq, _visit, patient] = data}
-          <span>{patient.lastName}{patient.firstName}</span>
-        </SelectItem>
-      {/each}
-    </div>
-  {:catch}
-    <div style="color: red">Error</div>
-  {/await}
+
+  <div class="select">
+    {#each entries as data}
+      <SelectItem {data} {selected}>
+        {@const [_wq, _visit, patient] = data}
+        <span>{patient.lastName}{patient.firstName}</span>
+      </SelectItem>
+    {/each}
+  </div>
   <svelte:fragment slot="commands">
-    <button on:click={() => enter(close)} disabled={$selected == null}>選択</button>
+    <button on:click={() => enter(close)} disabled={$selected == null}
+      >選択</button
+    >
     <button on:click={() => close()}>キャンセル</button>
   </svelte:fragment>
 </Dialog>
