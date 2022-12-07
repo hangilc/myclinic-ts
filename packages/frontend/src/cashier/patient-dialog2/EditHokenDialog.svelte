73e2a4cb855a6cd7e6028c25e56ca73cf1f41796
hangilc <hangil@chang.jp>
2022-12-07 09:51:16 +0900

    temporary saving (edit hoken)

--- packages/frontend/src/cashier/patient-dialog2/EditHokenDialog.svelte
+++ packages/frontend/src/cashier/patient-dialog2/EditHokenDialog.svelte
@@ -4,10 +4,14 @@
   import EditKouhiDialog from "./edit/EditKouhiDialog.svelte";
   import { Hoken } from "./hoken";
   import type { PatientData } from "./patient-data";
+  import SurfaceModal from "@/lib/SurfaceModal.svelte";
 
   export let data: PatientData;
-  export let hoken: Hoken | undefined;
+  export let hoken: Hoken;
   export let destroy: () => void;
+  let errors: string[] = [];
+
+  let title: string = `${hoken.name}編集`;
 
   let comp =
     hoken === undefined
@@ -19,6 +23,34 @@
           (_) => undefined,
           (_) => EditKouhiDialog
         );
+
+  function exit(): void {
+    destroy();
+    data.exit();
+  }
+
+  function close(): void {
+    destroy();
+    data.goback();
+  }
+
+  function doEnter() {
+
+  }
 </script>
 
-<svelte:component this={comp} {hoken} {data} {destroy} />
+<SurfaceModal destroy={exit} {title}>
+  {#if errors.length > 0}
+    <div class="error">
+      {#each errors as e}
+        <div>{e}</div>
+      {/each}
+    </div>
+  {/if}
+  <div class="commands">
+    <button on:click={doEnter}>入力</button>
+    <button on:click={close}>キャンセル</button>
+  </div>
+</SurfaceModal>
+
+<!-- <svelte:component this={comp} {hoken} {data} {destroy} /> -->
