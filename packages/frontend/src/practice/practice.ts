87683b0d133222ab7a182830faa0ec5d52621745
hangilc <hangil@chang.jp>
2022-12-19 08:39:04 +0900

    disabled browser ctrl-P (practice)

--- packages/frontend/src/practice/practice.ts
+++ packages/frontend/src/practice/practice.ts
@@ -2,6 +2,13 @@ import Practice from "./Practice.svelte"
 import { initAppEvents } from "@/app-events"
 
 initAppEvents();
+if( window ){
+  window.addEventListener("keydown", (e: KeyboardEvent) => {
+    if( (e.key === "p" || e.key === "P") && e.ctrlKey ){
+      e.preventDefault();
+    }
+  });
+}
 
 const app = new Practice({
   target: document.getElementById('app') as HTMLElement
