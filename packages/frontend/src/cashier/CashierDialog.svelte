43061749ed322b464f046100608e89a7bfb315f5
hangilc <hangil@chang.jp>
2022-12-19 13:51:04 +0900

    added charge dialog ui

--- packages/frontend/src/cashier/CashierDialog.svelte
+++ packages/frontend/src/cashier/CashierDialog.svelte
@@ -1,25 +1,56 @@
 <script lang="ts">
   import SurfaceModal from "@/lib/SurfaceModal.svelte";
-  import type { Charge, Meisai, Patient, Visit } from "myclinic-model";
+  import type {
+    Meisai,
+    Charge,
+    Patient,
+    Visit,
+  } from "myclinic-model";
 
   export let data: [Meisai, Visit, Patient, Charge];
   export let destroy: () => void;
-
-  function getPatient(): Patient {
-    return data[2];
-  }
 
   function patientLine(data: [Meisai, Visit, Patient, Charge]): string {
     const p = data[2];
     return `(${p.patientId}) ${p.fullName()} ${p.fullYomi()}`;
   }
+
+  function getMeisai(data: [Meisai, Visit, Patient, Charge]): Meisai {
+    return data[0];
+  }
+
+  function getCharge(data: [Meisai, Visit, Patient, Charge]): Charge {
+    return data[3];
+  }
 </script>
 
 <SurfaceModal {destroy} title="会計">
   <div class="body">
-    <div>
-      ({patientLine(data)})
+    <div class="patient">
+      {patientLine(data)})
     </div>
+    <div class="detail">
+      <div class="col1" />
+      <div class="col2" />
+      {#each getMeisai(data).items as item}
+        <div class="item-row">
+          <div class="item-cell1 header">{item.section.label}</div>
+          <div class="item-cell2" />
+        </div>
+        {#each item.entries as entry}
+          <div class="item-row">
+            <div class="item-cell1">{entry.label}</div>
+            <div class="item-cell2">
+              {entry.tanka.toLocaleString()}x{entry.count}={entry.totalTen.toLocaleString()}
+            </div>
+          </div>
+        {/each}
+      {/each}
+    </div>
+  </div>
+  <div class="summary">
+    <div>総点：{getMeisai(data).totalTen.toLocaleString()}点、、負担割：{getMeisai(data).futanWari}割</div>
+    <div class="charge">請求額：{getCharge(data).charge.toLocaleString()}円</div>
   </div>
   <div class="commands">
     <button>領収書印刷</button>
@@ -33,10 +64,55 @@
     width: 360px;
   }
 
+  .patient {
+    font-weight: bold;
+  }
+
+  .detail {
+    display: table;
+    border-top: 1px solid black;
+    border-bottom: 1px solid black;
+    margin: 10px 0;
+    padding: 10px 0;
+  }
+
+  .detail .col1 {
+    display: table-column;
+    width: auto;
+  }
+
+  .detail .col2 {
+    display: table-column;
+    width: 6rem;
+  }
+
+  .detail .item-row {
+    display: table-row;
+  }
+
+  .detail .item-cell1,
+  .detail .item-cell2 {
+    display: table-cell;
+  }
+
+  .detail .item-cell2 {
+    text-align: right;
+  }
+
+  .header {
+    font-weight: bold;
+  }
+
+  .charge {
+    color: blue;
+    font-weight: bold;
+  }
+
   .commands {
     display: flex;
     justify-content: right;
     align-items: center;
+    margin-top: 10px;
   }
 
   .commands * + * {
