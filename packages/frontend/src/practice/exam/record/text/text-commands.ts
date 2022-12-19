92466071d3bc4fed4c58a3cb805a428e9d15477c
hangilc <hangil@chang.jp>
2022-12-19 09:27:11 +0900

    added to text commands

--- packages/frontend/src/practice/exam/record/text/text-commands.ts
+++ packages/frontend/src/practice/exam/record/text/text-commands.ts
@@ -7,7 +7,8 @@ export class TextCommand {
 
 export function listTextCommands(): TextCommand[] {
   return [
-    new TextCommand("処方", "院外処方\nＲｐ）\n"),
+    new TextCommand("院外処方", "院外処方\nＲｐ）\n"),
+    new TextCommand("0410対応", "@0410対応＋\n"),
     new TextCommand("インフルエンザ", "インフルエンザ予防接種"),
     new TextCommand("コロナワクチン", "コロナワクチン５回目接種, BA4,BA5"),
   ];
