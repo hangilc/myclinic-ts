dc07a4efbc8fda95bcded49543daf547b125e9c9
hangilc <hangil@chang.jp>
2023-01-14 09:07:05 +0900

    temporary saving

--- packages/frontend/src/lib/Popup.svelte
+++ packages/frontend/src/lib/Popup.svelte
@@ -6,20 +6,23 @@
   let anchor: HTMLElement | SVGSVGElement | undefined = undefined;
   let clickLocation: ViewportCoord | undefined = undefined;
   let context: PopupContext | undefined = undefined;
+  export let triggerHook: () => Promise<void> = async () => {};
 
   function destroy(): void {
     context?.destroy();
     show = false;
   }
 
-  function trigger(event: MouseEvent): void {
+  async function trigger(event: MouseEvent) {
     event.preventDefault();
+    await triggerHook();
     anchor = event.currentTarget as HTMLElement | SVGSVGElement;
     show = true;
   }
 
-  function triggerClick(event: MouseEvent): void {
+  async function triggerClick(event: MouseEvent) {
     event.preventDefault();
+    await triggerHook();
     anchor = event.currentTarget as HTMLElement | SVGSVGElement;
     clickLocation = ViewportCoord.fromEvent(event);
     show = true;
