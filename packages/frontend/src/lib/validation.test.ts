03a86ee37d25903b99b0e0dbd3607d7ca0322c52
hangilc <hangil@chang.jp>
2023-01-19 23:50:15 +0900

    temporary saving

--- packages/frontend/src/lib/validation.test.ts
+++ packages/frontend/src/lib/validation.test.ts
@@ -1,4 +1,5 @@
 import { it, expect } from "vitest";
+import type { _ } from "vitest/dist/global-58e8e951";
 import {
   isNotNull,
   validate,
@@ -7,6 +8,11 @@ import {
   errorMessages,
   getError,
   isNotEmpty,
+  matchRegExp,
+  toInt,
+  toFloat,
+  validated2,
+  source,
 } from "./validation";
 
 it("should validate non-null", () => {
@@ -33,54 +39,62 @@ it("should validate non-empty string", () => {
   expect(errorMessages(getError(r2))).deep.equal(["greeting : 空白文字です"]);
 });
 
-// it("should validate with RegExp", () => {
-//   let ve = new VError();
-//   const validator = matchRegExp(/^hello+$/);
-//   let r = validator.unwrap("hello", "greeting", ve);
-//   expect(ve.isEmpty).toBe(true);
-//   expect(r).toBe("hello");
-//   ve = new VError();
-//   r = validator.unwrap(" hello", "greeting", ve);
-//   expect(ve.isEmpty).toBe(false);
-//   expect(ve.messages).deep.equal(["greeting : 入力が不適切です"]);
-// });
+it("should validate with RegExp", () => {
+  const v = matchRegExp<string>(/^hello+$/)
+  const r1 = validate("hello", v, "greeting");
+  expect(hasError(r1)).toBe(false);
+  expect(r1).toBe("hello");
+  const r2 = validate(" hello", v, "greeting");
+  expect(hasError(r2)).toBe(true);
+  expect(errorMessages(getError(r2))).deep.equal(["greeting : 入力が不適切です"]);
+});
 
-// it("should validate toInt", () => {
-//   let ve = new VError();
-//   const validator = toInt;
-//   let r = validator.unwrap("123", "int", ve);
-//   expect(ve.isEmpty).toBe(true);
-//   expect(r).toBe(123);
-//   ve = new VError();
-//   r = validator.unwrap("abc", "int", ve);
-//   expect(ve.isEmpty).toBe(false);
-//   expect(ve.messages).deep.equal(["int : 整数でありません"]);
-// });
+it("should validate toInt", () => {
+  const validator = toInt<string>();
+  const r1 = validate("123", validator, "int");
+  expect(hasError(r1)).toBe(false);
+  expect(r1).toBe(123);
+  const r2 = validate("abc", validator, "int");
+  expect(hasError(r2)).toBe(true);
+  expect(errorMessages(getError(r2))).deep.equal(["int : 整数でありません"]);
+});
 
-// it("should validate toFloat", () => {
-//   let ve = new VError();
-//   const validator = toFloat;
-//   let r = validator.unwrap("123.45", "int", ve);
-//   expect(ve.isEmpty).toBe(true);
-//   expect(r).toBe(123.45);
-//   ve = new VError();
-//   r = validator.unwrap("abc", "float", ve);
-//   expect(ve.isEmpty).toBe(false);
-//   expect(ve.messages).deep.equal(["float : 数値でありません"]);
-// });
+it("should validate toFloat", () => {
+  const validator = toFloat<string>();
+  const r1 = validate("123.45", validator, "float");
+  expect(hasError(r1)).toBe(false);
+  expect(r1).toBe(123.45);
+  const r2 = validate("abc", validator, "float");
+  expect(hasError(r2)).toBe(true);
+  expect(errorMessages(getError(r2))).deep.equal(["float : 数値でありません"]);
+});
 
-// it("should validate simple object", () => {
-//   let ve = new VError();
-//   const user = {
-//     id: isNotEmpty.and(toInt).unwrap("12", "id", ve),
-//     name: isNotEmpty.unwrap("Jane", "name", ve)
-//   };
-//   expect(ve.isEmpty).toBe(true);
-//   expect(user).deep.equal({ id: 12, name: "Jane"});
-//   const user2 = {
-//     id: isNotEmpty.and(toInt).unwrap("", "id", ve),
-//     name: isNotEmpty.unwrap("Jane", "name", ve)
-//   };
-//   expect(ve.isEmpty).toBe(false);
-//   expect(ve.messages).deep.equal(["id : 空白文字です"]);
-// });
+it("should validate simple object", () => {
+  const idVal = source("id", "12")
+    .validate(isNotEmpty())
+    .and(toInt());
+  
+  validate<string>(
+    "12", 
+    (isNotEmpty()).and(toInt()),
+    "id",
+    );
+  const nameVal = validate(
+    "Jane",
+    isNotNull<string, string>().and(isNotEmpty()),
+    "name"
+  );
+  const vs = validated2<string, _, _>(idVal, nameVal);
+  const user = { 
+    id: isNotEmpty.and(toInt).unwrap("12", "id", ve),
+    name: isNotEmpty.unwrap("Jane", "name", ve)
+  };
+  expect(ve.isEmpty).toBe(true);
+  expect(user).deep.equal({ id: 12, name: "Jane"});
+  const user2 = {
+    id: isNotEmpty.and(toInt).unwrap("", "id", ve),
+    name: isNotEmpty.unwrap("Jane", "name", ve)
+  };
+  expect(ve.isEmpty).toBe(false);
+  expect(ve.messages).deep.equal(["id : 空白文字です"]);
+});
--- packages/frontend/src/lib/validation.ts
+++ packages/frontend/src/lib/validation.ts
@@ -23,6 +23,11 @@ export class VError<M> {
     this.units = units;
   }
 
+  static from<M>(error: string): VError<M> {
+    const u = new VErrorUnit<M>([], error);
+    return new VError([u]);
+  }
+
   add(err: VError<M>): void {
     this.units.push(...err.units);
   }
@@ -42,21 +47,17 @@ export class VError<M> {
   static empty<M>(): VError<M> {
     return new VError<M>();
   }
-
-  // get messages(): string[] {
-  //   return this.units.map((u) => u.message);
-  // }
 }
 
 export function hasError<M>(e: any | VError<M>): boolean {
-  if( e instanceof VError<M> ){
+  if (e instanceof VError<M>) {
     return e.units.length > 0;
   }
   return false;
 }
 
 export function getError<M, T>(e: T | VError<M>): VError<M> | undefined {
-  if( e instanceof VError<M> ){
+  if (e instanceof VError<M>) {
     return e;
   } else {
     return undefined;
@@ -64,8 +65,8 @@ export function getError<M, T>(e: T | VError<M>): VError<M> | undefined {
 }
 
 export function errorMessages(e: undefined | VError<string>): string[] {
-  if( e instanceof VError<string> ){
-    return e.units.map(u => {
+  if (e instanceof VError<string>) {
+    return e.units.map((u) => {
       const ms = u.marks;
       return [...ms, u.error].join(" : ");
     });
@@ -74,23 +75,16 @@ export function errorMessages(e: undefined | VError<string>): string[] {
   }
 }
 
-type OkResult<T> = { ok: true; validated: T };
-
-type ErrorResult<M> = { ok: false; error: VError<M> };
-
-export type ValidationResult<M, T> = OkResult<T> | ErrorResult<M>;
-
 export class Validator<M, S, T> {
-  constructor(public f: (src: S) => ValidationResult<M, T>) {}
+  constructor(public f: (src: S) => T | VError<M>) {}
 
   and<U>(v: Validator<M, T, U>): Validator<M, S, U> {
     return new Validator<M, S, U>((s) => {
       const r = this.f(s);
-      if (r.ok) {
-        const t = r.validated;
-        return v.f(t);
-      } else {
+      if (r instanceof VError<M>) {
         return r;
+      } else {
+        return v.f(r);
       }
     });
   }
@@ -98,37 +92,13 @@ export class Validator<M, S, T> {
   map<U>(f: (t: T) => U): Validator<M, S, U> {
     return new Validator<M, S, U>((s: S) => {
       const r = this.f(s);
-      if (r.ok) {
-        return valid(f(r.validated));
-      } else {
+      if (r instanceof VError<M>) {
         return r;
+      } else {
+        return f(r);
       }
     });
   }
-
-  unwrapWithMark(
-    src: S,
-    mark: () => M,
-    errorHandler: (ve: VError<M>) => void
-  ): T {
-    const r = this.f(src);
-    if (r.ok) {
-      return r.validated;
-    } else {
-      errorHandler(r.error.prepend(mark()));
-      return undefined as never;
-    }
-  }
-}
-
-export function valid<T>(t: T): OkResult<T> {
-  return { ok: true, validated: t };
-}
-
-export function invalid<M>(error: string): ErrorResult<M> {
-  const unit = new VErrorUnit<M>([], error);
-  const ve = new VError<M>([unit]);
-  return { ok: false, error: ve };
 }
 
 function ensure<M, T>(
@@ -137,26 +107,22 @@ function ensure<M, T>(
 ): Validator<M, T, T> {
   return new Validator<M, T, T>((s: T) => {
     if (test(s)) {
-      return valid(s);
+      return s;
     } else {
       const e = typeof error === "string" ? error : error();
-      return invalid(e);
+      return VError.from(e);
     }
   });
 }
 
-function convert<M, S, T>(f: (s: S) => ValidationResult<M, T>) {
-  return new Validator<M, S, T>(f);
-}
-
 export function isNotNull<M, T>(): Validator<M, T | null, T> {
-  return new Validator<M, T | null, T>(s => {
-    if( s != null ){
-      return valid(s);
+  return new Validator<M, T | null, T>((s) => {
+    if (s != null) {
+      return s;
     } else {
-      return invalid("Null value");
+      return VError.from("Null value");
     }
-  })
+  });
 }
 
 export function isNotEmpty<M>(): Validator<M, string, string> {
@@ -167,14 +133,26 @@ export function matchRegExp<M>(re: RegExp): Validator<M, string, string> {
   return ensure<M, string>((s) => re.test(s), "入力が不適切です");
 }
 
-export class ValidatorWithMessage<S, T> extends Validator<string, S, T> {
-  constructor(f: (s: S) => ValidationResult<string, T>) {
-    super(f);
-  }
+export function toInt<M>(): Validator<M, string | number, number> {
+  return new Validator<M, string | number, number>((s: string | number) => {
+    const n = Number(s);
+    if (Number.isInteger(n)) {
+      return n;
+    } else {
+      return VError.from("整数でありません");
+    }
+  });
+}
 
-  // unwrap(src: S, mark: string, errorHandler: (e: VError<string>) => void): T {
-  //   return this.unwrapWithMark(src, () => mark, errorHandler);
-  // }
+export function toFloat<M>(): Validator<M, string | number, number> {
+  return new Validator<M, string | number, number>((s) => {
+    const n = Number(s);
+    if (!Number.isNaN(n)) {
+      return n;
+    } else {
+      return VError.from("数値でありません");
+    }
+  });
 }
 
 export function validate<M, S, T>(
@@ -183,10 +161,10 @@ export function validate<M, S, T>(
   mark: M
 ): T | VError<M> {
   const r = vtor.f(src);
-  if (r.ok) {
-    return r.validated;
+  if (r instanceof VError<M>) {
+    return r.prepend(mark);
   } else {
-    return r.error.prepend(mark);
+    return r;
   }
 }
 
@@ -203,27 +181,28 @@ export function validated2<M, T1, T2>(
   v2: T2 | VError<M>
 ): [T1, T2] | VError<M> {
   const left: [T1] | VError<M> = validated1(v1);
-  if( left instanceof VError<M> ){
-    if (v2 instanceof VError<M>) {
-      return left.combine(v2);
+  const right = v2;
+  if (left instanceof VError<M>) {
+    if (right instanceof VError<M>) {
+      return left.combine(right);
     } else {
       return left;
     }
-  } else if (v2 instanceof VError<M>) {
-    return v2;
+  } else if (right instanceof VError<M>) {
+    return right;
   } else {
-    return [...left, v2];
+    return [...left, right];
   }
 }
 
 export function validated3<M, T1, T2, T3>(
   v1: T1 | VError<M>,
   v2: T2 | VError<M>,
-  v3: T3 | VError<M>,
+  v3: T3 | VError<M>
 ): [T1, T2, T3] | VError<M> {
   const left: [T1, T2] | VError<M> = validated2(v1, v2);
   const right = v3;
-  if( left instanceof VError<M> ){
+  if (left instanceof VError<M>) {
     if (right instanceof VError<M>) {
       return left.combine(right);
     } else {
@@ -240,11 +219,11 @@ export function validated4<M, T1, T2, T3, T4>(
   v1: T1 | VError<M>,
   v2: T2 | VError<M>,
   v3: T3 | VError<M>,
-  v4: T4 | VError<M>,
+  v4: T4 | VError<M>
 ): [T1, T2, T3, T4] | VError<M> {
   const left: [T1, T2, T3] | VError<M> = validated3(v1, v2, v3);
   const right = v4;
-  if( left instanceof VError<M> ){
+  if (left instanceof VError<M>) {
     if (right instanceof VError<M>) {
       return left.combine(right);
     } else {
@@ -257,7 +236,22 @@ export function validated4<M, T1, T2, T3, T4>(
   }
 }
 
+class Source<M, S> {
+  mark: M;
+  value: S;
+  constructor(mark: M, value: S) {
+    this.mark = mark;
+    this.value = value;
+  }
 
+  validate<T>(vtor: Validator<M, S, T>): T | VError<M> {
+    return vtor.f(this.value);
+  }
+}
+
+export function source<M, S>(mark: M, value: S): Source<M, S> {
+  return new Source(mark, value);
+}
 // export class ValidatorBuilder<M extends VErrorMark> {
 //   marker: (label: string) => M;
 
@@ -387,3 +381,4 @@ export function validated4<M, T1, T2, T3, T4>(
 //     }));
 //   }
 // }
+
