a0005860d6a4fcb2dc2106adf262d9bf093d53ce
hangilc <hangil@chang.jp>
2023-01-20 09:21:55 +0900

    temporary saving

--- packages/frontend/src/lib/validation.test.ts
+++ packages/frontend/src/lib/validation.test.ts
@@ -1,89 +1,65 @@
 import { it, expect } from "vitest";
-import type { _ } from "vitest/dist/global-58e8e951";
-import {
-  isNotNull,
-  validate,
-  VError,
-  hasError,
-  errorMessages,
-  getError,
-  isNotEmpty,
-  matchRegExp,
-  toInt,
-  toFloat,
-  validated2,
-  source,
-} from "./validation";
+import { errorMessage, isNotEmpty, isNotNull, toInt, valid, VResult} from "./validation";
 
 it("should validate non-null", () => {
-  const r1 = validate("12", isNotNull(), "digits");
-  expect(r1 instanceof VError<string>).toBe(false);
-  expect(r1).toBe("12");
+  const r1 = valid("12").flatMap(isNotNull).mark("digits");
+  expect(r1.result.isValid).toBe(true);
+  expect(r1.result.getValue()).toBe("12");
 
-  const r2 = validate<string, string | null, string>(
-    null,
-    isNotNull(),
-    "digits"
-  );
-  expect(hasError(r2)).toBe(true);
-  expect(errorMessages(getError(r2))).deep.equal(["digits : Null value"]);
+  const r2 = valid<string | null>(null).flatMap(isNotNull).mark("digits");
+  expect(r2.result.isValid).toBe(false);
+  expect(errorMessage(r2)).toBe("digits : Null value");
 });
 
-it("should validate non-empty string", () => {
-  const r1 = validate("hello", isNotEmpty(), "greeting");
-  expect(hasError(r1)).toBe(false);
-  expect(r1).toBe("hello");
+// it("should validate non-empty string", () => {
+//   const r1 = validate("hello", isNotEmpty(), "greeting");
+//   expect(hasError(r1)).toBe(false);
+//   expect(r1).toBe("hello");
   
-  const r2 = validate<string, string, string>("", isNotEmpty(), "greeting");
-  expect(hasError(r2)).toBe(true);
-  expect(errorMessages(getError(r2))).deep.equal(["greeting : 空白文字です"]);
-});
+//   const r2 = validate<string, string, string>("", isNotEmpty(), "greeting");
+//   expect(hasError(r2)).toBe(true);
+//   expect(errorMessages(getError(r2))).deep.equal(["greeting : 空白文字です"]);
+// });
 
-it("should validate with RegExp", () => {
-  const v = matchRegExp<string>(/^hello+$/)
-  const r1 = validate("hello", v, "greeting");
-  expect(hasError(r1)).toBe(false);
-  expect(r1).toBe("hello");
-  const r2 = validate(" hello", v, "greeting");
-  expect(hasError(r2)).toBe(true);
-  expect(errorMessages(getError(r2))).deep.equal(["greeting : 入力が不適切です"]);
-});
+// it("should validate with RegExp", () => {
+//   const v = matchRegExp<string>(/^hello+$/)
+//   const r1 = validate("hello", v, "greeting");
+//   expect(hasError(r1)).toBe(false);
+//   expect(r1).toBe("hello");
+//   const r2 = validate(" hello", v, "greeting");
+//   expect(hasError(r2)).toBe(true);
+//   expect(errorMessages(getError(r2))).deep.equal(["greeting : 入力が不適切です"]);
+// });
 
-it("should validate toInt", () => {
-  const validator = toInt<string>();
-  const r1 = validate("123", validator, "int");
-  expect(hasError(r1)).toBe(false);
-  expect(r1).toBe(123);
-  const r2 = validate("abc", validator, "int");
-  expect(hasError(r2)).toBe(true);
-  expect(errorMessages(getError(r2))).deep.equal(["int : 整数でありません"]);
-});
+// it("should validate toInt", () => {
+//   const validator = toInt<string>();
+//   const r1 = validate("123", validator, "int");
+//   expect(hasError(r1)).toBe(false);
+//   expect(r1).toBe(123);
+//   const r2 = validate("abc", validator, "int");
+//   expect(hasError(r2)).toBe(true);
+//   expect(errorMessages(getError(r2))).deep.equal(["int : 整数でありません"]);
+// });
 
-it("should validate toFloat", () => {
-  const validator = toFloat<string>();
-  const r1 = validate("123.45", validator, "float");
-  expect(hasError(r1)).toBe(false);
-  expect(r1).toBe(123.45);
-  const r2 = validate("abc", validator, "float");
-  expect(hasError(r2)).toBe(true);
-  expect(errorMessages(getError(r2))).deep.equal(["float : 数値でありません"]);
-});
+// it("should validate toFloat", () => {
+//   const validator = toFloat<string>();
+//   const r1 = validate("123.45", validator, "float");
+//   expect(hasError(r1)).toBe(false);
+//   expect(r1).toBe(123.45);
+//   const r2 = validate("abc", validator, "float");
+//   expect(hasError(r2)).toBe(true);
+//   expect(errorMessages(getError(r2))).deep.equal(["float : 数値でありません"]);
+// });
 
 it("should validate simple object", () => {
-  const idVal = source("id", "12")
-    .validate(isNotEmpty())
-    .and(toInt());
+  const idVal = valid("12")
+    .flatMap(isNotEmpty)
+    .flatMap(toInt)
+    .mark("id");
+  const nameVal = valid("jane")
+    .flatMap(isNotEmpty)
+    .mark("name");
   
-  validate<string>(
-    "12", 
-    (isNotEmpty()).and(toInt()),
-    "id",
-    );
-  const nameVal = validate(
-    "Jane",
-    isNotNull<string, string>().and(isNotEmpty()),
-    "name"
-  );
   const vs = validated2<string, _, _>(idVal, nameVal);
   const user = { 
     id: isNotEmpty.and(toInt).unwrap("12", "id", ve),
