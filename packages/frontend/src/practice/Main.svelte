e98d9bfe50777997b8dd8bf70a54583d612aff0f
hangilc <hangil@chang.jp>
2023-01-22 10:59:24 +0900

    cleanup

--- packages/frontend/src/practice/Main.svelte
+++ packages/frontend/src/practice/Main.svelte
@@ -5,12 +5,6 @@
   import Phone from "./phone/Phone.svelte";
   import { currentPatient } from "./exam/ExamVars";
 
-  import DateForm from "@/lib/date-form/DateForm.svelte";
-  import { errorMessagesOf, validResult, VResult, type VError } from "@/lib/validation";
-  import DateFormPulldown from "@/lib/date-form/DateFormPulldown.svelte";
-  import DateFormWithCalendar from "@/lib/date-form/DateFormWithCalendar.svelte";
-  import EditableDate from "@/lib/editable-date/EditableDate.svelte";
-
   export let serviceStore: Writable<string>;
 
   currentPatient.subscribe((p) => {
@@ -21,30 +15,10 @@
     }
   });
 
-  let dateResult: VResult<Date | null> = validResult(null);
-  function doFormChange(r: VResult<Date | null>): void {
-    dateResult = r;
-  }
-
-  let date = new Date();
-  function doFormEnter(){
-  }
 </script>
 
 <div>
   <Exam isVisible={$serviceStore === "exam"} />
   <Cashier isVisible={$serviceStore === "cashier"} />
   <Phone isVisible={$serviceStore === "phone"} />
-
-  <EditableDate bind:date={date} />
-  <div>{date}</div>
-  {#if dateResult.isValid}
-    {dateResult.value}
-  {:else}
-    {#each errorMessagesOf(dateResult.errors) as error}
-      <div>
-        {error}
-      </div>
-    {/each}
-  {/if}
 </div>
