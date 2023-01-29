1ff9aef4683a9770d161f9592f1ea4c08c3e3799
hangilc <hangil@chang.jp>
2023-01-29 22:57:10 +0900

    added cypress

---
+++ packages/frontend/src/cypress/cypress/component/ShahokokuhoDialogContent.cy.js
@@ -0,0 +1,8 @@
+import ShahokokuhoDialogContent from "@/cashier/patient-dialog2/edit/ShahokokuhoDialogContent.svelte";
+
+describe('ShahokokuhoDialogContent.cy.js', () => {
+  it('playground', () => {
+    // cy.mount(ShahokokuhoDialogContent);
+    expect(true).to.eq(true);
+  })
+})
