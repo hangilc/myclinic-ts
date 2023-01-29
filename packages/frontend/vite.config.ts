5e819b1229b547cae43fc6ecd30b1c892719676d
hangilc <hangil@chang.jp>
2023-01-29 16:04:16 +0900

    config cleanup

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -35,35 +35,3 @@ export default defineConfig(({command, mode, ssrBuild }) => ({
 
 }))
 
-// https://vitejs.dev/config/
-// defineConfigSave({
-//   plugins: [svelte()],
-//   build: {
-//     rollupOptions: {
-//       input: {
-//         main: resolve(__dirname, "index.html"),
-//         practice: resolve(__dirname, "practice/index.html"),
-//         reception: resolve(__dirname, "reception/index.html"),
-//         appoint: resolve(__dirname, "appoint/index.html"),
-//       }
-//     },
-//     chunkSizeWarningLimit: 5000
-//   },
-//   resolve: {
-//     alias: {
-//       "@": resolve(__dirname, "src")
-//     }
-//   },
-//   base: "/vite/",
-//   server: {
-//     fs: {
-//       allow: [
-//         "../../node_modules/@histoire", 
-//         "../../node_modules/histoire",
-//         "../../node_modules/svelte-hmr",
-//         ".", 
-//         "../model",
-//       ],
-//     }
-//   }
-// })
