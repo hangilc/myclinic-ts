5f0d2634c0bedc5b4cc74f081a12f5118a1764df
hangilc <hangil@chang.jp>
2023-01-29 23:48:50 +0900

    temporary saving

--- packages/frontend/src/cypress/cypress/component/ShahokokuhoDialogContent.cy.js
+++
@@ -1,8 +0,0 @@
-import ShahokokuhoDialogContent from "@/cashier/patient-dialog2/edit/ShahokokuhoDialogContent.svelte";
-
-describe('ShahokokuhoDialogContent.cy.js', () => {
-  it('playground', () => {
-    // cy.mount(ShahokokuhoDialogContent);
-    expect(true).to.eq(true);
-  })
-})
---
+++ packages/frontend/src/cypress/cypress/component/ShahokokuhoDialogContent.cy.ts
@@ -0,0 +1,7 @@
+import ShahokokuhoDialogContent from "@/cashier/patient-dialog2/edit/ShahokokuhoDialogContent.svelte";
+
+describe('ShahokokuhoDialogContent.cy.ts', () => {
+  it('playground', () => {
+    cy.mount(ShahokokuhoDialogContent);
+  })
+})
