d09ddc72d122d06ad5e0eb884ac09cb2ba4a4aba
hangilc <hangil@chang.jp>
2023-01-30 09:20:52 +0900

    added e2e testing

--- work/cypress/component/ShahokokuhoDialogContent.cy.ts
+++
@@ -1,7 +0,0 @@
-import ShahokokuhoDialogContent from "@/cashier/patient-dialog2/edit/ShahokokuhoDialogContent.svelte";
-
-describe('ShahokokuhoDialogContent.cy.ts', () => {
-  it('playground', () => {
-    cy.mount(ShahokokuhoDialogContent);
-  })
-})
