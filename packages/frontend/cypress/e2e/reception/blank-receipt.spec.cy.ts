b4fd14c0c6ab9df798ab29fbabc9b904bb99539b
hangilc <hangil@chang.jp>
2023-02-03 23:01:26 +0900

    added tsconfig.json to cypress/e2e

--- packages/frontend/cypress/e2e/reception/blank-receipt.spec.cy.ts
+++ packages/frontend/cypress/e2e/reception/blank-receipt.spec.cy.ts
@@ -1,4 +1,5 @@
 import { castList } from "@/lib/cast";
+import { castOp } from "@/lib/drawer/op";
 import { openedDialog } from "./misc";
 
 describe("Blank Receipt", () => {
@@ -9,10 +10,10 @@ describe("Blank Receipt", () => {
     cy.intercept("http://localhost:48080/pref/receipt", "null");
     cy.intercept("http://localhost:48080/print/", (req) => {
       const body = typeof req.body === "string" ? JSON.parse(req.body) : req.body;
-      const setup = castList(arg => arg)(body.setup);
-      const pages = castList(castList(arg => arg))(body.pages);
-      // console.log("setup", setup);
-      // console.log("pages", pages);
+      const setup = castList(castOp)(body.setup);
+      const pages = castList(castList(castOp))(body.pages);
+      console.log("setup", setup);
+      console.log("pages", pages);
       req.reply("true");
     });
     cy.get("a").contains("手書き領収書印刷").click();
