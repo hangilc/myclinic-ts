6c1e505c794536f9ce47e1915486e284b6d258f6
hangilc <hangil@chang.jp>
2023-02-03 17:27:45 +0900

    added blank receipt e2e test

---
+++ packages/frontend/cypress/e2e/reception/blank-receipt.spec.cy.ts
@@ -0,0 +1,25 @@
+import { castList } from "@/lib/cast";
+import { openedDialog } from "./misc";
+
+describe("Blank Receipt", () => {
+  it("should open blank receipt dialog", () => {
+    cy.visit("/reception/");
+    cy.get("[data-cy=bars3-menu]").click();
+    cy.intercept("http://localhost:48080/setting", "[]");
+    cy.intercept("http://localhost:48080/pref/receipt", "null");
+    cy.intercept("http://localhost:48080/print/", (req) => {
+      const body = typeof req.body === "string" ? JSON.parse(req.body) : req.body;
+      const setup = castList(arg => arg)(body.setup);
+      const pages = castList(castList(arg => arg))(body.pages);
+      // console.log("setup", setup);
+      // console.log("pages", pages);
+      req.reply("true");
+    });
+    cy.get("a").contains("手書き領収書印刷").click();
+    openedDialog("領収書印刷").within(() => {
+      cy.get("button").contains("印刷").click();
+    })
+  })
+})
+
+export {}
