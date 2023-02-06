884410bdb1bd58a0bd00e133bbeda94186031b5d
hangilc <hangil@chang.jp>
2023-02-06 11:57:44 +0900

    temporary saving

--- packages/frontend/cypress/component/practice/disease/Edit.spec.cy.ts
+++ packages/frontend/cypress/component/practice/disease/Edit.spec.cy.ts
@@ -1,7 +1,7 @@
 import { base } from "@/lib/api";
 import Edit from "@/practice/exam/disease2/edit/Edit.svelte";
 import { dialogClose, openedDialog } from "@cypress/e2e/reception/misc";
-import { assertDateForm } from "@cypress/lib/form";
+import { assertDateForm, fillDateForm } from "@cypress/lib/form";
 import { ByoumeiMaster, Disease, DiseaseAdj, DiseaseData, DiseaseEndReason, ShuushokugoMaster } from "myclinic-model";
 
 describe("Edit Disease", () => {
@@ -91,5 +91,34 @@ describe("Edit Disease", () => {
     })
     dialogClose("確認");
     cy.wrap({}).then(() => expect(deleted).equal(true));
+  })
+
+  it.only("should edit byoumeimaster of disease", () => {
+    const diseases = [
+      new DiseaseData(
+        new Disease(1, 1, 1, "2022-02-01", "0000-00-00", "N"),
+        new ByoumeiMaster(1, "急性咽頭炎"),
+        [
+          [new DiseaseAdj(1, 1, 8002), new ShuushokugoMaster(8002, "の疑い")]
+        ]
+      )
+    ];
+    cy.mount(Edit, {
+      props: {
+        diseases,
+        onDelete: (diseaseId: number) => {
+          expect(diseaseId).equal(1);
+        }
+      }
+    });
+    cy.get("[data-cy=disease-name][data-disease-id=1]").click();
+    cy.get("[data-cy=disease-search-input]").type("急性気管支炎");
+    cy.intercept(base + "/search-byoumei-master*", (req) => {
+      const master = new ByoumeiMaster(2, "急性気管支炎");
+      req.reply([master]);
+    });
+    cy.get("button").contains("検索").click();
+    cy.get("[data-cy=search-result] [data-cy=search-result-item]").contains("急性気管支炎").click();
+    cy.get("[data-cy=disease-name]").contains("急性気管支炎の疑い");
   })
 });
