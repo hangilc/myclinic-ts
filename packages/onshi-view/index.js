adad169728921f7a9f438f4f1dfe164c6e8265ca
Hangil Chang <hangil@chang.jp>
2023-02-28 13:57:53 +0900

    added ws to onshi-view

--- packages/onshi-view/index.js
+++ packages/onshi-view/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const fs = require("fs");
+const path = require("path");
+const { WebSocketServer } = require("ws");
 
-let directory = ".";
+let faceDir = resolveFaceDir();
 let port = 9090;
 
-app.get("/", (req, res) => {
-  fs.readdir(directory, (err, files) => {
+app.get("/face", (req, res) => {
+  fs.readdir(faceDir, (err, files) => {
     if( err ){
       console.log(err);
       res.send("[]");
@@ -16,6 +18,61 @@ app.get("/", (req, res) => {
   })
 });
 
-app.listen(port, () => {
-  
+app.get("/face/:file", (req, res, next) => {
+  const fpath = path.resolve(faceDir, req.params["file"]);
+  fs.readFile(fpath, { encoding: "UTF-8" }, (err, buffer) => {
+    if( err ){
+      next(err);
+    } else {
+      res.set({ "Content-Type": "application/xml" })
+      res.send(buffer);
+    }
+  })
+});
+
+const ws = new WebSocketServer({ port: 9091 });
+
+const ws_peers = [];
+
+ws.on("connection", conn => {
+  conn.on("error", console.error);
+
+  conn.on("message", data => {
+    console.log("received ws message", data);
+  });
+
+  conn.on("close", () => {
+    const i = ws_peers.findIndex((c) => c === conn);
+    if( i >= 0 ){
+      ws_peers.splice(i, 1);
+      console.log(ws_peers);
+    }
+  })
+
+  ws_peers.push(conn);
+});
+
+fs.watch(faceDir, (eventType, fileName) => {
+  console.log(eventType, fileName);
+  if( eventType === "change" ){
+    ws_peers.forEach(peer => {
+      const data = {
+        type: "watch",
+        body: fileName
+      }
+      peer.send(JSON.stringify(data));
+    })
+  }
 })
+
+app.listen(port, () => {
+  console.log(`onshi-view listening to port ${port}`)
+})
+
+function resolveFaceDir() {
+  const d = process.env["ONSHI_FACE_DIR"];
+  if( !d ){
+    throw new Error("Cannot find env var: ONSHI_FACE_DIR");
+  }
+  return d;
+}
