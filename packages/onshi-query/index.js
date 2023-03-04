9e4c8ded43c7f7a865a71e4c7094087a2d095ec6
Hangil Chang <hangil@chang.jp>
2023-03-04 12:39:57 +0900

    temporary saving

--- packages/onshi-query/index.js
+++ packages/onshi-query/index.js
@@ -43,15 +43,16 @@ async function main() {
   console.log(patient);
   const [shahoList] = await conn.query(`
     select * from hoken_shahokokuho where patient_id = ? 
-      and valid_from <= ? and ? <= valid_upto
-    `, [ patientId, confirmDate, confirmDate ]);
-  console.log(shahoList);
+      and valid_from <= ? and (valid_upto = '0000-00-00' or ? <= valid_upto)
+  `, [ patientId, confirmDate, confirmDate ]);
   const shaho = shahoList[0];
+  console.log("shaho-length", shahoList.length);
   if( shaho ){
+    console.log(shaho);
     const result = await onshiLogin(certs.certFile, certs.keyFile, certs.jsonFile);
     const idToken = result.result.idToken;
     const r = await onshiSearch(idToken, {
-      hokensha: pad(shaho.hokensha_bangou, 8, " "),
+      hokensha: pad(shaho.hokensha_bangou, 8, "0"),
       hihokensha: shaho.hihokensha_bangou,
       kigou: shaho.hihokensha_kigou,
       birthdate: dateToSqlDate(patient.birth_day).replaceAll("-", ""),
@@ -59,5 +60,41 @@ async function main() {
     });
     console.log(JSON.stringify(r, undefined, 2));
   }
+  const [koukikoureiList] = await conn.query(`
+    select * from hoken_koukikourei where patient_id = ?
+    and valid_from <= ? and (valid_upto = '0000-00-00' or ? <= valid_upto)
+  `, [ patientId, confirmDate, confirmDate ]);
+  const koukikourei = koukikoureiList[0];
+  if( koukikourei ){
+    console.log(koukikourei);
+    const result = await onshiLogin(certs.certFile, certs.keyFile, certs.jsonFile);
+    const idToken = result.result.idToken;
+    const r = await onshiSearch(idToken, {
+      hokensha: pad(koukikourei.hokensha_bangou, 8, " "),
+      hihokensha: koukikourei.hihokensha_bangou,
+      birthdate: dateToSqlDate(patient.birth_day).replaceAll("-", ""),
+      confirmationDate: confirmDate.replaceAll("-", "")
+    });
+    console.log(JSON.stringify(r, undefined, 2));
+  }
+  const [kouhiList] = await conn.query(`
+    select * from kouhi where patient_id = ?
+    and valid_from <= ? and (valid_upto = '0000-00-00' or ? <= valid_upto)
+  `, [ patientId, confirmDate, confirmDate ]);
+  const kouhiProms = kouhiList.map(async (kouhi) => {
+    console.log(kouhi);
+    const result = await onshiLogin(certs.certFile, certs.keyFile, certs.jsonFile);
+    const idToken = result.result.idToken;
+    return await onshiSearch(idToken, {
+      hokensha: pad(kouhi.futansha, 8, " "),
+      hihokensha: kouhi.jukyuusha,
+      birthdate: dateToSqlDate(patient.birth_day).replaceAll("-", ""),
+      confirmationDate: confirmDate.replaceAll("-", "")
+    });
+  });
+  const kouhiConfirms = await Promise.all(kouhiProms);
+  kouhiConfirms.forEach(c => {
+    console.log(JSON.stringify(c, undefined, 2));
+  });
   await conn.end();
 }
