fcc19498ad9de100e0f3b2ccd528f19ac41c4202
hangilc <hangil@chang.jp>
2023-04-08 22:14:00 +0900

    revised FaceConfirmedWindow test

--- packages/frontend/cypress/component/cashier/FaceConfirmedWindow.spec.cy.ts
+++ packages/frontend/cypress/component/cashier/FaceConfirmedWindow.spec.cy.ts
@@ -5,9 +5,10 @@ import { createShahokokuho } from "@cypress/lib/shahokokuho-mock";
 import type { OnshiResult } from "onshi-result";
 import { onshiCreationModifier as m } from "@cypress/lib/onshi-mock";
 import { apiBase } from "@cypress/lib/base";
-import { Visit, type Patient, type Shahokokuho } from "myclinic-model";
+import { Patient, Shahokokuho, Visit } from "myclinic-model";
 import { enterShahokokuho } from "@cypress/lib/shahokokuho";
-import { confirmCancel, confirmYes, confirmYesContainingMessage } from "@cypress/lib/confirm";
+import { confirmYesContainingMessage } from "@cypress/lib/confirm";
+import { shahokokuhoOnshiConsistent } from "@/lib/hoken-onshi-consistent";
 
 describe("FaceConfirmedWindow", () => {
   it("should mount", () => {
@@ -22,10 +23,60 @@ describe("FaceConfirmedWindow", () => {
 
   it("should enter new hoken when none available", () => {
     enterPatient(createPatient()).as("patient");
-    enterShahokokuho(createShahokokuho()).as("shahokokuho");
     cy.get<Patient>("@patient").then((patient) => {
+      const shahokokuho = createShahokokuho({ patientId: patient.patientId });
+      const result = createOnshiResult(m.patient(patient), m.shahokokuho(shahokokuho));
+      expect(result.messageBody.resultList.length).equal(1);
+      cy.intercept(
+        "GET",
+        apiBase() + "/search-patient?text=*",
+        [10, [patient]]);
+      const props = {
+        destroy: () => { },
+        result,
+        onRegister: () => { }
+      };
+      cy.mount(FaceConfirmedWindow, { props });
+      cy.get("[data-cy=message]").contains("新しい保険証");
+      cy.get("button").contains("新規保険証登録").click();
+      confirmYesContainingMessage("登録します");
+      cy.get("[data-cy=message]").should("not.exist");
+      cy.stub(props, "onRegister").as("onRegister");
+      cy.get("button").contains("診察登録").click();
+      cy.get("@onRegister").should("be.called");
+      cy.request(
+        apiBase() + `/list-visit-id-by-patient-reverse?patient-id=${patient.patientId}&offset=0&count=1`,
+      ).its("body").then((body: number[]) => {
+        expect(body.length).equal(1);
+        return body[0]
+      }).as("visitId");
+      cy.get<number>("@visitId").then((visitId: number) => {
+        cy.request(
+          apiBase() + `/get-visit?visit-id=${visitId}`
+        ).its("body").then((body) => Visit.cast(body))
+          .as("visit");
+      });
+      cy.get<Visit>("@visit").then((visit: Visit) => {
+        const shahokokuhoId = visit.shahokokuhoId;
+        expect(shahokokuhoId).to.be.gt(0);
+        cy.request("GET", apiBase() + "/get-shahokokuho?shahokokuho-id=" + shahokokuhoId)
+          .its("body").then(body => Shahokokuho.cast(body)).as("shahokokuho");
+      });
       cy.get<Shahokokuho>("@shahokokuho").then((shahokokuho) => {
-        const result = createOnshiResult(h => h, m.patient(patient), m.shahokokuho(shahokokuho));
+        expect(shahokokuhoOnshiConsistent(shahokokuho, result.messageBody.resultList[0]))
+      });
+    });
+  });
+
+  it("should register patient with valid shahokokuho", () => {
+    enterPatient(createPatient()).as("patient");
+    cy.get<Patient>("@patient").then((patient: Patient) => {
+      const shahokokuho = createShahokokuho({ patientId: patient.patientId });
+      enterShahokokuho(shahokokuho).as("shahokokuho");
+    });
+    cy.get<Patient>("@patient").then((patient: Patient) => {
+      cy.get<Shahokokuho>("@shahokokuho").then((shahokokuho: Shahokokuho) => {
+        const result = createOnshiResult(m.patient(patient), m.shahokokuho(shahokokuho))
         cy.intercept(
           "GET",
           apiBase() + "/search-patient?text=*",
@@ -36,29 +87,10 @@ describe("FaceConfirmedWindow", () => {
           onRegister: () => { }
         };
         cy.mount(FaceConfirmedWindow, { props });
-        cy.get("[data-cy=message]").contains("新しい保険証");
-        cy.get("button").contains("新規保険証登録").click();
-        confirmYesContainingMessage("登録します");
         cy.get("[data-cy=message]").should("not.exist");
-        cy.stub(props, "onRegister").callsFake(() => {
-          cy.request(
-            apiBase() + `/list-visit-id-by-patient-reverse?patient-id=${patient.patientId}&offset=0&count=1`,
-          ).its("body").then((body: number[]) => {
-            expect(body.length).equal(1);
-            return body[0]
-          }).as("visitId");
-          cy.get<number>("@visitId").then((visitId: number) => {
-            cy.request(
-              apiBase() + `/get-visit?visit-id=${visitId}`
-            ).its("body").then((body) => Visit.cast(body))
-              .as("visit");
-          });
-          cy.get<Visit>("@visit").then((visit: Visit) => {
-            expect(visit.shahokokuhoId).containSubset(shahokokuho.shahokokuhoId);
-          });
-        });
-        cy.get("button").contains("診察登録").click();
-      })
+        cy.get("button").contains("診察登録").should("exist");
+      });
     });
-  })
+  });
 });
+
