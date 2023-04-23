355fb2520998ede7f8f1648cf954ce257f0507eb
hangilc <hangil@chang.jp>
2023-04-23 15:48:49 +0900

    added checkHokenInterval

--- packages/frontend/src/lib/hoken-check.ts
+++ packages/frontend/src/lib/hoken-check.ts
@@ -11,7 +11,11 @@ export class OverlapExists {
   }
 }
 
-type ErrorResult = OverlapExists;
+export class Used {
+  readonly isUsedClass: boolean = true;
+}
+
+type ErrorResult = OverlapExists | Used;
 
 export async function checkHokenInterval(hoken: Shahokokuho | Koukikourei): Promise<ErrorResult[]> {
   if( hoken instanceof Shahokokuho ){
@@ -22,12 +26,13 @@ export async function checkHokenInterval(hoken: Shahokokuho | Koukikourei): Prom
 }
 
 async function checkShahokokuhoInterval(shahokokuho: Shahokokuho): Promise<ErrorResult[]> {
+  const shahokokuhoId = shahokokuho.shahokokuhoId;
   const errs: ErrorResult[] = [];
   let list = await listHoken(shahokokuho.patientId);
-  if( shahokokuho.shahokokuhoId !== 0 ){
+  if( shahokokuhoId !== 0 ){
     list = list.filter(h => {
       if( h instanceof Shahokokuho ){
-        return h.shahokokuhoId !== shahokokuho.shahokokuhoId;
+        return h.shahokokuhoId !== shahokokuhoId;
       } else {
         return true;
       }
@@ -38,12 +43,52 @@ async function checkShahokokuhoInterval(shahokokuho: Shahokokuho): Promise<Error
       errs.push(new OverlapExists(h));
     }
   });
-  const cBefore = await api.countSh
+  if( shahokokuhoId > 0 ){
+    const cBefore = await api.countShahokokuhoUsageBefore(shahokokuhoId, shahokokuho.validFrom);
+    if( cBefore > 0 ){
+      errs.push(new Used());
+    }
+    if( shahokokuho.validUpto !== "0000-00-00" ){
+      const cAfter = await api.countShahokokuhoUsageAfter(shahokokuhoId, shahokokuho.validUpto);
+      if( cAfter > 0 ){
+        errs.push(new Used());
+      }
+    }
+  }
   return errs;
 }
 
 async function checkKoukikoureiInterval(koukikourei: Koukikourei): Promise<ErrorResult[]> {
-
+  const koukikoureiId = koukikourei.koukikoureiId;
+  const errs: ErrorResult[] = [];
+  let list = await listHoken(koukikourei.patientId);
+  if( koukikoureiId !== 0 ){
+    list = list.filter(h => {
+      if( h instanceof Koukikourei ){
+        return h.koukikoureiId !== koukikoureiId;
+      } else {
+        return true;
+      }
+    });
+  }
+  list.forEach(h => {
+    if( dateIntervalsOverlap(koukikourei.validFrom, koukikourei.validUpto, h.validFrom, h.validUpto) ){
+      errs.push(new OverlapExists(h));
+    }
+  });
+  if( koukikoureiId > 0 ){
+    const cBefore = await api.countKoukikoureiUsageBefore(koukikoureiId, koukikourei.validFrom);
+    if( cBefore > 0 ){
+      errs.push(new Used());
+    }
+    if( koukikourei.validUpto !== "0000-00-00" ){
+      const cAfter = await api.countKoukikoureiUsageAfter(koukikoureiId, koukikourei.validUpto);
+      if( cAfter > 0 ){
+        errs.push(new Used());
+      }
+    }
+  }
+  return errs;
 }
 
 async function listHoken(patientId: number): Promise<(Shahokokuho | Koukikourei)[]> {
