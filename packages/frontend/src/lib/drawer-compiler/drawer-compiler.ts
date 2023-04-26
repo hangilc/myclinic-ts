f0b232be8fc945cb033299e7dc690f0bdc97829c
hangilc <hangil@chang.jp>
2023-04-27 00:01:52 +0900

    extending text

--- packages/frontend/src/lib/drawer-compiler/drawer-compiler.ts
+++ packages/frontend/src/lib/drawer-compiler/drawer-compiler.ts
@@ -60,53 +60,48 @@ export class DrawerCompiler {
   }
 
   text(b: Box, t: string, opt: TextOpt = {}): void {
-    let y: number;
+    const chars: string[] = [];
+    const xs: number[] = [];
+    let left: number = 0;
     const fontSize = this.curFontSize;
-    function calcTextWidth(): number {
-      const ics = opt.interCharsSpace ?? 0;
-      let w = 0;
-      for(let c of t){
-        if( w > 0 ){
-          w += ics;
-        }
-        w += charWidth(c, fontSize);
-      }
-      return w;
+    const elements: string[] = Array.from(t);
+    const ws: number[] = elements.map(c => charWidth(c, fontSize));
+    let ics: number = 0;
+    if (opt.halign === HorizAlign.Justify && ws.length > 1) {
+      ics = (b.width - ws.reduce((a, b) => a + b, 0)) / (ws.length - 1);
+    } else if (opt.interCharsSpace !== undefined) {
+      ics = opt.interCharsSpace;
     }
-    function calcJustifyICS(): number {
-      const len = t.length;
-      if( len <= 1 ){
-        return 0;
-      } else {
-        return (b.width - calcTextWidth()) / (len - 1);
+    elements.forEach((e, i) => {
+      chars.push(e);
+      if( i > 0 ){
+        left += ics;
       }
-    }
-    function resolveICS(): number {
-      if( opt.halign === HorizAlign.Justify ){
-        return calcJustifyICS();
-      } else {
-        return opt.interCharsSpace ?? 0;
-      }
-    }
+      xs.push(left);
+      left += ws[i];
+    });
+    console.log("xs", xs);
+    let y: number;
     switch (opt.valign ?? VertAlign.Center) {
-      case VertAlign.Top: y = b.top; break;
       case VertAlign.Center: y = b.top + (b.height - fontSize) / 2; break;
       case VertAlign.Bottom: y = b.bottom - fontSize; break;
+      default: y = b.top; break;
     }
-    let x: number;
+    let dx = b.left;
     switch (opt.halign ?? HorizAlign.Center) {
-      case HorizAlign.Left: x = b.left; break;
-      case HorizAlign.Center: x = b.left + (b.width - calcTextWidth()) / 2; break;
-      case HorizAlign.Right: x = b.right - calcTextWidth(); break;
-      case HorizAlign.Justify: x = b.left; break;
+      case HorizAlign.Center: {
+        dx += (b.width - left) / 2.0;
+        break;
+      }
+      case HorizAlign.Right: {
+        dx += (b.width - left);
+        break;
+      }
     }
-    let xs: number[] = [];
-    const ics: number = resolveICS();
-    for (let c of t) {
-      xs.push(x);
-      x += charWidth(c.codePointAt(0)!, fontSize) + ics;
+    for(let i=0;i<xs.length;i++){
+      xs[i] += dx;
     }
-    this.ops.push(new OpDrawChars(t, xs, [y]));
+    this.ops.push(new OpDrawChars(chars.join(""), xs, [y]));
   }
 }
 
