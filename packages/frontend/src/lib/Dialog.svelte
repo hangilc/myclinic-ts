20041b7d4011fa62879b5ad4d2a9a6db590e7e58
hangilc <hangil@chang.jp>
2023-05-04 13:50:17 +0900

    added unfix/fix to Dialog

--- packages/frontend/src/lib/Dialog.svelte
+++ packages/frontend/src/lib/Dialog.svelte
@@ -12,6 +12,7 @@
 
   let zIndexScreen = alloc();
   let zIndexDialog = alloc();
+
   const screen = new Screen({
     target: document.body,
     props: {
@@ -19,6 +20,8 @@
       opacity: "0.4",
     },
   });
+
+  let fixed = true;
 
   onDestroy(() => {
     if (zIndexDialog != undefined) {
@@ -33,21 +36,67 @@
 
   onMount(() => {
     dialog.style.zIndex = zIndexDialog.toString();
-    // const v = document.documentElement;
   });
 
-  // function doMenuKey(event: KeyboardEvent): void {
-  //   if (event.key === "Escape") {
-  //     event.stopPropagation();
-  //     destroy();
-  //   }
-  // }
+  function doUnfix() {
+    dialog.style.position = "absolute";
+    fixed = false;
+  }
+
+  function doFix() {
+    dialog.style.position = "fixed";
+    fixed = true;
+  }
 </script>
 
-<div class="dialog" bind:this={dialog} data-cy="dialog" data-title={title}
-  style:width={styleWidth}>
+<div
+  class="dialog"
+  bind:this={dialog}
+  data-cy="dialog"
+  data-title={title}
+  style:width={styleWidth}
+>
   <div class="title" data-cy="dialog-title">
     <span>{title}</span>
+    <span class="spacer" />
+    {#if fixed}
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="#999"
+        width="16px"
+        height="16px"
+        data-cy="arrow-turn-down"
+        on:click={doUnfix}
+        >
+        <title>下の部分までスクロールできるようにします</title>
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M15 15l-6 6m0 0l-6-6m6 6V9a6 6 0 0112 0v3"
+        />
+      </svg>
+    {:else}
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="#999"
+        width="16px"
+        height="16px"
+        data-cy="arrow-turn-up"
+        on:click={doFix}
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M9 9l6-6m0 0l6 6m-6-6v12a6 6 0 01-12 0v-3"
+        />
+      </svg>
+    {/if}
     <svg
       on:click={destroy}
       class="closeIcon"
@@ -92,15 +141,19 @@
   .title {
     display: flex;
     align-items: center;
-    justify-content: space-between;
     margin-bottom: 10px;
+  }
+
+  .title .spacer {
+    flex-grow: 1;
   }
 
   .title span {
     font-weight: bold;
   }
 
-  .closeIcon {
+  .title svg {
     cursor: pointer;
+    margin-left: 6px;
   }
 </style>
