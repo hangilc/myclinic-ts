85ad9d9902f3122ce7b67af179cc0ffb57e1feaa
hangilc <hangil@chang.jp>
2023-05-05 22:28:38 +0900

    added check-orphan-*.sql

--- packages/node/check-orphan-kizai.sql
+++ packages/node/check-orphan-kizai.sql
@@ -1,9 +1,21 @@
-select k.kizaicode from visit_conduct_kizai as k inner join visit_conduct as c on k.visit_conduct_id = c.id
-    inner join visit as v on c.visit_id = v.visit_id 
-    left outer join tokuteikizai_master_arch as m on k.kizaicode = m.kizaicode
-    where m.valid_from <= date(v.v_datetime) 
-    and (m.valid_upto = '0000-00-00' or date(v.v_datetime) <= m.valid_upto)
-    and m.kizaicode is null
-    group by k.kizaicode;
-
-
+select
+    k.kizaicode
+from
+    visit_conduct_kizai as k
+where
+    not exists(
+        select
+            *
+        from
+            visit_conduct as c
+            inner join visit as v on c.visit_id = v.visit_id
+            inner join tokuteikizai_master_arch as m
+        where
+            k.visit_conduct_id = c.id
+            and m.kizaicode = k.kizaicode
+            and m.valid_from <= date(v.v_datetime)
+            and (
+                m.valid_upto = '0000-00-00'
+                or date(v.v_datetime) <= m.valid_upto
+            )
+    )
--- packages/node/check-orphan-shinryou.sql
+++ packages/node/check-orphan-shinryou.sql
@@ -1,8 +1,42 @@
-select s.shinryoucode from visit_shinryou as s inner join visit as v on s.visit_id = v.visit_id 
-    left outer join shinryoukoui_master_arch as m on s.shinryoucode = m.shinryoucode
-    where m.valid_from <= date(v.v_datetime) 
-    and (m.valid_upto = '0000-00-00' or date(v.v_datetime) <= m.valid_upto)
-    and m.shinryoucode is null
-    group by s.shinryoucode;
+select
+    s.shinryoucode
+from
+    visit_shinryou as s
+where
+    not exists(
+        select
+            *
+        from
+            visit as v
+            inner join shinryoukoui_master_arch as m
+        where
+            s.visit_id = v.visit_id
+            and s.shinryoucode = m.shinryoucode
+            and m.valid_from <= date(v.v_datetime)
+            and (
+                m.valid_upto = '0000-00-00'
+                or date(v.v_datetime) <= m.valid_upto
+            )
+    );
 
-
+select
+    s.shinryoucode
+from
+    visit_conduct_shinryou as s
+where
+    not exists(
+        select
+            *
+        from
+            visit_conduct as c
+            inner join visit as v on c.visit_id = v.visit_id
+            inner join shinryoukoui_master_arch as m
+        where
+            s.visit_conduct_id = c.id
+            and m.shinryoucode = s.shinryoucode
+            and m.valid_from <= date(v.v_datetime)
+            and (
+                m.valid_upto = '0000-00-00'
+                or date(v.v_datetime) <= m.valid_upto
+            )
+    )
