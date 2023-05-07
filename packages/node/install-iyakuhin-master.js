f15fa02cd3ea487a64a099535d7a69f4e09c0267
hangilc <hangil@chang.jp>
2023-05-08 08:34:06 +0900

    adding install-iyakuhin-master

--- packages/node/install-iyakuhin-master.js
+++ packages/node/install-iyakuhin-master.js
@@ -1,15 +1,15 @@
 // Usage: node install-iyakuhin-master.js Y.ZIP start-date
 
-// const AdmZip = require("adm-zip");
-// const iconv = require("iconv-lite");
-// const { parse } = require("csv-parse/sync");
-// const mysql = require("mysql");
-import AdmZip from "adm-zip";
 import path from "node:path";
+import AdmZip from "adm-zip";
 import iconv from "iconv-lite";
 import { parse } from "csv-parse/sync";
-import mysql from "mysql";
+import * as promptSync from "prompt-sync";
+let prompt = promptSync.default({sigint: true});
 
+/**
+ * @description convenience class to wrap CSV row
+ */
 class Row {
   constructor(csvRow) {
     this.csvRow = csvRow;
@@ -24,18 +24,13 @@ class Row {
   }
 }
 
-const HenkouKubunNoChange = 0;
-const HenkouKubunMasshou = 1;
-const HenkouKubunShinki = 3;
-const HenkouKubunHenkou = 5;
-const HenkouKubunHaishi = 9;
-
-const zipFile = process.argv[2];
-const startDate = process.argv[3];
+// parse command args
 if( process.argv.length !== 4 ){
   console.log("usage: Usage: node install-iyakuhin-master.js Y.ZIP start-date");
   process.exit(1);
 }
+const zipFile = process.argv[2];
+const startDate = process.argv[3];
 if( !zipFile ){
   console.log("Empty file name");
   process.exit(1);
@@ -45,45 +40,18 @@ if( !/^\d{4}-\d{2}-\d{2}$/.test(startDate) ){
   process.exit(1);
 }
 
-const connConfig = {
-  host: process.env["MYCLINIC_DB_HOST"] || "127.0.0.1",
-  user: process.env["MYCLINIC_DB_USER"],
-  password: process.env["MYCLINIC_DB_PASS"],
-  database: "myclinic",
-  charset: "utf8",
-};
+// read csv data
+let csvContent = await extractMasterContent(zipFile);
+let masters = parseCSVcontent(csvContent);
+askContinue(`There area ${masters.length} masters in ${zipFile}.`);
 
-extractMasterContent(zipFile, (err, content) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  } else if (content) {
-    const conn = mysql.createConnection(connConfig);
-    conn.connect();
-    conn.beginTransaction();
-    parse(content)
-      .map((row) => rowToEntry(new Row(row)))
-      .map(e => entryToMaster(e, startDate))
-      .forEach(m => {
-        enterIyakuhinMaster(conn, m);
-      });
-    conn.commit();
-    conn.end();
-  }
-});
 
 /**
- * @param {string} content - content of y.csv file
- */
-function handleContent(content) {
-  console.log("length", content.length);
-}
-
-/**
+ * @description reads CSV data
  * @param {string} fileName - file name of y.zip file
- * @param {(err: string | undefined, content: string | undefined) => void} cb - callback function
+ * @returns {Promise<string>} - content of master file
  */
-function extractMasterContent(fileName, cb) {
+async function extractMasterContent(fileName) {
   const name = path.parse(fileName).name;
   const csvFile = `${name}.csv`;
   const zip = new AdmZip(zipFile);
@@ -97,9 +65,18 @@ function extractMasterContent(fileName, cb) {
       content = iconv.decode(entry.getData(), "SHIFT_JIS");
     }
   });
-  cb(err, content);
+  if( err ){
+    return Promise.reject(err);
+  } else {
+    return Promise.resolve(content);
+  }
 }
 
+/**
+ * @description - convert Row to Entry data
+ * @parameter {Row}
+ * @returns {object}
+ */
 function rowToEntry(row) {
   return {
     kubun: row.getInt(1),
@@ -117,39 +94,8 @@ function rowToEntry(row) {
   };
 }
 
-// zip.getEntries().forEach((entry) => {
-//   if (entry.entryName === "y.csv") {
-//     zipEntry = entry;
-//   }
-// });
-// if (zipEntry === undefined) {
-//   console.error("Cannot find y.csv in zip");
-// } else {
-//   const csvContent = iconv.decode(zipEntry.getData(), "SHIFT_JIS");
-//   const csvRows = parse(csvContent);
-//   let entries = csvRows.map((csvRow) => rowToEntry(new Row(csvRow)));
-//   entries = entries.filter(
-//     (r) => r.kubun !== HenkouKubunHaishi && r.kubun !== HenkouKubunMasshou
-//   );
-//   entries = entries.filter((r) => r.yakka.length <= 10);
-//   const validFrom = dateToSqldate(new Date());
-//   const validUpto = "0000-00-00";
-//   masters = entries.map((entry) => entryToMaster(entry, validFrom, validUpto));
-//   const conn = mysql.createConnection({
-//     host: process.env["MYCLINIC_DB_HOST"] || "127.0.0.1",
-//     user: process.env["MYCLINIC_DB_USER"],
-//     password: process.env["MYCLINIC_DB_PASS"],
-//     database: "myclinic",
-//     charset: "utf8",
-//   });
-//   conn.connect();
-//   conn.beginTransaction();
-//   masters.forEach((master) => enter(conn, master));
-//   conn.commit();
-//   conn.end();
-// }
-
 /**
+ * @description converts Entry data to IyakuhinMaster
  * @param {object} entry
  * @param {string} validFrom - 開始日
  * @param {string} validUpto - 終了日
@@ -170,10 +116,24 @@ function entryToMaster(entry, validFrom, validUpto = "0000-00-00") {
   };
 }
 
-function dateToSqldate(date) {
-  return date.toISOString().substring(0, 10);
+/**
+ * @description parses CSV content to rows
+ * @param {string} content - CSV content
+ * @returns {object[]} -- array of IyakuhinMaster
+ */
+function parseCSVcontent(content) {
+  return parse(content)
+  .map((row) => rowToEntry(new Row(row)))
+  .map(e => entryToMaster(e, startDate));
 }
 
-function enterIyakuhinMaster(conn, master) {
-  conn.query("insert into iyakuhin_master_arch set ?", master);
+/**
+ * @description prompts message to stdout and asks for continuation
+ * @param {string} msg
+ */
+function askContinue(msg) {
+  let ans = prompt(msg + " Continue? (y/n): ");
+  if( ans === "n" ){
+    process.exit(1);
+  }
 }
