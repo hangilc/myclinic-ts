1864fb413ab9ee92819ef083df1e6fe4cb5bca20
hangilc <hangil@chang.jp>
2023-05-11 17:30:25 +0900

    temporary saving

--- packages/frontend/src/lib/onshi-hoken-consistent.ts
+++ packages/frontend/src/lib/onshi-hoken-consistent.ts
@@ -1,7 +1,66 @@
 import { Koukikourei, Shahokokuho, } from "myclinic-model";
+import type { OnshiResult } from "onshi-result";
 import type { ResultItem } from "onshi-result/ResultItem";
 import { isKoukikourei } from "./hoken-rep";
 import { toHankaku } from "./zenkaku";
+
+export namespace OnshiShahokokuhoInconsistency {
+  export class InconsistentHokenshaBangou {
+    readonly isInconsistentHokenshaBangou = true;
+
+    constructor(
+      public shahokokuhoValue: number,
+      public onshiValue: number,
+    ) {}
+
+    toString(): string {
+      return `保険者番号が一致しません。${shahokokuho.hokenshaBangou} - ${hokenshaBangou}`;
+    }
+  }
+}
+
+export type OnshiShahokokuhoConsistencyError = "string";
+
+
+
+export function checkOnshiShahokokuhoConsistency: OnshiShahokokuhoConsistencyErro | undefined (
+  result: OnshiResult,
+  shahokokuho: Shahokokuho,
+) {
+  const hokenshaBangou = parseInt(r.insurerNumber || "0");
+  if (shahokokuho.hokenshaBangou !== hokenshaBangou) {
+    return `保険者番号が一致しません。${shahokokuho.hokenshaBangou} - ${hokenshaBangou}`;
+  }
+  const hihokenshaKigou = r.insuredCardSymbol ?? "";
+  if (toHankaku(shahokokuho.hihokenshaKigou) !== toHankaku(hihokenshaKigou)) {
+    return `被保険者記号が一致しません。${shahokokuho.hihokenshaKigou} - ${hihokenshaKigou}`;
+  }
+  const hihokenshaBangou = r.insuredIdentificationNumber || "";
+  if (toHankaku(stripLeadingZero(shahokokuho.hihokenshaBangou)) !== toHankaku(stripLeadingZero(hihokenshaBangou))) {
+    return `被保険者番号が一致しません。${shahokokuho.hihokenshaBangou} - ${hihokenshaBangou}`;
+  }
+  const edaban = r.insuredBranchNumber || "";
+  if (shahokokuho.edaban !== "" && toHankaku(stripLeadingZero(shahokokuho.edaban)) !== toHankaku(stripLeadingZero(edaban))) {
+    return `枝番が一致しません。${shahokokuho.edaban} - ${edaban}`;
+  }
+  const kourei: number = r.kourei != undefined ? r.kourei.futanWari ?? 0 : 0;
+  if (shahokokuho.koureiStore !== kourei) {
+    return `高齢が一致しません。${shahokokuho.koureiStore} - ${kourei}`;
+  }
+  if (r.personalFamilyClassification != undefined) {
+    const honnin = r.personalFamilyClassification;
+    if (honnin === "本人") {
+      if (shahokokuho.honninStore === 0) {
+        return `本人・家族が一致しません。${shahokokuho.honninStore} - ${honnin}`;
+      }
+    } else {
+      if (shahokokuho.honninStore !== 0) {
+        return `本人・家族が一致しません。${shahokokuho.honninStore} - ${honnin}`;
+      }
+    }
+  }
+  return undefined;
+}
 
 export function shahokokuhoOnshiConsistent(
   shahokokuho: Shahokokuho,
