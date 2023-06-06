86ff7843b6008279fcbbbbd61b76325b2205eeec
hangilc <hangil@chang.jp>
2023-06-06 09:25:55 +0900

    temporary saving

--- packages/frontend/src/lib/rezept/futan-calc.ts
+++ packages/frontend/src/lib/rezept/futan-calc.ts
@@ -1,42 +1,104 @@
 import type { Kouhi, Koukikourei, Shahokokuho } from "myclinic-model";
+import type { LimitApplicationCertificateClassificationFlagLabel } from "onshi-result/codes";
 import { 負担区分コードRev, type 負担区分コードCode } from "./codes";
 import { futanWariOfHoken } from "./util";
+import { gendogakuOfKubun } from "@/lib/gendogaku";
+
+interface Cover {
+  kakari: number;
+  patientCharge: number;
+}
+
+interface HokenCover extends Cover {
+  futanWari: number;
+  gendogakuReaches?: true;
+}
+
+interface KouhiCover extends Cover {
+
+}
+
+interface KouhiProcessorArg {
+  kakari: number;
+  totalTen: number;
+  hokenFutanWari: number | undefined;
+}
+
+type KouhiProcessor = (arg: KouhiProcessorArg) => KouhiCover;
+
+function hibakushaNoKo({ kakari }: KouhiProcessorArg): KouhiCover {
+  return { kakari, patientCharge: 0 };
+}
+
+function taikiosenProcessor(gendogaku: number): KouhiProcessor {
+  return (arg: KouhiArg) => {
+
+  }
+}
 
 class Slot {
   totalTen: number;
-  hokenCharge: number = 0;
-  patientCharge: number = 0;
+  hokenCover: HokenCover | undefined;
+  kouhiCovers: Cover[] = [];
 
   constructor(totalTen: number) {
     this.totalTen = totalTen;
   }
+
+  setHokenCover(hokenCover: HokenCover): void {
+    this.hokenCover = hokenCover;
+  }
+
+  get patientCharge(): number {
+    if (!this.hokenCover && this.kouhiCovers.length === 0) {
+      throw new Error("No hoken, no kouhi");
+    }
+    return this.kouhiCovers.reduce((acc, ele) => acc + ele.patientCharge, this.hokenCover?.patientCharge ?? 0);
+  }
 }
 
-export class FutanCalc {
-  hoken: Shahokokuho | Koukikourei | undefined;
-  kouhiList: Kouhi[];
-  slots: Map<負担区分コードCode, Slot> = new Map();
-
-  constructor(hoken: Shahokokuho | Koukikourei | undefined, kouhiList: Kouhi[]) {
-    this.hoken = hoken;
-    this.kouhiList = kouhiList;
+function processHoken(
+  totalTen: number,
+  futanWari: number,
+  gendogaku: number | undefined,
+  accPatientCharge: number
+): HokenCover {
+  let patientCharge: number = totalTen * futanWari;
+  return {
+    kakari: totalTen * 10,
+    patientCharge,,
+    futanWari,
   }
+}
 
-  process(totalTens: Map<負担区分コードCode, number>): void {
-    const kubunList = sortFutanKubun(Array.from(totalTens.keys()));
-    for (let kubun of kubunList) {
-      const slot = new Slot(totalTens.get(kubun)!);
-      const [hoken, kouhiList] = kubunToHoken(kubun, this.hoken, this.kouhiList);
-      if( hoken ){
-        const futanWari = futanWariOfHoken(hoken);
-        slot.hokenCharge = slot.totalTen * futanWari;
-      } else {
-        slot.hokenCharge = slot.totalTen * 10;
-      }
-      slot.patientCharge = slot.hokenCharge;
-    }
+export type ShotokuKubun = LimitApplicationCertificateClassificationFlagLabel;
+export type TotalCover = Map<負担区分コードCode, [HokenCover, KouhiCover[]]>;
+
+export function calcFutanOne(
+  futanWari: number | undefined,
+  shotokuKubun: ShotokuKubun | undefined,
+  kouhiList: KouhiProcessor[],
+  totalTens: Map<負担区分コードCode, number>,
+  prevCover: TotalCover | undefined,
+): TotalCover {
+  const futanKubuns: 負担区分コードCode[] = sortFutanKubun(Array.from(totalTens.keys()));
+  throw new Error("Not implemented");
+}
+
+export function calcFutan(
+  futanWari: number | undefined,
+  shotokuKubun: ShotokuKubun | undefined,
+  kouhiList: KouhiProcessor[],
+  totalTensList: Map<負担区分コードCode, number>[], // in chronological order (new one is before old one)
+): TotalCover {
+  if( totalTensList.length === 0 ){
+    throw new Error("Empty total tens list");
   }
-
+  let totalCover: TotalCover = calcFutanOne(futanWari, shotokuKubun, kouhiList, totalTensList[0], undefined);
+  for(let i = 1; i < totalTensList.length; i++){
+    totalCover = calcFutanOne(futanWari, shotokuKubun, kouhiList, totalTensList[i], totalCover)
+  }
+  return totalCover;
 }
 
 function sortFutanKubun(kubuns: 負担区分コードCode[]): 負担区分コードCode[] {
@@ -44,18 +106,14 @@ function sortFutanKubun(kubuns: 負担区分コードCode[]): 負担区分コー
   return kubuns;
 }
 
-function kubunToHoken(kubun: 負担区分コードCode, hoken: Shahokokuho | Koukikourei | undefined, kouhiList: Kouhi[]):
-  [Shahokokuho | Koukikourei | undefined, Kouhi[]] {
-  const ks: Kouhi[] = [];
+function kubunToKouhiList(kubun: 負担区分コードCode, kouhiList: KouhiProcessor[]):
+  KouhiProcessor[] {
+  const ks: KouhiProcessor[] = [];
   kubun.split("").forEach((k) => {
     if (k !== "H") {
       const i = parseInt(k);
       ks.push(kouhiList[i - 1]);
     }
-  })
-  const name = 負担区分コードRev.get(kubun)!;
-  return [
-    name.startsWith("H") ? hoken : undefined,
-    ks
-  ];
+  });
+  return ks;
 }
