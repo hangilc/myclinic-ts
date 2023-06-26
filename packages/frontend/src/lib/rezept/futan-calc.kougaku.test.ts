ffaec28919606533871d389a6044c92952e3da7a
hangilc <hangil@chang.jp>
2023-06-26 16:48:09 +0900

    adjusted futan-calc tests

--- packages/frontend/src/lib/rezept/futan-calc.kougaku.test.ts
+++ packages/frontend/src/lib/rezept/futan-calc.kougaku.test.ts
@@ -47,229 +47,229 @@ describe("高額療養費（70歳未満）", () => {
   });
 
   it("事例5　本人入院（標準報酬月額26万円以下）", () => {
-    const covers = calcFutan(3, "エ", [], [
+    const covers = calcFutan(3, "エ", [],
       mkTen("H", 21635),
-    ], {});
+      {});
     expect(patientChargeOf(covers)).equal(57600);
   });
 
   it("事例6　本人入院（低所得者）", () => {
-    const covers = calcFutan(3, "オ", [], [
+    const covers = calcFutan(3, "オ", [],
       mkTen("H", 18795),
-    ], {});
+      {});
     expect(patientChargeOf(covers)).equal(35400);
   });
 
   it("事例7　本人入院（標準報酬月額83万円以上）（多数回該当）", () => {
-    const covers = calcFutan(3, "ア", [], [
+    const covers = calcFutan(3, "ア", [],
       mkTen("H", 94576),
-    ], { gendogakuTasuuGaitou: true });
+      { gendogakuTasuuGaitou: true });
     expect(patientChargeOf(covers)).equal(140100);
   });
 
   it("事例8　本人入院（標準報酬月額53万～79万円）（多数回該当）", () => {
-    const covers = calcFutan(3, "イ", [], [
+    const covers = calcFutan(3, "イ", [],
       mkTen("H", 72641),
-    ], { gendogakuTasuuGaitou: true });
+      { gendogakuTasuuGaitou: true });
     expect(patientChargeOf(covers)).equal(93000);
   });
 
   it("事例9　本人入院（標準報酬月額28万～50万円）（多数回該当）", () => {
-    const covers = calcFutan(3, "ウ", [], [
+    const covers = calcFutan(3, "ウ", [],
       mkTen("H", 36452),
-    ], { gendogakuTasuuGaitou: true });
+      { gendogakuTasuuGaitou: true });
     expect(patientChargeOf(covers)).equal(44400);
   });
 
   it("事例10　本人入院（標準報酬月額26万円以下）（多数回該当）", () => {
-    const covers = calcFutan(3, "エ", [], [
+    const covers = calcFutan(3, "エ", [],
       mkTen("H", 21635),
-    ], { gendogakuTasuuGaitou: true });
+      { gendogakuTasuuGaitou: true });
     expect(patientChargeOf(covers)).equal(44400);
   });
 
   it("事例11　本人入院（低所得者）（多数回該当）", () => {
-    const covers = calcFutan(3, "オ", [], [
+    const covers = calcFutan(3, "オ", [],
       mkTen("H", 18795),
-    ], { gendogakuTasuuGaitou: true });
+      { gendogakuTasuuGaitou: true });
     expect(patientChargeOf(covers)).equal(24600);
   });
 
   it("事例12　本人入院（長）", () => {
-    const covers = calcFutan(3, "ウ", [], [
+    const covers = calcFutan(3, "ウ", [],
       mkTen("H", 17500),
-    ], { marucho: 10000 });
+      { marucho: 10000 });
     expect(patientChargeOf(covers)).equal(10000);
   });
 
   it("事例13　本人入院（標準報酬月額83万円以上）・公費（結核患者の適正医療）", () => {
-    const covers = calcFutan(3, "ア", [KouhiKekkaku], [
+    const covers = calcFutan(3, "ア", [KouhiKekkaku],
       mkTens(
         ["H1", 6000], ["H", 9000]
-      )
-    ]);
+      ),
+    );
     expect(patientChargeOf(covers)).equal(30000);
   });
 
   it("事例14　本人入院（標準報酬月額83万円以上）・公費（結核患者の適正医療）", () => {
-    const covers = calcFutan(3, "ア", [KouhiKekkaku], [
+    const covers = calcFutan(3, "ア", [KouhiKekkaku],
       mkTens(
         ["H1", 85000], ["H", 5000]
-      )
-    ], { debug: false });
+      ),
+      { debug: false });
     expect(patientChargeOf(covers)).equal(57500);
   });
 
   it("事例15　本人入院（標準報酬月額83万円以上）・公費（結核患者の適正医療）", () => {
-    const covers = calcFutan(3, "ア", [KouhiKekkaku], [
+    const covers = calcFutan(3, "ア", [KouhiKekkaku],
       mkTens(
         ["H1", 5000], ["H", 95000]
-      )
-    ], { debug: false });
+      ),
+      { debug: false });
     expect(patientChargeOf(covers)).equal(256180);
   });
 
   it("事例16　本人入院（標準報酬月額83万円以上）・公費（結核患者の適正医療）", () => {
-    const covers = calcFutan(3, "ア", [KouhiKekkaku], [
+    const covers = calcFutan(3, "ア", [KouhiKekkaku],
       mkTens(
         ["H1", 10000], ["H", 110000]
-      )
-    ], { debug: false });
+      ),
+      { debug: false });
     expect(patientChargeOf(covers)).equal(256180);
   });
 
   it("事例17　本人入院（標準報酬月額83万円以上）・公費（結核患者の適正医療）", () => {
-    const covers = calcFutan(3, "ア", [KouhiKekkaku], [
+    const covers = calcFutan(3, "ア", [KouhiKekkaku],
       mkTens(
         ["H1", 85000], ["H", 95000]
-      )
-    ], { debug: false });
+      ),
+      { debug: false });
     expect(patientChargeOf(covers)).equal(262180);
   });
 
   it("事例18　本人入院外（標準報酬月額53万～79万円）・公費（結核患者の適正医療）", () => {
-    const covers = calcFutan(3, "イ", [KouhiKekkaku], [
+    const covers = calcFutan(3, "イ", [KouhiKekkaku],
       mkTens(
         ["H1", 85000], ["H", 5000]
-      )
-    ], { debug: false });
+      ),
+      { debug: false });
     expect(patientChargeOf(covers)).equal(57500);
   });
 
   it("事例19　本人入院外（標準報酬月額53万～79万円）・公費（結核患者の適正医療）", () => {
-    const covers = calcFutan(3, "イ", [KouhiKekkaku], [
+    const covers = calcFutan(3, "イ", [KouhiKekkaku],
       mkTens(
         ["H1", 5000], ["H", 95000]
-      )
-    ], { debug: false });
+      ),
+      { debug: false });
     expect(patientChargeOf(covers)).equal(173820);
   });
 
   it("事例20　本人入院外（標準報酬月額53万～79万円）・公費（結核患者の適正医療）", () => {
-    const covers = calcFutan(3, "イ", [KouhiKekkaku], [
+    const covers = calcFutan(3, "イ", [KouhiKekkaku],
       mkTens(
         ["H1", 10000], ["H", 110000]
-      )
-    ], { debug: false });
+      ),
+      { debug: false });
     expect(patientChargeOf(covers)).equal(173820);
   });
 
   it("事例21　本人入院外（標準報酬月額53万～79万円）・公費（結核患者の適正医療）", () => {
-    const covers = calcFutan(3, "イ", [KouhiKekkaku], [
+    const covers = calcFutan(3, "イ", [KouhiKekkaku],
       mkTens(
         ["H1", 85000], ["H", 95000]
-      )
-    ], { debug: false });
+      ),
+      { debug: false });
     expect(patientChargeOf(covers)).equal(179820);
   });
 
   it("事例22　本人入院（標準報酬月額28万円～50万円）・公費（更生医療）", () => {
-    const covers = calcFutan(3, "ウ", [KouhiKouseiIryou], [
+    const covers = calcFutan(3, "ウ", [KouhiKouseiIryou],
       mkTens(
         ["H1", 20000], ["H", 40000]
-      )
-    ], { gendogaku: { kingaku: 20000, kouhiBangou: 1 }, debug: false });
+      ),
+      { gendogaku: { kingaku: 20000, kouhiBangou: 1 }, debug: false });
     expect(patientChargeOf(covers)).equal(83430);
   });
 
   it("事例23　本人入院（標準報酬月額28万円～50万円）・公費（更生医療）", () => {
-    const covers = calcFutan(3, "ウ", [KouhiKouseiIryou], [
+    const covers = calcFutan(3, "ウ", [KouhiKouseiIryou],
       mkTens(
         ["H1", 30000], ["H", 30000]
-      )
-    ], { gendogaku: { kingaku: 30000, kouhiBangou: 1 }, debug: false });
+      ),
+      { gendogaku: { kingaku: 30000, kouhiBangou: 1 }, debug: false });
     expect(patientChargeOf(covers)).equal(83430);
   });
 
   it("事例24　本人入院（標準報酬月額26万円以下）・公費（更生医療）", () => {
-    const covers = calcFutan(3, "エ", [KouhiKouseiIryou], [
+    const covers = calcFutan(3, "エ", [KouhiKouseiIryou],
       mkTens(
         ["H1", 30000]
-      )
-    ], { gendogaku: { kingaku: 5000, kouhiBangou: 1 }, debug: false });
+      ),
+      { gendogaku: { kingaku: 5000, kouhiBangou: 1 }, debug: false });
     expect(patientChargeOf(covers)).equal(5000);
   });
 
   it("事例25　本人入院（標準報酬月額26万円以下）・公費（更生医療）", () => {
-    const covers = calcFutan(3, "エ", [KouhiKouseiIryou], [
+    const covers = calcFutan(3, "エ", [KouhiKouseiIryou],
       mkTens(
         ["H1", 10000], ["H", 20000]
-      )
-    ], { gendogaku: { kingaku: 5000, kouhiBangou: 1 }, debug: false });
+      ),
+      { gendogaku: { kingaku: 5000, kouhiBangou: 1 }, debug: false });
     expect(patientChargeOf(covers)).equal(57600);
   });
 
   it("事例26　本人入院（標準報酬月額26万円以下）・公費（更生医療）", () => {
-    const covers = calcFutan(3, "エ", [KouhiKouseiIryou], [
+    const covers = calcFutan(3, "エ", [KouhiKouseiIryou],
       mkTens(
         ["H1", 30000], ["H", 30000]
-      )
-    ], { gendogaku: { kingaku: 5000, kouhiBangou: 1 }, debug: false });
+      ),
+      { gendogaku: { kingaku: 5000, kouhiBangou: 1 }, debug: false });
     expect(patientChargeOf(covers)).equal(57600);
   });
 
   it("事例27　本人入院（低所得者）・公費（更生医療）", () => {
-    const covers = calcFutan(3, "オ", [KouhiKouseiIryou], [
+    const covers = calcFutan(3, "オ", [KouhiKouseiIryou],
       mkTens(
         ["H1", 10000], ["H", 20000]
-      )
-    ], { gendogaku: { kingaku: 0, kouhiBangou: 1 }, debug: false });
+      ),
+      { gendogaku: { kingaku: 0, kouhiBangou: 1 }, debug: false });
     expect(patientChargeOf(covers)).equal(35400);
   });
 
   it("事例28　本人入院（低所得者）・公費（更生医療）", () => {
-    const covers = calcFutan(3, "オ", [KouhiKouseiIryou], [
+    const covers = calcFutan(3, "オ", [KouhiKouseiIryou],
       mkTens(
         ["H1", 30000], ["H", 10000]
-      )
-    ], { gendogaku: { kingaku: 0, kouhiBangou: 1 }, debug: false });
+      ),
+      { gendogaku: { kingaku: 0, kouhiBangou: 1 }, debug: false });
     expect(patientChargeOf(covers)).equal(30000);
   });
 
   it("事例29　本人入院（低所得者）・公費（更生医療）", () => {
-    const covers = calcFutan(3, "オ", [KouhiKouseiIryou], [
+    const covers = calcFutan(3, "オ", [KouhiKouseiIryou],
       mkTens(
         ["H1", 30000], ["H", 30000]
-      )
-    ], { gendogaku: { kingaku: 0, kouhiBangou: 1 }, debug: false });
+      ),
+      { gendogaku: { kingaku: 0, kouhiBangou: 1 }, debug: false });
     expect(patientChargeOf(covers)).equal(35400);
   });
 
   it("事例30　本人入院（低所得者）・公費（更生医療）", () => {
-    const covers = calcFutan(3, "オ", [KouhiKouseiIryou], [
+    const covers = calcFutan(3, "オ", [KouhiKouseiIryou],
       mkTens(
         ["H1", 10000], ["H", 20000]
-      )
-    ], { gendogaku: { kingaku: 5000, kouhiBangou: 1 }, debug: false });
+      ),
+      { gendogaku: { kingaku: 5000, kouhiBangou: 1 }, debug: false });
     expect(patientChargeOf(covers)).equal(35400);
   });
 
   it("事例31　本人入院（低所得者）・公費（更生医療）", () => {
-    const covers = calcFutan(3, "オ", [KouhiKouseiIryou], [
+    const covers = calcFutan(3, "オ", [KouhiKouseiIryou],
       mkTens(
         ["H1", 30000], ["H", 30000]
-      )
-    ], { gendogaku: { kingaku: 5000, kouhiBangou: 1 }, debug: false });
+      ),
+      { gendogaku: { kingaku: 5000, kouhiBangou: 1 }, debug: false });
     expect(patientChargeOf(covers)).equal(35400);
   });
 
