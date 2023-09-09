f820416e4a0ba743ef0ee9fa1c82b3bfb3f94138
hangilc <hangil@chang.jp>
2023-09-09 09:10:11 +0900

    added generic1 check

--- packages/frontend/src/practice/RcptCheck.svelte
+++ packages/frontend/src/practice/RcptCheck.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
   import ServiceHeader from "@/ServiceHeader.svelte";
+  import api from "@/lib/api";
   import { pad } from "@/lib/pad";
   import { cvtVisitsToUnit, loadVisits } from "@/lib/rezept-adapter";
-  import type { Visit } from "myclinic-model";
+  import type { Visit, VisitEx } from "myclinic-model";
+  import { checkForRcpt, type CheckError } from "./rcpt-check/check";
 
   export let isVisible = false;
   let shinryouYearMonth: string = defaultShinryouYearMonth();
   let patientTotal = 0;
   let current = 0;
+  let serial = 1;
+  let errors: [number, CheckError][] = [];
 
   function defaultShinryouYearMonth(): string {
     let now = new Date();
@@ -35,9 +39,19 @@
     const visitsList: Visit[][] = [...visitMap.shaho, ...visitMap.kokuho];
     current = 0;
     patientTotal = visitsList.length;
+    errors = [];
+    serial = 1;
     for(const visits of visitsList) {
+      if( visits.length === 0 ){
+        continue;
+      }
       current += 1;
-      const u = await cvtVisitsToUnit(visits);
+      let patientVisits: VisitEx[] = await Promise.all(visits.map(async visit => await api.getVisitEx(visit.visitId)));
+      const errs = checkForRcpt(patientVisits);
+      if( errs !== "ok" && errs !== "no-visit" ){
+        const es: [number, CheckError][] = errs.map(e => [serial++, e]);
+        errors = [...errors, ...es];
+      }
     }
   }
 </script>
@@ -54,6 +68,17 @@
     {#if patientTotal > 0}
       {current} / {patientTotal}
     {/if}
+  </div>
+  <div>
+    {#each errors as error (error[0])}
+      {@const checkError = error[1]}
+      {@const code = checkError[0]}
+      {@const visit = checkError[1]}
+      {@const patient = visit.patient}
+      <div>
+        {code}: ({patient.patientId}) {patient.fullName()}
+      </div>
+    {/each}
   </div>
 </div>
 
--- packages/frontend/src/practice/rcpt-check/check.ts
+++ packages/frontend/src/practice/rcpt-check/check.ts
@@ -1,9 +1,36 @@
-import type { RezeptUnit } from "myclinic-rezept";
+import { isShohousen, parseShohousen } from "@/lib/shohousen/parse-shohousen";
+import type { VisitEx } from "myclinic-model";
 
-export function check(unit: RezeptUnit) {
-  checkGeneric1(unit);
+export type CheckError = ["一般名処方加算１（１品目）", VisitEx];
+export type CheckResult = "ok" | "no-visit"　| CheckError[];
+
+export function checkForRcpt(visits: VisitEx[]): CheckResult {
+  if( visits.length === 0 ){
+    return "no-visit";
+  }
+  const errs: CheckError[] = [];
+  function chk(err: CheckError | undefined) {
+    if( err ){
+      errs.push(err);
+    }
+  }
+  visits.forEach(visit => chk(checkGeneric1(visit)));
+  return errs.length === 0 ? "ok" : errs;
 }
 
-function checkGeneric1(unit: RezeptUnit) {
-  
+// 一般名処方加算１は２品目以上の薬剤が処方されている場合に適応される。
+// 120004270: 一般名処方加算１（処方箋料）
+function checkGeneric1(visit: VisitEx): ["一般名処方加算１（１品目）", VisitEx] | undefined {
+  const idx = visit.shinryouList.findIndex(s => s.shinryoucode === 120004270);
+  if( idx >= 0 ){
+    let texts = visit.texts.filter(t => isShohousen(t.content));
+    if( texts.length === 1 ){
+      const shohousen = parseShohousen(texts[0].content);
+      console.log("shohousen", shohousen);
+      if( shohousen.totalDrugs === 1 ){
+        return ["一般名処方加算１（１品目）", visit];
+      }
+    }
+  }
+  return undefined;
 }
