66915f8b6eb5e48a47f7283e7bee5df07480b6b9
hangilc <hangil@chang.jp>
2023-12-02 13:58:56 +0900

    added deploy

---
+++ scripts/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+STAMP=$(date '+%Y-%m%d-%H%M%S')
+echo $STAMP
+
+npm run build -w packages/frontend
+ssh changclinic-server cp -r  vite vite-$STAMP
+rsync -avg packages/frontend/dist/ changclinic-server:~/vite/
