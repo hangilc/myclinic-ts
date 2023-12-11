bd42f7d242df6b60cffec29ec1cc2d92da68dfc5
hangilc <hangil@chang.jp>
2023-12-12 08:49:04 +0900

    modified deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,7 @@
 STAMP=$(date '+%Y-%m%d-%H%M%S')
 echo $STAMP
 
+npm run compile
 npm run build -w packages/frontend
 ssh changclinic-server cp -r  vite vite-$STAMP
 rsync -avg packages/frontend/dist/ changclinic-server:~/vite/
