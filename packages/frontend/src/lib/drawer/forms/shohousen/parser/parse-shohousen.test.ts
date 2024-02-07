20791f06878741b60e015880ed8cd8de4f3f7264
hangilc <hangil@chang.jp>
2024-02-07 23:26:41 +0900

    temporary saving

--- packages/frontend/src/lib/drawer/forms/shohousen/parser/parse-shohousen.test.ts
+++ packages/frontend/src/lib/drawer/forms/shohousen/parser/parse-shohousen.test.ts
@@ -1,7 +1,11 @@
-import { parseShohousen } from "./parse-shohousen"
+import { extractChunks } from "./parse-shohousen"
 
 describe("shohousen parser", () => {
-  it("should parse naifuku", () => {
-    parseShohousen("");
+  it("should extract chunks", () => {
+    const chunks = extractChunks("Ｒｐ）\n１）カロナール錠５００ｍｇ　３錠\n　分３　毎食後　５日分\n@memo:オンライン対応");
+    console.log(chunks);
+    expect(chunks.length).equal(2);
+    expect(chunks[0]).equal("１）カロナール錠５００ｍｇ　３錠\n　分３　毎食後　５日分\n");
+    expect(chunks[1]).equal("@memo:オンライン対応");
   })
 })
--- packages/frontend/src/lib/drawer/forms/shohousen/parser/parse-shohousen.ts
+++ packages/frontend/src/lib/drawer/forms/shohousen/parser/parse-shohousen.ts
@@ -1,3 +1,31 @@
+let reStart = /^([ ＿]*[0-9０-９]+[)）])|@/mg;
+
 export function parseShohousen(content: string) {
-  
+  const starts = extractByStarter(reStart, content);
+
+}
+
+
+export function extractChunks(content: string): string[] {
+  return extractByStarter(reStart, content);
+
+}
+
+function extractByStarter(re: RegExp, text: string): string[] {
+  const ms = text.matchAll(re);
+  const starts: number[] = [];
+  for (let m of ms) {
+    if (m.index !== undefined) {
+      starts.push(m.index);
+    }
+  }
+  const result: string[] = [];
+  starts.push(text.length);
+  let start = starts[0];
+  for(let j=1;j<starts.length;j++){
+    const end = starts[j];
+    result.push(text.substring(start, end));
+    start = end;
+  }
+  return result;
 }
