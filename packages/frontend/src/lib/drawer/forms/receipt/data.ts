7683a4d2c1c86ea0fb568dceb84b97ee80ab7582
hangilc <hangil@chang.jp>
2024-02-15 13:39:01 +0900

    temporary saving

--- packages/frontend/src/lib/drawer/forms/receipt/data.ts
+++ packages/frontend/src/lib/drawer/forms/receipt/data.ts
@@ -7,15 +7,86 @@ import { stringDrawWidth } from "../../compiler/char-width";
 
 export function drawData(ctx: DrawerContext, data: ReceiptDrawerData) {
   renderPatient(ctx, data.patientName ?? "", c.getMark(ctx, "nameBox"));
+  renderKingaku(ctx, data.charge ?? 0, c.getMark(ctx, "kingakuBox"));
+  renderVisitDate(ctx, data.visitDate ?? "", c.getMark(ctx, "dateBox"));
+  renderIssueDate(ctx, data.issueDate ?? "", c.getMark(ctx, "issueBox"));
+  renderDetail(ctx, data.patientId ?? "", c.getMark(ctx, "patientIdBox"));
+  renderDetail(ctx, data.hoken ?? "", c.getMark(ctx, "hokenBox"));
+  renderDetail(ctx, formatFutanWari(data.futanWari ?? ""), c.getMark(ctx, "futanWariBox"));
+  renderDetail(ctx, data.shoshin ?? "", c.getMark(ctx, "shoshinBox"));
+  renderDetail(ctx, data.kanri ?? "", c.getMark(ctx, "kanriBox"));
+  renderDetail(ctx, data.zaitaku ?? "", c.getMark(ctx, "zaitakuBox"));
+  renderDetail(ctx, data.kensa ?? "", c.getMark(ctx, "kensaBox"));
+  renderDetail(ctx, data.gazou ?? "", c.getMark(ctx, "gazouBox"));
+  renderDetail(ctx, data.touyaku ?? "", c.getMark(ctx, "touyakuBox"));
+  renderDetail(ctx, data.chuusha ?? "", c.getMark(ctx, "chuushaBox"));
+  renderDetail(ctx, data.shochi ?? "", c.getMark(ctx, "shochiBox"));
+  renderDetail(ctx, data.sonota ?? "", c.getMark(ctx, "sonotaBox"));
+  renderDetail(ctx, data.souten ?? "", c.getMark(ctx, "soutenBox"));
+  renderHokengai(ctx, data.hokengai ?? [], c.getMark(ctx, ""));
   renderInstitute(ctx, data.clinicName ?? "", data.addressLines ?? [], c.getMark(ctx, "instituteBox"));
+}
+
+function formatFutanWari(futanWari: string) {
+  if( futanWari ){
+    futanWari = futanWari.replace(/割$/, "");
+    futanWari = futanWari + "割";
+  }
+  return futanWari
 }
 
 function renderPatient(ctx: DrawerContext, name: string, box: Box) {
   c.setFont(ctx, "mincho-6");
   let fontSize = c.currentFontSize(ctx);
-  if( stringDrawWidth(name, fontSize) <= b.width(box) ) {
+  if (stringDrawWidth(name, fontSize) <= b.width(box)) {
     c.drawText(ctx, name, box, "center", "bottom");
   }
+}
+
+function renderKingaku(ctx: DrawerContext, kingaku: number, box: Box) {
+  if (kingaku > 0) {
+    c.setFont(ctx, "gothic-5");
+    const s = kingaku.toLocaleString();
+    c.drawText(ctx, s, box, "right", "bottom");
+  }
+}
+
+function renderDate(ctx: DrawerContext, date: string, box: Box) {
+  const m = date.match(/^(\d{4})-(\d{2})-(\d{2})/);
+  if( m ){
+    const year = m[1];
+    const month = m[2].replace(/^0/, "");
+    const day = m[3].replace(/^0/, "");
+    const s = `${year}年${month}月${day}日`;
+    c.setFont(ctx, "mincho-4");
+    c.drawText(ctx, s, box, "left", "center");
+  }
+}
+
+function renderVisitDate(ctx: DrawerContext, date: string, box: Box) {
+  renderDate(ctx, date, box);
+}
+
+function renderIssueDate(ctx: DrawerContext, date: string, box: Box) {
+  renderDate(ctx, date, box);
+}
+
+function renderDetail(ctx: DrawerContext, text: string, box: Box) {
+  c.setFont(ctx, "mincho-4");
+  c.drawText(ctx, text, box, "center", "center");
+}
+
+function renderHokengai(ctx: DrawerContext, hokengai: string[], box: Box ){
+  box = b.modify(box, b.shrinkHoriz(1, 0));
+  const rows = b.splitToRows(box, b.evenSplitter(4));
+  c.setFont(ctx, "mincho-4");
+  for(let i=0;i<hokengai.length;i++){
+    const str = hokengai[i];
+    if( i < rows.length ){
+      c.drawText(ctx, str, rows[i], "left", "center");
+    }
+  }
+
 }
 
 function renderInstitute(ctx: DrawerContext, name: string, addressLines: string[], box: Box) {
