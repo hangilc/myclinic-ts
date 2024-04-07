b6680cbe01b9cd793eea2440a395d9171ec209c1
hangilc <hangil@chang.jp>
2024-04-07 22:45:20 +0900

    adding futan calc

--- packages/rezept/futan/calc.test.ts
+++ packages/rezept/futan/calc.test.ts
@@ -1,44 +1,59 @@
-import { HokenProvider, Nanbyou, Payer, calcFutan, jikoFutan } from "./calc";
+import { calcJikofutan, calcPayments, mkHokenPayer, mkNanbyouPayer } from "./calc";
 
 describe("futan/calc", () => {
   it("should calc jiko futan", () => {
-    const futan = jikoFutan(2000, [{ amount: 1400 }]);
+    const futan = calcJikofutan(2000, [{ payment: 1400 }]);
     expect(futan).toBe(600);
   });
 
   it("should calc basic case", () => {
-    const shaho = new HokenProvider(3);
+    const shaho = mkHokenPayer(3);
     const bill = 2000;
-    const instance = calcFutan(bill, [shaho.asPayer()], []);
-    expect(instance.payments).toMatchObject([{ amount: 1400 }]);
-    expect(jikoFutan(bill, instance.payments)).toBe(600);
+    calcPayments(bill, [shaho]);
+    expect(shaho).toMatchObject({ payment: 1400 });
+    expect(calcJikofutan(bill, [shaho])).toBe(600);
   });
 
   it("should handle nanbyou with futanWari 3", () => {
-    const hoken = new HokenProvider(3);
-    const nanbyou = new Nanbyou(5000);
+    const hoken = mkHokenPayer(3);
+    const nanbyou = mkNanbyouPayer(5000);
     const bill = 2000;
-    const instance = calcFutan(bill, [hoken.asPayer(), nanbyou.asPayer()], []);
-    console.log(instance);
-    expect(jikoFutan(bill, instance.payments)).toBe(400);
+    calcPayments(bill, [hoken, nanbyou]);
+    expect(hoken).toMatchObject({ payment: 1400 });
+    expect(nanbyou).toMatchObject({ payment: 200 });
+    expect(calcJikofutan(bill, [hoken, nanbyou])).toBe(400);
   });
 
   it("should handle nanbyou with futanWari 2", () => {
-    const hoken = new HokenProvider(2);
-    const nanbyou = new Nanbyou(5000);
+    const hoken = mkHokenPayer(2);
+    const nanbyou = mkNanbyouPayer(5000);
     const bill = 2000;
-    const instance = calcFutan(bill, [hoken.asPayer(), nanbyou.asPayer()], []);
-    console.log(instance);
-    expect(jikoFutan(bill, instance.payments)).toBe(400);
+    calcPayments(bill, [hoken, nanbyou]);
+    expect(hoken).toMatchObject({ payment: 1600 });
+    expect(nanbyou).toMatchObject({ payment: 0 });
+    expect(calcJikofutan(bill, [hoken, nanbyou])).toBe(400);820
   });
 
   it("should handle nanbyou with futanWari 1", () => {
-    const hoken = new HokenProvider(1);
-    const nanbyou = new Nanbyou(5000);
+    const hoken = mkHokenPayer(1);
+    const nanbyou = mkNanbyouPayer(5000);
     const bill = 2000;
-    const instance = calcFutan(bill, [hoken.asPayer(), nanbyou.asPayer()], []);
-    console.log(instance);
-    expect(jikoFutan(bill, instance.payments)).toBe(200);
+    calcPayments(bill, [hoken, nanbyou]);
+    expect(hoken).toMatchObject({ payment: 1800 });
+    expect(nanbyou).toMatchObject({ payment: 0 });
+    expect(calcJikofutan(bill, [hoken, nanbyou])).toBe(200);
+  });
+
+  // 難病計算例７０歳未満.pdf
+
+  it("should handle nanbyou 18", () => {
+    const hoken = mkHokenPayer(3, 257180);
+    const nanbyou = mkNanbyouPayer(30000);
+    const bill = 1300000;
+    calcPayments(bill, [hoken, nanbyou]);
+    expect(hoken).toMatchObject({ payment: 1042820 });
+    expect(nanbyou).toMatchObject({ payment: 227180 });
+    expect(calcJikofutan(bill, [hoken, nanbyou])).toBe(30000);
   });
 });
 
--- packages/rezept/futan/calc.ts
+++ packages/rezept/futan/calc.ts
@@ -1,87 +1,92 @@
-export interface PaymentContext {
+
+interface PaymentContext {
   totalBill: number;
-  accumJikoFutan: number;
 }
 
-export interface Payer {
-  calcFutan(bill: number, ctx: PaymentContext): number;
+interface Payer {
+  calc(bill: number, ctx: PaymentContext): number;
+  payment: number;
 }
 
-export interface Payment {
-  payer: Payer;
-  amount: number;
+export function calcPayments(bill: number, payers: Payer[]) {
+  const ctx = { totalBill: bill };
+  payers.forEach(p => {
+    const payment = p.calc(bill, ctx);
+    p.payment = payment;
+    bill -= p.payment;
+  });
 }
 
-export interface Instance {
-  totalBill: number;
-  payments: Payment[];
-}
-
-export function calcFutan(totalBill: number, payers: Payer[], prevs: Instance[]): Instance {
-  const payments: Payment[] = [];
-  const accumJikoFutan: number = prevs.reduce((acc, ele) => {
-    return acc + jikoFutan(ele.totalBill, ele.payments);
-  }, 0);
-  const ctx: PaymentContext = {
-    totalBill,
-    accumJikoFutan,
-  }
-  let bill = totalBill;
-  payers.forEach(payer => {
-    const amount = payer.calcFutan(bill, ctx);
-    payments.push({
-      payer,
-      amount,
-    });
-    bill -= amount;
-  })
-  return { totalBill, payments }
-}
-
-export function jikoFutan(totalBill: number, futanList: { amount: number }[]): number {
-  let bill = totalBill;
-  futanList.forEach(f => bill -= f.amount);
+export function calcJikofutan(bill: number, payers: { payment: number }[]): number {
+  payers.forEach(p => bill -= p.payment);
   return bill;
 }
 
-export class HokenProvider {
-  futanWari: number;
-
-  constructor(futanWari: number){
-    this.futanWari = futanWari;
-  }
-
-  asPayer(): Payer {
-    const futanWari = this.futanWari;
-    return {
-      calcFutan(bill: number): number {
-        return bill * (1 - futanWari/10.0);
-      }
-    }
-  }
-}
-
-export class Nanbyou {
-  gendo: number;
-
-  constructor(gendo: number) {
-    this.gendo = gendo;
-  }
-
-  overrideGendo(newGendo: number): Nanbyou {
-    return new Nanbyou(newGendo);
-  }
-
-  asPayer(): Payer {
-    return {
-      calcFutan(bill: number, ctx: PaymentContext): number {
-        let futan = 0;
-        if( bill > ctx.totalBill * 0.2 ){
-          futan = bill - ctx.totalBill * 0.2;
+export function mkHokenPayer(futanWari: number, gendogaku?: number): Payer {
+  return {
+    calc(bill: number) {
+      let jikofutan = bill * futanWari / 10.0;
+      if( gendogaku != undefined ){
+        if( jikofutan > gendogaku ){
+          jikofutan = gendogaku;
         }
-        return futan;
       }
-    }
+      return bill - jikofutan;
+    },
+    payment: 0,
   }
 }
 
+export function mkNanbyouPayer(gendogaku: number): Payer {
+  return {
+    calc(bill: number, ctx: PaymentContext) {
+      let jikofutan = bill;
+      if( jikofutan > ctx.totalBill * 0.2 ){
+        jikofutan = ctx.totalBill * 0.2;
+      }
+      if( jikofutan > gendogaku ){
+        jikofutan = gendogaku;
+      }
+      return bill - jikofutan;
+    },
+    payment: 0,
+  }
+}
+
+// 公費
+// 高校生 (patient 7358)
+// 被爆者 (5366)
+// マル都 (7170)
+// 心身障害 (3288)
+// 難病 (7300)
+// 心身障害 (1753)
+
+// 2022-01-01 以降
+// +----------+
+// | futansha |
+// +----------+
+// | 54136015 |難病
+// | 80137151 |重度障がい者医療
+// | 82134008 |被爆者の子に対する医療
+// | 82137530 |マル都（大気汚染）
+// | 89135156 |高校生
+// +----------+
+
+
+// +----------+----------+-------------+
+// | kouhi_id | futansha | patient_id  |
+// +----------+----------+-------------+
+// |     1421 | 89135156 |       7358  | 高校生
+// |     1419 | 54136015 |        416  | 難病
+// |     1344 | 82137530 |       7170  | マル都（大気汚染）
+// |     1105 | 82134008 |       5366  | 被爆者の子に対する医療
+// |     1025 | 51433019 |       5525  | 水俣病
+// |      908 | 82138009 |       1977  | 特殊医療・人工透析
+// |      288 | 82137555 |       2727  | 大気汚染関連疾病
+// |      254 | 38136016 |         84  | B型C型ウイルス肝炎医療
+// |      113 | 51136018 |        131  | 難病医療（スモン、劇症肝炎、重症急性膵炎、プリオン病（医原性））
+// |       32 | 82137001 |       1242  | 大気汚染関連疾病
+// |       21 | 81136152 |        608  | ひとり親
+// |        5 | 81137150 |        129  | ひとり親
+// |        1 | 80137151 |        134  | 重度障がい者医療
+// +----------+----------+-------------+
