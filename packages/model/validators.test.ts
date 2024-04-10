81e3c00985415a7a9e7af1339fe35541c661ce2c
hangilc <hangil@chang.jp>
2024-04-10 10:43:27 +0900

    adding kouhi validator

---
+++ packages/model/validators.test.ts
@@ -0,0 +1,128 @@
+import { ValiError } from "valibot";
+import { validateKouhi } from "./validators";
+
+describe("model validators", () => {
+  it("should validate kouhi", () => {
+    const obj = {
+      kouhiId: 1,
+      futansha: 12345678,
+      jukyuusha: 11111111,
+      validFrom: "2023-11-01",
+      validUpto: "2024-10-31",
+      patientId: 123,
+    };
+    const kouhi = validateKouhi(obj);
+    expect(kouhi).toMatchObject(obj);
+  });
+
+  it("should check futansha and jukyuusha in kouhi", () => {
+    const obj = {
+      kouhiId: 1,
+      validFrom: "2023-11-01",
+      validUpto: "2024-10-31",
+      patientId: 123,
+    };
+    try {
+      validateKouhi(obj);
+    } catch (e) {
+      expect(e).toBeInstanceOf(ValiError);
+      const ve = e as ValiError;
+      expect(ve.issues.map(i => i.message)).toEqual(["負担者番号のエラー。", "受給者番号のエラー。"]);
+    }
+  });
+
+  it("should validate valildFrom in kouhi", () => {
+    const obj = {
+      kouhiId: 1,
+      futansha: 12345678,
+      jukyuusha: 11111111,
+      validFrom: "20231101",
+      validUpto: "2024-10-31",
+      patientId: 123,
+    };
+    try {
+      validateKouhi(obj);
+    } catch (e) {
+      expect(e).toBeInstanceOf(ValiError);
+      const ve = e as ValiError;
+      expect(ve.issues.map(i => i.message)).toEqual(["期限開始の形式エラー。"]);
+    }
+  });
+
+  it("should coerce Date to valildFrom in kouhi", () => {
+    const obj = {
+      kouhiId: 1,
+      futansha: 12345678,
+      jukyuusha: 11111111,
+      validFrom: new Date(2023, 10, 1),
+      validUpto: "2024-10-31",
+      patientId: 123,
+    };
+    const kouhi = validateKouhi(obj);
+    expect(kouhi).toMatchObject(Object.assign({}, obj, { validFrom: "2023-11-01" }));
+  });
+
+  it("should coerce to valildUpto in kouhi", () => {
+    const obj = {
+      kouhiId: 1,
+      futansha: 12345678,
+      jukyuusha: 11111111,
+      validFrom: "2023-11-01",
+      validUpto: new Date(2024, 9, 31),
+      patientId: 123,
+    };
+    const kouhi = validateKouhi(obj);
+    expect(kouhi).toMatchObject(Object.assign({}, obj, { validUpto: "2024-10-31" }));
+  });
+
+  it("should coerce null to valildUpto in kouhi", () => {
+    const obj = {
+      kouhiId: 1,
+      futansha: 12345678,
+      jukyuusha: 11111111,
+      validFrom: "2023-11-01",
+      validUpto: null,
+      patientId: 123,
+    };
+    const kouhi = validateKouhi(obj);
+    expect(kouhi).toMatchObject(Object.assign({}, obj, { validUpto: "0000-00-00" }));
+  });
+
+  it("should coerce undefined to valildUpto in kouhi", () => {
+    const obj = {
+      kouhiId: 1,
+      futansha: 12345678,
+      jukyuusha: 11111111,
+      validFrom: "2023-11-01",
+      patientId: 123,
+    };
+    const kouhi = validateKouhi(obj);
+    expect(kouhi).toMatchObject(Object.assign({}, obj, { validUpto: "0000-00-00" }));
+  });
+
+  it("should coerce empty string to valildUpto in kouhi", () => {
+    const obj = {
+      kouhiId: 1,
+      futansha: 12345678,
+      jukyuusha: 11111111,
+      validFrom: "2023-11-01",
+      validUpto: "",
+      patientId: 123,
+    };
+    const kouhi = validateKouhi(obj);
+    expect(kouhi).toMatchObject(Object.assign({}, obj, { validUpto: "0000-00-00" }));
+  });
+
+  it("should coerce string to number in kouhi", () => {
+    const obj = {
+      kouhiId: 1,
+      futansha: "12345678",
+      jukyuusha: "11111111",
+      validFrom: "2023-11-01",
+      validUpto: "2024-10-31",
+      patientId: 123,
+    };
+    const kouhi = validateKouhi(obj);
+    expect(kouhi).toMatchObject(Object.assign({}, obj, { futansha: parseInt(obj.futansha), jukyuusha: parseInt(obj.jukyuusha) }));
+  })
+});
--- packages/model/validators.ts
+++ packages/model/validators.ts
@@ -1,12 +1,16 @@
-import { KouhiInterface, PatientSummary } from "./model";
+import { KouhiInterface, PatientSummary, dateToSqlDate } from "./model";
 import * as v from "valibot";
 
 const KouhiSchema = v.object({
   kouhiId: v.number(),
-  futansha: v.number(),
-  jukyuusha: v.number(),
-  validFrom: v.string([v.regex(/^\d{4}-\d{2}-\d{2}$/)]),
-  validUpto: v.string([v.regex(/^((\d{4}-\d{2}-\d{2})|(0000-00-00))$/)]),
+  futansha: v.coerce(v.number("負担者番号のエラー。"), Number),
+  jukyuusha: v.coerce(v.number("受給者番号のエラー。"), Number),
+  validFrom: v.coerce(
+    v.string("期限開始のエラー。", [v.regex(/^\d{4}-\d{2}-\d{2}$/, "期限開始の形式エラー。")]),
+    coerceToDate),
+  validUpto: v.coerce(
+    v.string([v.regex(/^((\d{4}-\d{2}-\d{2})|(0000-00-00))$/)]),
+    coerceToUptoDate),
   patientId: v.number(),
   memo: v.union([v.string(), v.undefined_()]),
 });
@@ -22,4 +26,22 @@ const PatientSummarySchema = v.object({
 
 export function validatePatientSummary(obj: any): PatientSummary {
   return v.parse(PatientSummarySchema, obj);
+}
+
+function coerceToDate(arg: unknown): unknown {
+  if (arg instanceof Date) {
+    return dateToSqlDate(arg);
+  } else {
+    return arg;
+  }
+}
+
+function coerceToUptoDate(arg: unknown): unknown {
+  if (arg instanceof Date) {
+    return dateToSqlDate(arg);
+  } else if (arg == null || arg === "" ) {
+    return "0000-00-00";
+  } else {
+    return arg;
+  }
 }
