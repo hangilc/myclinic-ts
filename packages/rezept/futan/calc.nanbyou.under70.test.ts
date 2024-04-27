bd035b8437e494d276414b6b319237322815ead1
hangilc <hangil@chang.jp>
2024-04-27 15:14:00 +0900

    started calc nanbyou under70

--- packages/rezept/futan/calc.nanbyou.under70.test.ts
+++ packages/rezept/futan/calc.nanbyou.under70.test.ts
@@ -110,6 +110,80 @@ const specs: SpecExec[] = [
       ["payment", { payer: "nanbyou", kakari: 9000, jikofutan: 6000 }],
     ],
   },
+  {
+    title: "事例7　本人入院（標準報酬月額28万～50万円）・難病医療（既認定者）",
+    payers: [
+      ["hoken", { futanWari: 3 }], ["nanbyou", { gendogaku: 10000 }]
+    ],
+    bills: [
+      [370000, ["hoken", "nanbyou"]],
+    ],
+    setting: {
+      shotokuKubun: "ウ",
+      isNyuuin: true,
+    },
+    asserts: [
+      ["jikofutan", 10000],
+      ["payment", { payer: "hoken", uncovered: 81130 }],
+      ["payment", { payer: "nanbyou", kakari: 81130, jikofutan: 10000 }],
+    ],
+  },
+  {
+    title: "事例8　本人入院（標準報酬月額28万～50万円）・難病医療（既認定者）",
+    payers: [
+      ["hoken", { futanWari: 3 }], ["nanbyou", { gendogaku: 10000 }]
+    ],
+    bills: [
+      [60000, ["hoken", "nanbyou"]],
+      [310000, ["hoken"]],
+    ],
+    setting: {
+      shotokuKubun: "ウ",
+      isNyuuin: true,
+    },
+    asserts: [
+      ["jikofutan", 90530],
+      ["payment", { payer: "hoken", uncovered: 98530 }],
+      ["payment", { payer: "nanbyou", kakari: 18000, jikofutan: 10000 }],
+    ],
+  },
+  {
+    title: "事例9　本人入院（標準報酬月額28万～50万円）・難病医療（既認定者）",
+    payers: [
+      ["hoken", { futanWari: 3 }], ["nanbyou", { gendogaku: 10000 }]
+    ],
+    bills: [
+      [300000, ["hoken", "nanbyou"]],
+      [380000, ["hoken"]],
+    ],
+    setting: {
+      shotokuKubun: "ウ",
+      isNyuuin: true,
+    },
+    asserts: [
+      ["jikofutan", 84230],
+      ["payment", { payer: "hoken", uncovered: 154660 }],
+      ["payment", { payer: "nanbyou", kakari: 80430, jikofutan: 10000 }],
+    ],
+  },
+  {
+    title: "事例10　本人入院（標準報酬月額26万円以下）・難病医療（既認定者）",
+    payers: [
+      ["hoken", { futanWari: 3 }], ["nanbyou", { gendogaku: 5000 }]
+    ],
+    bills: [
+      [220000, ["hoken", "nanbyou"]],
+    ],
+    setting: {
+      shotokuKubun: "エ",
+      isNyuuin: true,
+    },
+    asserts: [
+      ["jikofutan", 5000],
+      ["payment", { payer: "hoken", uncovered: 57600 }],
+      ["payment", { payer: "nanbyou", kakari: 57600, jikofutan: 5000 }],
+    ],
+  },
 ]
 
 describe("難病計算例７０歳未満", () => {
