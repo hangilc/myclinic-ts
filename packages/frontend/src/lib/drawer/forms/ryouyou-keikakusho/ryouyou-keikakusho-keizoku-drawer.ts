e61ea166ebf01f4565cfe5ec3f1ad96a3900fa09
hangilc <hangil@chang.jp>
2024-11-22 23:47:30 +0900

    rewriting middle area

--- packages/frontend/src/lib/drawer/forms/ryouyou-keikakusho/ryouyou-keikakusho-keizoku-drawer.ts
+++ packages/frontend/src/lib/drawer/forms/ryouyou-keikakusho/ryouyou-keikakusho-keizoku-drawer.ts
@@ -82,7 +82,7 @@ function drawMiddleAreaSeq(ctx: DC, box: Box, data: RyouyouKeikakushoData) {
   c.drawText(ctx, "ねらい:重点目標の達成状況を理解できること・目標再設定と指導された生活習慣改善に取り組めること",
     rows[0], "center", "center", { dy: -0.25 });
   drawMokuhyouSeq(ctx, rows[1], data);
-  // drawJuuten(ctx, rows[2]);
+  drawJuutenSeq(ctx, rows[2], data);
   // drawKensa(ctx, rows[3]);
 }
 
@@ -293,6 +293,176 @@ function drawMokuhyouSeq(ctx: DC, box: Box, data: RyouyouKeikakushoData) {
       })
     });
   }
+}
+
+function drawJuutenSeq(ctx: DC, box: Box, data: RyouyouKeikakushoData) {
+  function boxedAndGap(label: string, gap: number, trail: string, boxMark: string, gapMark: string): c.CompositeItem[] {
+    return [
+      ...widgets.boxed(label, boxMark),
+      p.gap(gap, { mark: gapMark }),
+      p.text(trail),
+    ]
+  }
+  const cols = b.splitToColumns(box, b.splitAt(7));
+  c.setPen(ctx, "thick");
+  c.frameRight(ctx, cols[0]);
+  c.drawTextVertically(ctx, "︻重点を置く領域と指導項目︼", cols[0], "center", "center");
+  b.withSplitRows(cols[1], b.splitAt(39, 69, 79), (rs) => {
+    rs.forEach(r => c.frameBottom(ctx, r));
+    const [shokuji, undou, tabako, sonota] = rs;
+    const bodyModifier = b.inset(1);
+    // 食事
+    b.withSplitColumns(shokuji, b.splitAt(18), ([mark, body]) => {
+      c.frameRight(ctx, mark);
+      c.drawComposite(ctx, mark, [
+        ...widgets.boxed("食事", "juuten-食事-mark"),
+      ], { halign: "center" });
+      b.withSplitRows(body, b.evenSplitter(8), rs => {
+        seq(ctx, rs[0], [
+          ...boxed(ctx, "今回は、指導の必要なし", data, "juuten-食事-指導の必要なし-mark")
+        ]);
+        b.withSplitColumns(rs[1], b.splitAt(87), ([left, right]) => {
+          seq(ctx, left, [
+            ...boxed(ctx, "食事摂取量を適正にする", data, "juuten-食事-摂取量を適正にする-mark")
+          ]);
+          seq(ctx, right, [
+            ...boxed(ctx, "食塩・調味料を控える", data, "juuten-食事-食塩・調味料を控える-mark")
+          ]);
+        });
+        b.withSplitColumns(rs[2], b.splitAt(87), ([left, right]) => {
+          seq(ctx, left, [
+            ...boxed(ctx, "野菜・きのこ・海藻など食物繊維の摂取を増やす", data, "juuten-食事-食物繊維の摂取を増やす-mark")
+          ]);
+          seq(ctx, right, [
+            ...boxed(ctx, "外食の際の注意事項(", data, "juuten-食事-外食の際の注意事項-mark"),
+            p.expander({ mark: "juuten-食事-外食の際の注意事項" }),
+            textBlock(ctx, ")", { valign: "center" }),
+          ]);
+        });
+        // b.withSplitColumns(rs[3], b.splitAt(87), ([left, right]) => {
+        //   c.drawComposite(ctx, left, [
+        //     ...widgets.boxed("油を使った料理(揚げ物や炒め物等)の摂取を減らす", "juuten-食事-油を使った料理の摂取を減らす-mark")
+        //   ]);
+        //   c.drawComposite(ctx, right, [
+        //     ...widgets.boxed("その他", "juuten-食事-その他-mark")
+        //   ]);
+        // });
+        // c.drawComposite(ctx, rs[4], [
+        //   ...widgets.boxed("節酒:[減らす(種類・量:", "juuten-食事-節酒-mark"),
+        //   p.gap(42, { mark: "shokuji-食事-節酒" }),
+        //   p.text("を週"),
+        //   p.expander({ mark: "shokuji-食事-節酒-回" }),
+        //   p.text("回)]"),
+        //   p.gapTo(106),
+        // ]);
+        // c.drawComposite(ctx, rs[5], [
+        //   ...widgets.boxed("間食:[減らす(種類・量:", "juuten-食事-間食-mark"),
+        //   p.gap(42, { mark: "shokuji-食事-間食" }),
+        //   p.text("を週"),
+        //   p.expander({ mark: "shokuji-食事-間食-回" }),
+        //   p.text("回)]"),
+        //   p.gapTo(106),
+        // ]);
+        // c.drawComposite(ctx, rs[6], [
+        //   ...widgets.boxed("食べ方:(ゆっくり食べる・その他(", "juuten-食事-食べ方-mark"),
+        //   p.expander({ mark: "juuten-食事-食べ方" }),
+        //   p.text("))"),
+        //   p.gapTo(106,)
+        // ]);
+        // c.drawComposite(ctx, rs[7], [
+        //   ...widgets.boxed("食事時間:朝食、昼食、夕食を規則正しくとる", "juuten-食事-食事時間-mark"),
+        // ])
+      }, { boxModifiers: [bodyModifier] })
+    })
+    // 運動
+    // b.withSplitColumns(undou, b.splitAt(18), ([mark, body]) => {
+    //   c.frameRight(ctx, mark);
+    //   c.drawComposite(ctx, mark, [
+    //     ...widgets.boxed("運動", "juuten-運動-mark"),
+    //   ], { halign: "center" })
+    //   b.withSplitRows(body, b.evenSplitter(6), rs => {
+    //     c.drawComposite(ctx, rs.shift()!, [
+    //       ...widgets.boxed("今回は、指導の必要なし", "juuten-運動-指導の必要なし-mark")
+    //     ]);
+    //     c.drawComposite(ctx, rs.shift()!, [
+    //       ...widgets.boxed("運動処方:種類(ｳｫｰｷﾝｸﾞ・", "juuten-運動-種類-mark"),
+    //       p.expander({ mark: "juuten-運動-種類" }),
+    //       p.text(")"),
+    //       p.gapTo(126),
+    //     ]);
+    //     c.drawComposite(ctx, rs.shift()!, [
+    //       p.gap(7),
+    //       p.text("時間(30分以上・"),
+    //       p.gap(30, { mark: "juuten-運動-時間" }),
+    //       p.text(")、頻度(ほぼ毎日・週"),
+    //       p.expander({ mark: "juuten-運動-頻度" }),
+    //       p.text("日)"),
+    //       p.gapTo(126),
+    //     ]);
+    //     c.drawComposite(ctx, rs.shift()!, [
+    //       p.gap(7),
+    //       p.text("強度(息がはずむが会話が可能な強さ or 脈拍"),
+    //       p.gap(14, { mark: "juuten-運動-強度-脈拍" }),
+    //       p.text("拍/分 or "),
+    //       p.expander({ mark: "juuten-運動-強度-その他" }),
+    //       p.text(")"),
+    //       p.gapTo(126),
+    //     ]);
+    //     c.drawComposite(ctx, rs.shift()!, [
+    //       ...widgets.boxed("日常生活の活動量増加(例:1日1万歩・", "juuten-運動-活動量-mark"),
+    //       p.expander({ mark: "juuten-運動-活動量" }),
+    //       p.text(")"),
+    //       p.gapTo(102),
+    //     ]);
+    //     c.drawComposite(ctx, rs.shift()!, [
+    //       ...widgets.boxed("運動時の注意事項など(", "juuten-運動-注意事項-mark"),
+    //       p.expander({ mark: "juuten-運動-注意事項" }),
+    //       p.text(")"),
+    //       p.gapTo(102),
+    //     ]);
+    //   }, { boxModifiers: [bodyModifier] });
+    // })
+    // // たばこ
+    // b.withSplitColumns(tabako, b.splitAt(18), ([mark, body]) => {
+    //   c.frameRight(ctx, mark);
+    //   c.drawComposite(ctx, mark, [
+    //     ...widgets.boxed("たばこ", "juuten-たばこ-mark"),
+    //   ], { halign: "center" });
+    //   c.drawComposite(ctx, body, [
+    //     ...widgets.boxed("喫煙・節煙の有効性", "juuten-たばこ-禁煙・節煙の有効性-mark"),
+    //     p.gapTo(43),
+    //     ...widgets.boxed("禁煙の実施補法等", "juuten-たばこ-禁煙の実施補法等-mark"),
+    //   ], { boxModifiers: [bodyModifier] });
+    // })
+    // その他
+    // b.withSplitColumns(sonota, b.splitAt(18), ([mark, body]) => {
+    //   c.frameRight(ctx, mark);
+    //   {
+    //     const line1 = [...widgets.boxed("その", "juuten-shokuji-mark")];
+    //     const line2 = [p.gap(5), p.text("他")];
+    //     const w = Math.max(c.calcTotalCompositeWidth(ctx, line1, b.width(mark)), c.calcTotalCompositeWidth(ctx, line2, b.width(mark)));
+    //     const h = c.currentFontSize(ctx) * 2;
+    //     const dx = (b.width(mark) - w) / 2;
+    //     const dy = (b.height(mark) - h) / 2;
+    //     const innerBox = b.modify(mark, b.innerBox(dx, dy, dx + w, dy + h));
+    //     b.withSplitRows(innerBox, b.evenSplitter(2), rs => {
+    //       c.drawComposite(ctx, rs[0], line1);
+    //       c.drawComposite(ctx, rs[1], line2);
+    //     })
+    //   }
+    //   b.withSplitRows(body, b.evenSplitter(3), rs => {
+    //     b.withSplitColumns(rs[0], b.splitAt(24, 48, 92), cs => {
+    //       c.drawComposite(ctx, cs[0], widgets.boxed("仕事", "juuten-その他-仕事"));
+    //       c.drawComposite(ctx, cs[1], widgets.boxed("余暇", "juuten-その他-余暇"));
+    //       c.drawComposite(ctx, cs[2], widgets.boxed("睡眠の確保(質・量)", "juuten-その他-睡眠の確保"));
+    //       c.drawComposite(ctx, cs[3], widgets.boxed("減量", "juuten-その他-減量"));
+    //     });
+    //     c.drawComposite(ctx, rs[1], widgets.boxed("家庭での計測(歩数、体重、血圧、腹囲等)", "juuten-その他-家庭での計測"));
+    //     c.drawComposite(ctx, rs[2],
+    //       boxedAndGap("その他(", 88, ")", "juuten-その他-その他-mark", "juuten-その他-その他"));
+    //   }, { boxModifiers: [bodyModifier] });
+    // })
+  })
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
