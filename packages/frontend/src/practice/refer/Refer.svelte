b47596002d501a0ca12531a2279d3ee0635cd983
hangilc <hangil@chang.jp>
2024-11-27 21:12:42 +0900

    added visits-view to refer

--- packages/frontend/src/practice/refer/Refer.svelte
+++ packages/frontend/src/practice/refer/Refer.svelte
@@ -11,6 +11,8 @@
   import ChevronDown from "@/icons/ChevronDown.svelte";
   import PopupMenu from "@/lib/PopupMenu.svelte";
   import type { ReferDrawerData } from "@/lib/drawer/forms/refer/refer-drawer-data";
+  import { writable, type Writable } from "svelte/store";
+  import VisitsView from "@/lib/VisitsView.svelte";
 
   export let isVisible = false;
   let title = "紹介状";
@@ -24,7 +26,8 @@
   let address = "";
   let clinicName = "";
   let issuerDoctorName = "";
-  let patient: Patient | undefined = undefined;
+  // let patient: Patient | undefined = undefined;
+  let patient: Writable<Patient | undefined> = writable(undefined);
   let clinicInfo: ClinicInfo | undefined = undefined;
   let referConfig: ReferConfig[] = [];
 
@@ -55,7 +58,7 @@
   }
 
   function initWithPatient(p: Patient) {
-    patient = p;
+    $patient = p;
     patientName = `患者：${p.lastName}${p.firstName}`;
     const bd = DateWrapper.from(p.birthday);
     const sex = p.sex === "M" ? "男" : "女";
@@ -124,7 +127,7 @@
   }
 
   function doClearPatient() {
-    patient = undefined;
+    $patient = undefined;
     title = "紹介状";
     referHospital = "";
     referDoctor = "";
@@ -186,61 +189,83 @@
 </script>
 
 {#if isVisible}
-  <ServiceHeader title="紹介状" />
-  <div style="margin: 10px 0;">
-    {#if patient === undefined}
-      <button on:click={doSelectPatient}>患者選択</button>
-    {:else}
-      <button on:click={doClearPatient}>患者終了</button>
-    {/if}
-    <a href="javascript:void(0)" style="margin-left:2em;" on:click={doConfig}
-      >Config</a
-    >
-    {#if patient}
-      <div style="margin-top: 4px;">
-        ({patient.patientId}) {patient.lastName}
-        {patient.firstName}
-      </div>
-    {/if}
-  </div>
-  <div style="display:grid; grid-template-columns:auto 1fr;gap: 4px;">
-    <span>表題：</span>
-    <input type="text" bind:value={title} style="width: 6em;" />
-    <span>医療機関</span>
-    <div style="display:flex;items-align:center;">
-      <input type="text" bind:value={referHospital} style="width: 16em;" />
-      <!-- svelte-ignore a11y-no-static-element-interactions -->
-      <a style="margin-left:4px;cursor:pointer;" on:click={doConfigPopup} href="javascript:void(0)">
-        <ChevronDown />
-      </a>
-    </div>
-    <span>医師</span>
+  <div class="wrapper">
     <div>
-      <input type="text" bind:value={referDoctor} style="width: 16em;" /> 先生御机下
+      <ServiceHeader title="紹介状" />
+      <div style="margin: 10px 0;">
+        {#if $patient === undefined}
+          <button on:click={doSelectPatient}>患者選択</button>
+        {:else}
+          <button on:click={doClearPatient}>患者終了</button>
+        {/if}
+        <a
+          href="javascript:void(0)"
+          style="margin-left:2em;"
+          on:click={doConfig}>Config</a
+        >
+        {#if $patient}
+          <div style="margin-top: 4px;">
+            ({$patient.patientId}) {$patient.lastName}
+            {$patient.firstName}
+          </div>
+        {/if}
+      </div>
+      <div style="display:grid; grid-template-columns:auto 1fr;gap: 4px;">
+        <span>表題：</span>
+        <input type="text" bind:value={title} style="width: 6em;" />
+        <span>医療機関</span>
+        <div style="display:flex;items-align:center;">
+          <input type="text" bind:value={referHospital} style="width: 16em;" />
+          <!-- svelte-ignore a11y-no-static-element-interactions -->
+          <a
+            style="margin-left:4px;cursor:pointer;"
+            on:click={doConfigPopup}
+            href="javascript:void(0)"
+          >
+            <ChevronDown />
+          </a>
+        </div>
+        <span>医師</span>
+        <div>
+          <input type="text" bind:value={referDoctor} style="width: 16em;" /> 先生御机下
+        </div>
+        <span>患者氏名</span>
+        <input type="text" bind:value={patientName} style="width: 16em;" />
+        <span>患者情報</span>
+        <input type="text" bind:value={patientInfo} style="width: 16em;" />
+        <span>診断</span>
+        <input type="text" bind:value={diagnosis} style="width: 16em;" />
+        <span>内容</span>
+        <textarea bind:value={content} style="width: 26em;height: 10em;" />
+        <span>発行日</span>
+        <input type="text" bind:value={issueDate} style="width: 16em;" />
+        <span>住所</span>
+        <textarea
+          bind:value={address}
+          style="width: 18em;height:5.5em;resize:vertical"
+        />
+        <span>発行機関</span>
+        <input type="text" bind:value={clinicName} style="width: 16em;" />
+        <span>発行医師</span>
+        <input type="text" bind:value={issuerDoctorName} style="width: 16em;" />
+      </div>
+      <div style="margin-top:10px;">
+        <button on:click={doView}>表示</button>
+        <button on:click={doRecord}>クリップボードへコピー</button>
+        <button on:click={doLoad}>クリップボードからペースト</button>
+      </div>
     </div>
-    <span>患者氏名</span>
-    <input type="text" bind:value={patientName} style="width: 16em;" />
-    <span>患者情報</span>
-    <input type="text" bind:value={patientInfo} style="width: 16em;" />
-    <span>診断</span>
-    <input type="text" bind:value={diagnosis} style="width: 16em;" />
-    <span>内容</span>
-    <textarea bind:value={content} style="width: 26em;height: 10em;" />
-    <span>発行日</span>
-    <input type="text" bind:value={issueDate} style="width: 16em;" />
-    <span>住所</span>
-    <textarea
-      bind:value={address}
-      style="width: 18em;height:5.5em;resize:vertical"
-    />
-    <span>発行機関</span>
-    <input type="text" bind:value={clinicName} style="width: 16em;" />
-    <span>発行医師</span>
-    <input type="text" bind:value={issuerDoctorName} style="width: 16em;" />
-  </div>
-  <div style="margin-top:10px;">
-    <button on:click={doView}>表示</button>
-    <button on:click={doRecord}>クリップボードへコピー</button>
-    <button on:click={doLoad}>クリップボードからペースト</button>
+    <div class="visit-view">
+      <VisitsView {patient} />
+    </div>
   </div>
 {/if}
+
+<style>
+    .wrapper {
+    display: grid;
+    grid-template-columns: 1fr 400px;
+    column-gap: 10px;
+  }
+
+</style>
