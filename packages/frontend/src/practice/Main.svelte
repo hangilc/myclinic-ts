8c2174a1243363766817a7594ddd5a2806f5e18d
hangilc <hangil@chang.jp>
2024-12-20 15:42:29 +0900

    cleanup

--- packages/frontend/src/practice/Main.svelte
+++ packages/frontend/src/practice/Main.svelte
@@ -18,9 +18,6 @@
   import Refer from "./refer/Refer.svelte";
   import Shindansho from "./shindansho/Shindansho.svelte";
   import ShohouUsage from "./shohou-usage/ShohouUsage.svelte";
-  import { parseShohou } from "@/lib/parse-shohou";
-  import DrawerDialog from "@/lib/drawer/DrawerDialog.svelte";
-  import { drawShohousen2024NoRefill } from "@/lib/drawer/forms/shohousen-2024/shohousenDrawer2024NoRefill";
 
   export let serviceStore: Writable<string>;
 
@@ -32,54 +29,6 @@
     }
   });
 
-  ///////////////////////////////////////////////////////////////////////////////
-  // function testShohousen() {
-//     let shohou = parseShohou(`Ｒｐ）
-// １）カロナール錠５００　３錠
-// 　　アンブロキソール錠１５ｍｇ　３錠
-// 　　分３　毎食後　５日分
-// `)
-//     console.log("shohou", JSON.stringify(shohou, undefined, 2));
-//   }
-
-  function testShohousen() {
-    const ops = drawShohousen2024NoRefill({
-      hokenshaBangou: "123456",
-      hihokenshaKigou: "12-56",
-      hihokenshaBangou: "324111",
-      edaban: "01",
-      futansha: "12345678",
-      jukyuusha: "7654321",
-      futansha2: "11111111",
-      jukyuusha2: "7777777",
-      shimei: "診療 太郎",
-      birthdate: "1957-06-02",
-      sex: "M",
-      hokenKubun: "hihokensha",
-      clinicAddress: "〒111-0000 杉並区某所南1-22-15",
-      clinicName: "某内科クリニック",
-      clinicPhone: "03-1111-2222",
-      doctorName: "処方 二郎",
-      clinicTodoufuken: "13",
-      clinicKikancode: "1234567",
-      koufuDate: "2024-12-02",
-      validUptoDate: "2024-12-09",
-      bikou:
-        "高８\n一包化をお願いします。",
-    });
-    const d: DrawerDialog = new DrawerDialog({
-      target: document.body,
-      props: {
-        destroy: () => d.$destroy(),
-        pages: ops,
-        width: 148,
-        height: 210,
-        scale: 3,
-        kind: "shohousen2024",
-        title: "処方箋印刷",
-      },
-    });
-  }
 </script>
 
 <div>
@@ -112,7 +61,4 @@
     <ShohouUsage />
   {/if}
   <BigChar isVisible={$serviceStore === "big-char"} />
-  <div>
-    <button on:click={testShohousen}>処方箋</button>
-  </div>
 </div>
