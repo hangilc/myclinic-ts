13b48e61cbe3f99ae6b1c54b4020aad6ea7d6d3d
hangilc <hangil@chang.jp>
2024-12-28 23:47:43 +0900

    added kigen editor

--- packages/frontend/src/lib/denshi-shohou/KigenForm.svelte
+++ packages/frontend/src/lib/denshi-shohou/KigenForm.svelte
@@ -27,8 +27,16 @@
   function doClear() {
     onEnter(undefined);
   }
+
+  function format(date: Date | null): string {
+    if( date == null ){
+      return "（未設定）";
+    } else {
+      return DateWrapper.fromDate(date).render(d => `${d.getYear()}年${d.getMonth()}月${d.getDay()}日（${d.getYoubi()}）`)
+    }
+  }
 </script>
 <div style="border:1px solid gray;border-radius:4px;padding:10px;margin:4px 0;">
-  有効期限：<EditableDate bind:date={date} onChange={onChange} />
+  有効期限：<EditableDate bind:date={date} onChange={onChange} {format} />
   <a href="javascript:void(0)" on:click={doClear}>クリア</a>
 </div>
