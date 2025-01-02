18466d2bdca52b6dc0df9ff445ca7c91e6cd70da
hangilc <hangil@chang.jp>
2025-01-03 00:42:46 +0900

    cleanup

--- packages/frontend/src/lib/futan-wari.ts
+++ packages/frontend/src/lib/futan-wari.ts
@@ -22,13 +22,7 @@ export function resolveFutanWari(hoken: Shahokokuho | Koukikourei, testUnder6: (
 }
 
 export function isUnder6(birthdate: DateWrapperLike, at: DateWrapperLike): boolean {
-  console.log("enter isUnder6", birthdate, at);
-  // const b6 = DateWrapper.from(birthdate).incYear(6).incDay(-1);
-  let b6 = DateWrapper.from(birthdate);
-  b6 = b6.incYear(6);
-  b6 = b6.incDay(-1);
-  console.log("b6", b6);
-
+  const b6 = DateWrapper.from(birthdate).incYear(6).incDay(-1);
   let d: DateWrapper = DateWrapper.fromYearMonthDay(b6.getYear(), 3, 31);
   if (cmpNumSeq(b6.monthDayTuple(), [3, 31]) > 0) {
     d = d.incYear(1);
