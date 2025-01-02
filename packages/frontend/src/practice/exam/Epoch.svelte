04fad797b3420f264227c4b53a27d877979b8f01
hangilc <hangil@chang.jp>
2025-01-02 11:19:08 +0900

    added epoch list

--- packages/frontend/src/practice/exam/Epoch.svelte
+++ packages/frontend/src/practice/exam/Epoch.svelte
@@ -4,38 +4,132 @@
   import api from "@/lib/api";
   import type { Visit } from "myclinic-model";
   import { DateWrapper } from "myclinic-util";
+  import ChevronDown from "@/icons/ChevronDown.svelte";
 
   const unsubs: (() => void)[] = [];
-  let epochs: { id: number, text: string, visit: Visit }[] = [];
+  let epochs: { id: number; text: string; visit: Visit }[] = [];
   let index = 1;
+  let showMenu = false;
+  let showEditor = false;
+  let editorContent = "";
 
-  unsubs.push(currentPatient.subscribe(async (patient) => {
-    if( patient ){
-      const texts = await api.searchTextForPatient("[[EPOCH]]", patient.patientId, 200, 0);
-      texts.sort((a, b) => -a[1].visitedAt.localeCompare(b[1].visitedAt));
-      epochs = texts.map(([t, v]) => ({ id: index++, text: t.content, visit: v }));
-    } else {
-      epochs = [];
-    }
-  }));
+  unsubs.push(
+    currentPatient.subscribe(async (patient) => {
+      if (patient) {
+        await fetchEpochs(patient.patientId);
+      } else {
+        epochs = [];
+      }
+    })
+  );
 
-  onDestroy(() => unsubs.forEach(sub => sub()));
-  
+  onDestroy(() => unsubs.forEach((sub) => sub()));
+
+  async function fetchEpochs(patientId: number) {
+    const texts = await api.searchTextForPatient(
+      "[[EPOCH]]",
+      patientId,
+      200,
+      0
+    );
+    texts.sort((a, b) => -a[1].visitedAt.localeCompare(b[1].visitedAt));
+    epochs = texts.map(([t, v]) => ({
+      id: index++,
+      text: t.content,
+      visit: v,
+    }));
+  }
+
   function formatEpoch(e: { text: string }): string {
     let t = e.text.replaceAll(/\[\[EPOCH\]\]\s*\n?/g, "");
-    t = t.replaceAll(/^●.*\n?/mg, "");
+    t = t.replaceAll(/^●.*\n?/gm, "");
     t = t.trim();
     t = t.replaceAll("\n", "<br />\n");
     return t;
   }
 
   function formatDate(at: string): string {
-    return DateWrapper.from(at).render(d => `${d.getGengou()}${d.getNen()}年${d.getMonth()}月${d.getDay()}日`)
+    return DateWrapper.from(at).render(
+      (d) => `${d.getGengou()}${d.getNen()}年${d.getMonth()}月${d.getDay()}日`
+    );
   }
 
+  async function doReload() {
+    const patient = $currentPatient;
+    if (patient) {
+      await fetchEpochs(patient.patientId);
+      showMenu = false;
+    }
+  }
+
+  function doList() {
+    onList(false);
+  }
+
+  function doListReverse() {
+    onList(true);
+  }
+
+  function onList(reverse: boolean) {
+    if (showEditor) {
+      editorContent = "";
+      showEditor = false;
+    } else {
+      const es = [...epochs];
+      es.sort((a, b) => {
+        return a.visit.visitedAt.localeCompare(b.visit.visitedAt);
+      });
+      if( reverse ){
+        es.reverse();
+      }
+      const ss = es
+        .map((epoch) => {
+          const date = DateWrapper.from(
+            epoch.visit.visitedAt.substring(0, 10)
+          ).render(
+            (d) => `【${d.getYear()}年${d.getMonth()}月${d.getDay()}日}】`
+          );
+          let text = epoch.text;
+          text = text.replaceAll(/\[\[EPOCH\]\]\s*\n?/g, "");
+          return `${date}${text}`.trim();
+        })
+        .join("\n");
+      editorContent = ss;
+      showEditor = true;
+    }
+  }
 </script>
 
 <div>
+  {#if epochs.length > 0}
+    <div>
+      <span style="font-weight:bold">エポック</span>
+      <a
+        href="javascript:void(0)"
+        style="position:relative;top:3px;"
+        on:click={() => (showMenu = !showMenu)}
+      >
+        <ChevronDown />
+      </a>
+    </div>
+    {#if showMenu}
+      <div>
+        <a href="javascript:void(0)" on:click={doReload}>リロード</a>
+        <span class="sep">|</span>
+        <a href="javascript:void(0)" on:click={doList}>リスト作成</a>
+        <span class="sep">|</span>
+        <a href="javascript:void(0)" on:click={doListReverse}
+          >リスト作成（逆順）</a
+        >
+      </div>
+      {#if showEditor}
+        <textarea
+          style="width:95%;resize:vertical;height:10em;"
+          bind:value={editorContent}
+        ></textarea>
+      {/if}
+    {/if}
+  {/if}
   {#each epochs as epoch (epoch.id)}
     <div class="epoch">
       <div class="date">{formatDate(epoch.visit.visitedAt)}</div>
@@ -55,5 +149,11 @@
 
   .epoch .date {
     color: green;
+  }
+
+  .sep {
+    opacity: 0.3;
+    position: relative;
+    top: -2px;
   }
 </style>
