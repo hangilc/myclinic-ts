563b8fe2f5bb38a4bcf9fd0e71fd8a5d669d7e1d
hangilc <hangil@chang.jp>
2025-01-13 20:43:57 +0900

    temporary saving

--- packages/frontend/src/lib/denshi-shohou/drug-group-form/UnevenForm.svelte
+++ packages/frontend/src/lib/denshi-shohou/drug-group-form/UnevenForm.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { toHankaku } from "myclinic-rezept/zenkaku";
   import type { 不均等レコード } from "../presc-info";
 
   export let 不均等レコード: 不均等レコード | undefined;
@@ -8,6 +9,70 @@
   let 不均等３回目服用量 = "";
   let 不均等４回目服用量 = "";
   let 不均等５回目服用量 = "";
+
+  function doClear() {
+    onDone(undefined);
+  }
+
+  function doEnter() {
+    if (不均等１回目服用量 === "") {
+      alert("不均等１回目服用量が入力されていません。");
+      return;
+    }
+    if (不均等２回目服用量 === "") {
+      alert("不均等２回目服用量が入力されていません。");
+      return;
+    }
+    try {
+      const rec: 不均等レコード = {
+        不均等１回目服用量: resolve(不均等１回目服用量, "不均等１回目服用量"),
+        不均等２回目服用量: resolve(不均等２回目服用量, "不均等２回目服用量"),
+        不均等３回目服用量: resolveOpt(
+          不均等３回目服用量,
+          "不均等３回目服用量"
+        ),
+        不均等４回目服用量: resolveOpt(
+          不均等４回目服用量,
+          "不均等４回目服用量"
+        ),
+        不均等５回目服用量: resolveOpt(
+          不均等５回目服用量,
+          "不均等５回目服用量"
+        ),
+      };
+      onDone(rec);
+    } catch (ex) {
+      alert(ex);
+    }
+  }
+
+  function toNumberString(value: string, label: string): string {
+    value = toHankaku(value);
+    const n = parseFloat(value);
+    if (isNaN(n)) {
+      throw `${label}の入力が数値でありません。`;
+    }
+    if (!(n >= 0)) {
+      throw `${label}の値が正またはゼロでありません。`;
+    }
+    return n.toString();
+  }
+
+  function resolve(value: string, label: string): string {
+    value = value.trim();
+    if (value === "") {
+      throw `${label}の値が入力されていません。`;
+    }
+    return toNumberString(value, label);
+  }
+
+  function resolveOpt(value: string, label: string): string | undefined {
+    value = value.trim();
+    if (value === "") {
+      return undefined;
+    }
+    return toNumberString(value, label);
+  }
 </script>
 
 <div>
@@ -46,4 +111,8 @@
       style="width:4em"
     />
   </div>
+</div>
+<div>
+  <button on:click={doClear}>不均等クリア</button>
+  <button on:click={doEnter}>不均等設定</button>
 </div>
