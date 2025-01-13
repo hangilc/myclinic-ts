06d3f359f76c8e50c95483004df30145d75c4b36
hangilc <hangil@chang.jp>
2025-01-13 21:01:49 +0900

    added uneven form

--- packages/frontend/src/lib/denshi-shohou/DrugGroupForm.svelte
+++ packages/frontend/src/lib/denshi-shohou/DrugGroupForm.svelte
@@ -14,11 +14,13 @@
     剤形レコード,
     用法レコード,
     用法補足レコード,
+    不均等レコード,
   } from "./presc-info";
   import UsageAdditionForm from "./drug-group-form/UsageAdditionForm.svelte";
   import JohoKubunForm from "./drug-group-form/JohoKubunForm.svelte";
   import DrugKindForm from "./drug-group-form/DrugKindForm.svelte";
   import AmountForm from "./drug-group-form/AmountForm.svelte";
+  import UnevenForm from "./drug-group-form/UnevenForm.svelte";
 
   export let at: string;
   let 剤形区分: 剤形区分 = "内服";
@@ -28,6 +30,7 @@
   let 情報区分: 情報区分 = "医薬品";
   let drugKind: DrugKind | undefined = undefined;
   let amount: number | undefined = undefined;
+  let 不均等レコード: 不均等レコード | undefined = undefined;
   let edit: EditMode | undefined = undefined;
 
   function doEnter() {
@@ -68,6 +71,7 @@
     };
     const 薬品情報: 薬品情報 = {
       薬品レコード,
+      不均等レコード,
     };
     const rp: RP剤情報 = {
       剤形レコード,
@@ -87,6 +91,27 @@
         return "回分";
       default:
         return "";
+    }
+  }
+
+  function unevenRep(rec: 不均等レコード | undefined): string {
+    if( rec ){
+      const parts: string[] = [
+        rec.不均等１回目服用量,
+        rec.不均等２回目服用量,
+      ];
+      if( rec.不均等３回目服用量 != undefined ){
+        parts.push(rec.不均等３回目服用量)
+      }
+      if( rec.不均等４回目服用量 != undefined ){
+        parts.push(rec.不均等４回目服用量)
+      }
+      if( rec.不均等５回目服用量 != undefined ){
+        parts.push(rec.不均等５回目服用量)
+      }
+      return parts.join("-");
+    } else {
+      return "";
     }
   }
 
@@ -132,6 +157,11 @@
     amount = value;
     edit = undefined;
   }
+
+  function onDone不均等レコード(value: 不均等レコード | undefined) {
+    不均等レコード = value;
+    edit = undefined;
+  }
 </script>
 
 <!-- svelte-ignore a11y-no-static-element-interactions -->
@@ -165,6 +195,8 @@
         {/each}
       </ul>
     </div>
+    <div class="title" on:click={() => doEdit("不均等レコード")}>不均等</div>
+    <div>{unevenRep(不均等レコード)}</div>
   </div>
   <div>
     {#if edit === "剤形区分"}
@@ -188,6 +220,9 @@
     {:else if edit === "用量"}
       <div class="edit-title">用量</div>
       <AmountForm {amount} unit={drugKind?.単位名 ?? ""} onDone={onDone用量} />
+    {:else if edit === "不均等レコード"}
+      <div class="edit-title">不均等</div>
+      <UnevenForm 不均等レコード={不均等レコード} onDone={onDone不均等レコード} />
     {/if}
   </div>
   <div style="margin-top:10px;padding:10px;text-align:right;">
