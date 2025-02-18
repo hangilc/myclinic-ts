1d6cb3a463eca6d2f9c1f58b6ce394124859e66c
hangilc <hangil@chang.jp>
2025-02-18 17:48:20 +0900

    resolved conflict in drug-disease.ts

--- packages/frontend/src/lib/drug-disease.ts
+++ packages/frontend/src/lib/drug-disease.ts
@@ -12,22 +12,17 @@ export function hasMatchingDrugDisease(
   drugName: string, diseaseNames: string[], drugDiseases: DrugDisease[]
 ): true | { pre: string[], name: string, post: string[] }[] {
   const fixes: { pre: string[], name: string, post: string[] }[] = [];
-  const debug =  false;
-  if( debug ){ console.log("drugName", drugName)}
+  const debug = false;
+  if (debug) { console.log("drugName", drugName) }
   for (let item of drugDiseases) {
     if (drugName.includes(item.drugName)) {
-<<<<<<< HEAD
-      if( debug ) { console.log("name match", item); }
+      if (debug) { console.log("name match", item); }
       for (let diseaseName of diseaseNames) {
         if (diseaseName.includes(item.diseaseName)) {
-=======
-      for (let diseaseName of diseaseNames) {
-        if (item.diseaseName.includes(diseaseName)) {
->>>>>>> denshi-conv
           return true;
         }
       }
-      if( item.fix ){
+      if (item.fix) {
         fixes.push(item.fix);
       }
     }
@@ -36,7 +31,7 @@ export function hasMatchingDrugDisease(
 }
 
 export function fullNameOfFix(fix: { pre: string[], name: string, post: string[] } | undefined): string {
-  if( fix ){
+  if (fix) {
     return [...fix.pre, fix.name, ...fix.post].join("");
 
   } else {
