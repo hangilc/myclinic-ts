d38a1a954b1495b9c79c3afb447e791b80b35004
Hangil Chang <hangil@chang.jp>
2025-05-05 00:25:35 +0900

    added check ryouyou douisho

--- packages/frontend/src/practice/rcpt-check/checkers/check-ryouyou-douisho.ts
+++ packages/frontend/src/practice/rcpt-check/checkers/check-ryouyou-douisho.ts
@@ -13,7 +13,8 @@ export async function checkRyouyouDouisho(visits: VisitEx[]): Promise<CheckError
         const fromDate = DateWrapper.from(date).setDay(1).incMonth(-5).setDay(16).asSqlDate();
         const uptoDate = DateWrapper.from(date).incDay(-1).asSqlDate();
         console.log("fromDate", fromDate, uptoDate);
-        const prevs = await api.listVisitIdByDateIntervalAndPatient(fromDate, uptoDate, visit.patient.patientId);
+        const prevs = await api.listVisitIdByDateIntervalAndPatient(
+          fromDate, uptoDate, visit.patient.patientId);
         let lastDate = "";
         for(let prev of prevs){
           const list = await api.listShinryouForVisit(prev);
@@ -28,10 +29,24 @@ export async function checkRyouyouDouisho(visits: VisitEx[]): Promise<CheckError
           }
         }
         if( lastDate !== "" ){
-          
+          let dueDate = kasanStartDate(lastDate);
+          checkErrors.push({
+            code: `療養費同意書交付料算定不可：以前${lastDate}に算定されているので、` +
+              `次に算定できるのは、${dueDate}です。`
+          })
         }
       }
     }
   }
   return checkErrors;
 }
+
+function kasanStartDate(prevDate: string): string {
+  const prev = DateWrapper.from(prevDate);
+  if( prev.getDay() < 16 ){
+    return prev.setDay(1).incMonth(5).asSqlDate();
+  } else {
+    return prev.setDay(1).incMonth(6).asSqlDate();
+  }
+}
+
