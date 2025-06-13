ade9054674978fc5e9f12a2c3aa94ea7c4f1f393
Hangil Chang <hangil@chang.jp>
2025-06-13 09:28:59 +0900

    temporary saving

--- packages/frontend/src/lib/denshi-editor/DenshiEditor.svelte
+++ packages/frontend/src/lib/denshi-editor/DenshiEditor.svelte
@@ -3,7 +3,13 @@
   import type { PrescInfoData, 薬品情報 } from "@/lib/denshi-shohou/presc-info";
   import PrescRep from "./PrescRep.svelte";
   import NewDrug from "./NewDrug.svelte";
-  import { indexRP剤情報, index薬品情報, unindex薬品情報, type RP剤情報Indexed, type 薬品情報Indexed } from "./denshi-editor-types";
+  import {
+    indexRP剤情報,
+    index薬品情報,
+    unindex薬品情報,
+    type RP剤情報Indexed,
+    type 薬品情報Indexed,
+  } from "./denshi-editor-types";
   import EditDrug from "./EditDrug.svelte";
 
   export let destroy: () => void;
@@ -12,73 +18,82 @@
   let clearForm: () => void = () => {};
   let at = shohouDateToSqlDate(data.処方箋交付年月日);
 
-  let groups: RP剤情報Indexed[] = data.RP剤情報グループ.map(g => indexRP剤情報(g));
-  
+  let groups: RP剤情報Indexed[] = data.RP剤情報グループ.map((g) =>
+    indexRP剤情報(g),
+  );
+
   function shohouDateToSqlDate(shohouDate: string): string {
-	let y = shohouDate.substring(0, 4);
-	let m = shohouDate.substring(4, 6);
-	let d = shohouDate.substring(6,8);
-	return `${y}-${m}-${d}`;
+    let y = shohouDate.substring(0, 4);
+    let m = shohouDate.substring(4, 6);
+    let d = shohouDate.substring(6, 8);
+    return `${y}-${m}-${d}`;
   }
 
   function doAddDrug(group: RP剤情報Indexed) {
-	clearForm();
-	const e: NewDrug = new NewDrug({
-	  target: formElement,
-	  props: {
-		at,
-		onDone: () => {
-		  clearForm();
-		},
-		onEnter: drug => {
-		  group.薬品情報グループ.push(index薬品情報(drug));
-		  groups = groups;
-		}
-	  }
-	});
-	clearForm = () => e.$destroy();
+    clearForm();
+    const e: NewDrug = new NewDrug({
+      target: formElement,
+      props: {
+        at,
+        onDone: () => {
+          clearForm();
+        },
+        onEnter: (drug) => {
+          group.薬品情報グループ.push(index薬品情報(drug));
+          groups = groups;
+        },
+      },
+    });
+    clearForm = () => e.$destroy();
+  }
+
+  function findGroup(id: number): RP剤情報Indexed | undefined {
+    for (let g of groups) {
+      if (g.id === id) {
+        return g;
+      }
+    }
+    return undefined;
   }
 
   function doDrugSelect(group: RP剤情報Indexed, drug: 薬品情報Indexed) {
-	clearForm();
-	const e: EditDrug = new EditDrug({
-	  target: formElement,
-	  props: {
-		at,
-		drug: unindex薬品情報(drug),
-		剤形区分: group.剤形レコード.剤形区分,
-		onDone: () => {
-		  clearForm();
-		},
-		onEnter: (updated: 薬品情報) => {
-		  console.log("onEnter", updated);
-		  group.薬品情報グループ =  group.薬品情報グループ.map(
-			d => d.id === drug.id ? index薬品情報(updated) : d
-		  );
-		  console.log("updated", group);
-		  groups = groups;
-		  console.log("groups", groups);
-		},
-		onDelete: () => {
-		  group.薬品情報グループ = group.薬品情報グループ.filter(d => d.id !== drug.id);
-		  groups = groups;
-		}
-	  }
-	});
-	clearForm = () => e.$destroy();
-	group.薬品情報グループ = group.薬品情報グループ.filter(d => d.id !== drug.id);
-	groups = groups;
+    clearForm();
+    const e: EditDrug = new EditDrug({
+      target: formElement,
+      props: {
+        at,
+        drug: unindex薬品情報(drug),
+        剤形区分: group.剤形レコード.剤形区分,
+        onDone: () => {
+          clearForm();
+        },
+        onEnter: (updated: 薬品情報) => {
+          console.log("onEnter", updated);
+          let g = findGroup(group.id);
+          if (g) {
+            g.薬品情報グループ = g.薬品情報グループ.map((d) =>
+              d.id === drug.id ? index薬品情報(updated) : d,
+            );
+            groups = groups;
+          }
+        },
+        onDelete: () => {
+          group.薬品情報グループ = group.薬品情報グループ.filter(
+            (d) => d.id !== drug.id,
+          );
+          groups = groups;
+        },
+      },
+    });
+    clearForm = () => e.$destroy();
   }
-  
 </script>
 
 <Dialog2 title="電子処方箋編集" {destroy}>
   <div class="top">
-    <div class="rep"><PrescRep
-	  {groups}
-	  onAddDrug={doAddDrug}
-	  onDrugSelect={doDrugSelect}
-	/></div>
+    <div class="rep">
+      <PrescRep {groups} onAddDrug={doAddDrug} onDrugSelect={doDrugSelect} />
+    </div>
     <div class="form" bind:this={formElement}></div>
   </div>
 </Dialog2>
