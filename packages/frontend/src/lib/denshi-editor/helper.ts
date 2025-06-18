1f8a5a2839bf6c4811bd46bbb75618bba032d2e6
Hangil Chang <hangil@chang.jp>
2025-06-18 10:12:49 +0900

    added unevenRep

--- packages/frontend/src/lib/denshi-editor/helper.ts
+++ packages/frontend/src/lib/denshi-editor/helper.ts
@@ -1,6 +1,7 @@
-import type { 薬品レコード, 薬品情報 } from "@/lib//denshi-shohou/presc-info";
+import type { 不均等レコード, 薬品レコード, 薬品情報 } from "@/lib/denshi-shohou/presc-info";
 import type { 力価フラグ, 情報区分, 薬品コード種別 } from "@/lib/denshi-shohou/denshi-shohou";
 import { toZenkaku } from "@/lib/zenkaku";
+import { 不均等レコードWrapper } from "../denshi-shohou/denshi-type-wrappers";
 
 export function validateDrug(drug: {
   情報区分: 情報区分;
@@ -13,8 +14,7 @@ export function validateDrug(drug: {
 }): 薬品レコード | string {
   if( drug.薬品コード === "" ){
     return "薬品コードが設定されていません。";
-  }
-  if( drug.薬品名称 === "" ){
+  }  if( drug.薬品名称 === "" ){
     return "薬品名称が設定されていません。";
   }
   if( drug.分量 === "" ){
@@ -31,9 +31,18 @@ export function validateDrug(drug: {
   return Object.assign({}, drug, { 単位名: drug.単位名 });
 }
 
+function unevenRep(u: 不均等レコード | undefined): string {
+  if( u ){
+	  let wrapper = new 不均等レコードWrapper(u);
+	  return "(" + wrapper.getParts().join("-") +  ")"
+  } else {
+	  return "";
+  }
+}
+
 export function drugRep(drug: 薬品情報): string {
   return `${drug.薬品レコード.薬品名称}　${toZenkaku(
     drug.薬品レコード.分量,
-  )}${drug.薬品レコード.単位名}`;
+  )}${drug.薬品レコード.単位名}${unevenRep(drug.不均等レコード)}`;
 }
 
