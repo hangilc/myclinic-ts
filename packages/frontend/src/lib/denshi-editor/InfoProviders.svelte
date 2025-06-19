a85684552a94242837894f549c3f61f9ff5d68f0
Hangil Chang <hangil@chang.jp>
2025-06-19 13:41:53 +0900

    temporary saving

--- packages/frontend/src/lib/denshi-editor/InfoProviders.svelte
+++ packages/frontend/src/lib/denshi-editor/InfoProviders.svelte
@@ -2,11 +2,26 @@
   import type { 提供診療情報レコードIndexed } from "./denshi-editor-types";
 
   export let 提供診療情報レコード: 提供診療情報レコードIndexed[];
+
+  function doEnter(rec: 提供診療情報レコードIndexed) {
+    rec.orig = Object.assign({}, rec.orig, {
+      提供診療情報: rec.提供診療情報,
+    });
+    rec.isEditing = false;
+  }
 </script>
 
 {#each 提供診療情報レコード as rec (rec.id)}
-  <div class="info">
-    <span class="info-label">{rec.提供診療情報区分}：</span>
-    <span class="info-value">{rec.提供診療情報}</span>
-  </div>
+  {#if rec.isEditing}
+    <form on:submit|preventDefault={() => doEnter(rec)}>
+      薬品名称： <input type="text" bind:value={rec.薬品名称} /> <span class="opt">optional</span>
+    </form>
+  {:else}
+  {/if}
 {/each}
+
+<style>
+  .opt {
+    color: #999;
+  }
+</style>
