e17787b730ca937de8287ac50a5c635bde01fc32
Hangil Chang <hangil@chang.jp>
2025-06-19 09:18:48 +0900

    temporary saving

--- packages/frontend/src/lib/denshi-editor/drug-form/YouhouHosoku.svelte
+++ packages/frontend/src/lib/denshi-editor/drug-form/YouhouHosoku.svelte
@@ -1,13 +1,32 @@
 <script lang="ts">
+  import { receiveMessageOnPort } from "worker_threads";
   import type { 用法補足レコードIndexed } from "../denshi-editor-types";
+  import CancelLink from "../icons/CancelLink.svelte";
+  import SubmitLink from "../icons/SubmitLink.svelte";
+  import TrashLink from "../icons/TrashLink.svelte";
 
   export let 用法補足レコード: 用法補足レコードIndexed[];
 
   function doEnter(rec: 用法補足レコードIndexed) {
     rec.orig = Object.assign({}, rec.orig, {
-      用法補足情報: rec.用法補足情報
+      用法補足情報: rec.用法補足情報,
     });
     rec.isEditing = false;
+    用法補足レコード = 用法補足レコード;
+  }
+
+  function doDelete(rec: 用法補足レコードIndexed) {
+    用法補足レコード = 用法補足レコード.filter((r) => r.id !== rec.id);
+  }
+
+  function doEdit(rec: 用法補足レコードIndexed) {
+    rec.isEditing = true;
+    用法補足レコード = 用法補足レコード;
+  }
+
+  function doCancel(rec: 用法補足レコードIndexed) {
+    rec.isEditing = false;
+    rec.用法補足情報 = rec.orig.用法補足情報;
     用法補足レコード = 用法補足レコード;
   }
 </script>
@@ -15,14 +34,30 @@
 {#each 用法補足レコード as rec (rec.id)}
   {#if rec.用法補足区分 === "用法の続き"}
     {#if rec.isEditing}
-      用法の続き：
-      <form on:submit|preventDefault={() => doEnter(rec)} >
-        <input type="text" bind:value={rec.用法補足情報} />
+      <form on:submit|preventDefault={() => doEnter(rec)}>
+        用法の続き：<input type="text" bind:value={rec.用法補足情報} />
+        {#if rec.用法補足情報 !== ""}
+          <SubmitLink onClick={() => doEnter(rec)} />
+        {/if}
+        <TrashLink onClick={() => doDelete(rec)} />
+        <CancelLink onClick={() => doCancel(rec)} />
       </form>
     {:else}
-      <div>{rec.用法補足情報}</div>
+      <div>
+        <!-- svelte-ignore a11y-no-static-element-interactions -->
+        <span class="editable" on:click={() => doEdit(rec)}
+          >{rec.用法補足情報}</span
+        >
+        <TrashLink onClick={() => doDelete(rec)} />
+      </div>
     {/if}
   {:else}
-    <div>{rec.用法補足情報}</div>
+    <div>{rec.用法補足情報} <TrashLink onClick={() => doDelete(rec)} /></div>
   {/if}
 {/each}
+
+<style>
+  .editable {
+    cursor: pointer;
+  }
+</style>
---
+++ packages/frontend/src/lib/denshi-editor/icons/CancelLink.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import XCircle from "@/icons/XCircle.svelte";
+
+  export let onClick: () => void;
+  export let style: string = "";
+</script>
+
+<!-- svelte-ignore a11y-invalid-attribute -->
+<a href="javascript:void(0)" tabindex="-1" {style} on:click={onClick}>
+  Cancel
+  <XCircle color="currentColor" width="20" />
+</a>
+
+<style>
+  a {
+    position: relative;
+    color: gray;
+  }
+
+  a:hover {
+    color: red;
+  }
+
+  a :global(svg) {
+    position: relative;
+    top: 4.5px;
+  }
+</style>
