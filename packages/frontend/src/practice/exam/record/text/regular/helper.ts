b676d27aad7be8e3a5ae4f24fdfb4bfffe874cc9
Hangil Chang <hangil@chang.jp>
2025-06-30 17:21:25 +0900

    fixed isKensa

--- packages/frontend/src/practice/exam/record/text/regular/helper.ts
+++ packages/frontend/src/practice/exam/record/text/regular/helper.ts
@@ -1,4 +1,4 @@
 export function isKensa(patientId: number, content: string): boolean {
-  const re = new RegExp(`^${patientId}\\s+\\d+/\\d+/\\d+.+\n検査結果：`)
+  const re = new RegExp(`^0*${patientId}\\s+\\d+/\\d+/\\d+.+\n検査結果：`)
   return re.test(content);
 }
