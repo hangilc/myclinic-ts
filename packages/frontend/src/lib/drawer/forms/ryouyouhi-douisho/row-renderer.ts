8681030188da9a006573d1c69a56c89e7afac8e7
Hangil Chang <hangil@chang.jp>
2025-07-02 17:48:04 +0900

    temporary saving

---
+++ packages/frontend/src/lib/drawer/forms/ryouyouhi-douisho/row-renderer.ts
@@ -0,0 +1,95 @@
+import type { Box } from "@/lib/drawer/compiler/box";
+import type { DrawerContext } from "@/lib/drawer/compiler/context";
+import * as x from "./xsplit";
+import * as b from "@/lib/drawer/compiler/box";
+import * as c from "@/lib/drawer/compiler/compiler";
+import type { VAlign } from "@/lib/drawer/compiler/align";
+
+export interface Item {
+  width: (ctx: DrawerContext) => number | { kind: "gap"} | { kind: "at"; at: number };
+  render: (ctx: DrawerContext, box: Box) => void;
+}
+
+export interface FixedWidthItem {
+  width: (ctx: DrawerContext) => number;
+  render: (ctx: DrawerContext, box: Box) => void;
+}
+
+export function t(text: string, opt?: {
+  valign?: VAlign;
+  render?: (ctx: DrawerContext, box: Box, orig: (ctx: DrawerContext, box: Box) => void) => void;
+}): FixedWidthItem {
+  let render = (ctx: DrawerContext, box: Box) => {
+    let valign: VAlign = opt?.valign ?? "center";
+    c.drawText(ctx, text, box, "left", valign);
+  }
+  return {
+    width: (ctx) => c.textWidth(ctx, text),
+    render: extendRender(render, opt?.render),
+  }
+}
+
+export function fixed(w: number, render?: (ctx: DrawerContext, box: Box) => void
+): FixedWidthItem {
+  return {
+    width: () => w,
+    render: (ctx, box) => {
+      if( render ){
+        render(ctx, box);
+      }
+    }
+  }
+}
+
+export function gap(
+  render?: (ctx: DrawerContext, box: Box) => void,
+): Item {
+  return {
+    width: () => ({ kind: "gap"}),
+    render: render ?? (() => {}),
+  }
+}
+
+export function at(pos: number, render?: (ctx: DrawerContext, box: Box) => void): Item {
+  return {
+    width: () => ({ kind: "at", at: pos }),
+    render: (ctx, box) => {
+      if( render ){
+        render(ctx, box);
+      }
+    }
+  }
+}
+
+function extendRender(
+  orig?: (ctx: DrawerContext, box: Box) => void,
+  optRender?: (ctx: DrawerContext, box: Box, orig: (ctx: DrawerContext, box: Box) => void) => void,
+): (ctx: DrawerContext, box: Box) => void {
+  orig = orig ?? (() => {});
+  if( optRender ){
+    return (ctx, box) => {
+      optRender(ctx, box, orig);
+    }
+  } else {
+    return orig;
+  }
+}
+
+export function renderRow(ctx: DrawerContext, row: Box, ...items: Item[]) {
+  const splits = items.map(item => {
+    let w = item.width(ctx);
+    if( typeof w === "number" ){
+      return x.fixed(w);
+    } else if( w.kind === "gap" ){
+      return x.gap();
+    } else {
+      return x.at(w.at);
+    }
+  });
+  let bs = b.splitToColumns(row, x.split(...splits));
+  for(let i=0;i<items.length;i++){
+    let item = items[i];
+    let box = bs[i];
+    item.render(ctx, box);
+  }
+}
--- packages/frontend/src/lib/drawer/forms/ryouyouhi-douisho/ryouyouhi-douisho-drawer.ts
+++ packages/frontend/src/lib/drawer/forms/ryouyouhi-douisho/ryouyouhi-douisho-drawer.ts
@@ -2,16 +2,18 @@ import type { Op } from "../../compiler/op";
 import * as c from "../../compiler/compiler";
 import * as b from "../../compiler/box";
 import type { Box } from "../../compiler/box";
-import * as p from "../../compiler/composite-item";
+import * as r from "./row-renderer";
 import { A4 } from "../../compiler/paper-size";
 import { mkDrawerContext, type DrawerContext } from "../../compiler/context";
 import { ConductDrugEx } from "myclinic-model";
 import { breakNextLine } from "../../compiler/break-lines";
 import { split } from "../shohousen-2025/xsplit";
+import type { join } from "path";
+
 
 export interface RyouyouhiDouishoDrawerData {
+  patientAddress: string;
   "patient-name": string;
-  "patient-address": string;
   "birth-date": string;
   "condition-name": string;
   "onset-date": string;
@@ -25,8 +27,8 @@ export interface RyouyouhiDouishoDrawerData {
 
 export function mkRyouyouhiDouishoDrawerData(): RyouyouhiDouishoDrawerData {
   return {
+    patientAddress: "",
     "patient-name": "",
-    "patient-address": "",
     "birth-date": "",
     "condition-name": "",
     "onset-date": "",
@@ -105,6 +107,33 @@ function drawPatient(ctx: DrawerContext, box: Box, data: RyouyouhiDouishoDrawerD
   let [left, right] = b.splitToColumns(box, b.splitAt(leftColumnWidth));
   c.frameRight(ctx, left);
   c.drawTextJustified(ctx, "患者", b.modify(left, b.shrinkHoriz(3, 3)), "center");
+  let rows = b.splitToRows(right, b.evenSplitter(3));
+  let middleWidth = 37;
+  let keyMargin = 7;
+  {
+    let row = rows[0];
+    c.frameBottom(ctx, row);
+    let [keyBox, bodyBox] = b.splitToColumns(row, b.splitAt(middleWidth));
+    c.frameRight(ctx, keyBox);
+    c.drawTextJustified(ctx, "住所", b.modify(keyBox, b.shrinkHoriz(keyMargin, keyMargin)), "center");
+  }
+  {
+    let row = rows[1];
+    c.frameBottom(ctx, row);
+    let [keyBox, bodyBox] = b.splitToColumns(row, b.splitAt(middleWidth));
+    c.frameRight(ctx, keyBox);
+    c.drawTextJustified(ctx, "氏名", b.modify(keyBox, b.shrinkHoriz(keyMargin, keyMargin)), "center");
+  }
+  {
+    let row = rows[2];
+    c.frameBottom(ctx, row);
+    let [keyBox, bodyBox] = b.splitToColumns(row, b.splitAt(middleWidth));
+    c.frameRight(ctx, keyBox);
+    c.drawTextJustified(ctx, "生年月日", b.modify(keyBox, b.shrinkHoriz(keyMargin, keyMargin)), "center");
+    let area: Box = b.modify(bodyBox, b.shrinkHoriz(9, 9));
+    r.renderRow(ctx, area, r.t("明"), r.t("・"), r.t("大"), r.t("・"), r.t("昭"), r.t("・"), r.t("平"),
+      r.gap(), r.t("年"), r.gap(), r.t("月"), r.gap(), r.t("日"));
+  }
 }
 
 function drawByoumei(ctx: DrawerContext, box: Box, data: RyouyouhiDouishoDrawerData) {
@@ -117,24 +146,79 @@ function drawHatubyouDate(ctx: DrawerContext, box: Box, data: RyouyouhiDouishoDr
   let [left, right] = b.splitToColumns(box, b.splitAt(leftColumnWidth));
   c.frameRight(ctx, left);
   c.drawText(ctx, "発病年月日", left, "center", "center");
+  r.renderRow(ctx, b.modify(right, b.shrinkHoriz(15, 42)),
+    r.t("昭"), r.t("・"), r.t("平"), r.t("・"), r.t("令"), r.gap(), r.t("年"), r.gap(), r.t("月"), r.gap(), r.t("日"));
 }
 
 function drawDouiKubun(ctx: DrawerContext, box: Box, data: RyouyouhiDouishoDrawerData) {
   let [left, right] = b.splitToColumns(box, b.splitAt(leftColumnWidth));
   c.frameRight(ctx, left);
   c.drawText(ctx, "同意区分", left, "center", "center");
+  r.renderRow(ctx, b.modify(right, b.shrinkHoriz(15, 0)),
+    r.t("初回の同意"), r.t("　・　"), r.t("再　同　意"));
 }
 
 function drawShinsatuDate(ctx: DrawerContext, box: Box, data: RyouyouhiDouishoDrawerData) {
   let [left, right] = b.splitToColumns(box, b.splitAt(leftColumnWidth));
   c.frameRight(ctx, left);
   c.drawTextJustified(ctx, "診察日", b.modify(left, b.shrinkHoriz(3, 3)), "center");
+  r.renderRow(ctx, b.modify(right, b.shrinkHoriz(20, 45)),
+    r.t("令和"), r.gap(), r.t("年"), r.gap(), r.t("月"), r.gap(), r.t("日"));
 }
 
 function drawShoujou(ctx: DrawerContext, box: Box, data: RyouyouhiDouishoDrawerData) {
   let [left, right] = b.splitToColumns(box, b.splitAt(leftColumnWidth));
   c.frameRight(ctx, left);
   c.drawTextJustified(ctx, "症状", b.modify(left, b.shrinkHoriz(3, 3)), "center");
+  let rows = b.splitToRows(right, b.splitWidths(10.5, 16));
+  c.frameBottom(ctx, rows[0]);
+  c.frameBottom(ctx, rows[1]);
+  let middleWidth = 20;
+  {
+    let row = rows[0];
+    let [keyBox, bodyBox] = b.splitToColumns(row, b.splitAt(middleWidth));
+    c.frameRight(ctx, keyBox);
+    {
+      let [upper, lower] = b.splitToRows(b.modify(keyBox, b.shrinkVert(0.5, 0.5)), b.evenSplitter(2));
+      c.drawTextJustified(ctx, "筋麻痺", b.modify(upper, b.shrinkHoriz(1.5, 1.5)), "center");
+      c.drawTextJustified(ctx, "筋委縮", b.modify(lower, b.shrinkHoriz(1.5, 1.5)), "center");
+    }
+    r.renderRow(ctx, b.modify(bodyBox, b.shrinkHoriz(7, 18)),
+      r.t("躯幹"), r.gap(), r.t("・"), r.gap(),
+      r.t("右上肢"), r.gap(), r.t("・"), r.gap(),
+      r.t("左上肢"), r.gap(), r.t("・"), r.gap(),
+      r.t("右下肢"), r.gap(), r.t("・"), r.gap(),
+      r.t("左下肢"));
+  }
+  {
+    let row = rows[1];
+    let [keyBox, bodyBox] = b.splitToColumns(row, b.splitAt(middleWidth));
+    c.frameRight(ctx, keyBox);
+    c.drawText(ctx, "関節拘縮", keyBox, "center", "center");
+    let [upper, lower] = b.splitToRows(b.modify(bodyBox, b.shrinkVert(1.5, 1.5), b.shrinkHoriz(7, 0)), b.evenSplitter(2));
+    r.renderRow(ctx, upper,
+      r.t("右肩"), r.t("・"),
+      r.t("右肘"), r.t("・"),
+      r.t("右手首"), r.t("・"),
+      r.t("右股関節"), r.t("・"),
+      r.t("右膝"), r.t("・"),
+      r.t("右足首"), r.t("　　"),
+      r.t("その他"));
+    r.renderRow(ctx, lower,
+      r.t("左肩"), r.t("・"),
+      r.t("左肘"), r.t("・"),
+      r.t("左手首"), r.t("・"),
+      r.t("左股関節"), r.t("・"),
+      r.t("左膝"), r.t("・"),
+      r.t("左足首"), r.t("　"),
+      r.t("（　　　　　　）"));
+  }
+  {
+        let row = rows[2];
+    let [keyBox, bodyBox] = b.splitToColumns(row, b.splitAt(middleWidth));
+    c.frameRight(ctx, keyBox);
+    c.drawTextJustified(ctx, "その他", b.modify(keyBox, b.shrinkHoriz(1.5, 1.5)), "center");
+  }
 }
 
 function drawSejutu(ctx: DrawerContext, box: Box, data: RyouyouhiDouishoDrawerData) {
