10f99ab95afaa04e8f0c44688ee46dcd4813f938
Hangil Chang <hangil@chang.jp>
2025-07-08 08:41:13 +0900

    temporary saving

--- packages/frontend/src/lib/denshi-editor/denshi-tmpl.ts
+++ packages/frontend/src/lib/denshi-editor/denshi-tmpl.ts
@@ -22,8 +22,71 @@ import type {
 import type { 薬品コード種別 } from "@/lib/denshi-shohou/denshi-shohou";
 import type { Shohou } from "@/lib/parse-shohou";
 import { DateWrapper } from "myclinic-util";
+import { initPrescInfoDataFromVisitId } from "../denshi-shohou/visit-shohou";
 
 let serialId = 1;
+
+export class DenshiWrapper {
+  data: PrescInfoData;
+  備考レコード: 備考レコードWrapper[];
+  提供診療情報レコード: 提供診療情報レコードWrapper[];
+  検査値データ等レコード: 検査値データ等レコードWrapper[];
+
+  constructor(data: PrescInfoData) {
+    this.data = data;
+    this.備考レコード = [];
+    this.提供診療情報レコード = [];
+    this.検査値データ等レコード = [];
+  }
+
+  static async fromShohou(shohou: Shohou, visitId: number): Promise<DenshiWrapper> {
+    const prescInfo = await initPrescInfoDataFromVisitId(visitId);
+    prescInfo.使用期限年月日 = get使用期限年月日FromShohou(shohou);
+    const self = new DenshiWrapper(prescInfo);
+    self.備考レコード = get備考レコードFromShohou(shohou).map(r => new 備考レコードWrapper(r));
+    self.提供診療情報レコード = get提供診療情報レコードFromShohou(shohou).map(r => new 提供診療情報レコードWrapper(r));
+    self.検査値データ等レコード = get検査値データ等レコードFromShohou(shohou).map(r => new 検査値データ等レコードWrapper(r));
+    return self;
+  }
+
+  toPrescInfoData(): PrescInfoData {
+    return Object.assign({}, this.data, {
+      備考レコード: this.備考レコード.map(r => r.data),
+      提供診療情報レコード: this.提供診療情報レコード.map(r => r.data),
+      検査値データ等レコード: this.検査値データ等レコード.map(r => r.data),
+    })
+  }
+}
+
+export class 備考レコードWrapper {
+  id: number;
+  data: 備考レコード;
+
+  constructor(data: 備考レコード) {
+    this.id = serialId++;
+    this.data = data;
+  }
+}
+
+export class 提供診療情報レコードWrapper {
+  id: number;
+  data: 提供診療情報レコード;
+
+  constructor(data: 提供診療情報レコード) {
+    this.id = serialId++;
+    this.data = data;
+  }
+}
+
+export class 検査値データ等レコードWrapper {
+  id: number;
+  data: 検査値データ等レコード;
+
+  constructor(data: 検査値データ等レコード) {
+    this.id = serialId++;
+    this.data = data;
+  }
+}
 
 export interface TopTmpl {
   orig: {
@@ -122,7 +185,51 @@ export function get検査値データ等レコードFromShohou(shohou: Shohou): 
   return [];
 }
 
+export class RP剤情報Wrapper {
+  id: number;
+  data: RP剤情報;
+  用法補足レコード: 用法補足レコードWrapper[];
+  薬品情報グループ: 薬品情報Wrapper[];
 
+  constructor(data: RP剤情報) {
+    this.id = serialId++;
+    this.data = data;
+    this.用法補足レコード = [];
+    this.薬品情報グループ = [];
+  }
+}
+
+export class 用法補足レコードWrapper {
+  id: number;
+  data: 用法補足レコード;
+
+  constructor(data: 用法補足レコード) {
+    this.id = serialId++;
+    this.data = data;
+  }
+}
+
+export class 薬品情報Wrapper {
+  id: number;
+  data: 薬品情報;
+  薬品補足レコード: 薬品補足レコードWrapper[];
+
+  constructor(data: 薬品情報) {
+    this.id = serialId++;
+    this.data = data;
+    this.薬品補足レコード = [];
+  }
+}
+
+export class 薬品補足レコードWrapper {
+  id: number;
+  data: 薬品補足レコード;
+
+  constructor(data: 薬品補足レコード) {
+    this.id = serialId++;
+    this.data = data;
+  }
+}
 
 // export interface RP剤情報Tmpl {
 //   id: number;
