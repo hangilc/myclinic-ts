72758e9df8c63eb22602f04cf6dfede86f0a9bcb
Hangil Chang <hangil@chang.jp>
2025-07-22 18:06:04 +0900

    adding presc search

---
+++ packages/frontend/src/lib/denshi-editor/components/presc-search/DenshiShohouItem.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import type { PrescInfoData } from "@/lib/denshi-shohou/presc-info";
+  import { TextMemoWrapper } from "@/lib/text-memo";
+  import type { Text, Visit } from "myclinic-model";
+  import { drugRep } from "../../helper";
+
+  export let text: Text;
+  export let visit: Visit;
+
+  const data: PrescInfoData = TextMemoWrapper.fromText(text).probeShohouMemo()!.shohou;
+</script>
+
+<div class="top"></div>
+{#each data.RP剤情報グループ as group}
+  {#each group.薬品情報グループ as drug}
+    <div>{drugRep(drug)}</div>
+  {/each}
+{/each}
+
+<style>
+  .top {
+    font-size: 14px;
+  }
+</style>
--- packages/frontend/src/lib/denshi-editor/components/presc-search/PrescSearchList.svelte
+++ packages/frontend/src/lib/denshi-editor/components/presc-search/PrescSearchList.svelte
@@ -2,13 +2,24 @@
   import type { Text, Visit } from "myclinic-model";
   import api from "@/lib/api";
   import PaperShohouItem from "./PaperShohouItem.svelte";
+  import { TextMemoWrapper } from "@/lib/text-memo";
+  import DenshiShohouItem from "./DenshiShohouItem.svelte";
 
   export let list: [Text, Visit][] = [];
 
+  function isDenshi(text: Text): boolean {
+    return TextMemoWrapper.fromText(text).getMemoKind() === "shohou";
+  }
+
+ $:  console.log("list", list);
 </script>
 
 <div>
   {#each list as [text, visit] (text.textId)}
+    {#if isDenshi(text)}
+    <DenshiShohouItem {text} {visit} />
+    {:else}
     <PaperShohouItem {text} {visit} />
+    {/if}
   {/each}
 </div>
