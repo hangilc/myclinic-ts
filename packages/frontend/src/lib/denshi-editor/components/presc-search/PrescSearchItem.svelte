d64b3982f590da8cc4f5a3eb5ebda93416ff748f
Hangil Chang <hangil@chang.jp>
2025-07-29 18:04:28 +0900

    adding PrescSearchItem

--- packages/frontend/src/lib/denshi-editor/components/presc-search/PrescSearchItem.svelte
+++ packages/frontend/src/lib/denshi-editor/components/presc-search/PrescSearchItem.svelte
@@ -5,6 +5,8 @@
 
   export let group: RP剤情報;
   export let selectedName: string | undefined;
+  let selected: boolean[] | undefined = undefined;
+  let isEditing = false;
 
   function rep(drug: 薬品情報): string {
     let html = drugRep(drug);
@@ -18,15 +20,38 @@
     }
   }
 
+  function initSelected() {
+    if( selected === undefined ){
+      selected = [];
+      for(let i=0;i<group.薬品情報グループ.length;i++){
+        selected.push(false);
+      }
+    }
+  }
 
+  function onDrugClick(index: number) {
+    initSelected();
+    if( selected === undefined ){
+      throw new Error("cannot happen");
+    }
+    selected[index] = true;
+    isEditing = true;
+  }
 </script>
 
 <div class="drugs">
-  {#each group.薬品情報グループ as drug}
-    <div>{@html rep(drug)}</div>
+  {#each group.薬品情報グループ as drug, index}
+    <!-- svelte-ignore a11y-no-static-element-interactions -->
+    <div on:click={() => onDrugClick(index)}>{@html rep(drug)}</div>
   {/each}
 </div>
-<div>
+<div class="usage">
   {group.用法レコード.用法名称}
   {daysTimesDisp(group)}
 </div>
+
+<style>
+  .drugs, .usage {
+    cursor: pointer;
+  }
+</style>
