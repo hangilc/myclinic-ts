22787ee78a71531b6ce8e82849a75bef439e6f8a
Hangil Chang <hangil@chang.jp>
2025-08-05 09:32:23 +0900

    temporary saving

--- packages/frontend/src/lib/denshi-editor/icons/PencilSquareLink.svelte
+++ packages/frontend/src/lib/denshi-editor/icons/PencilSquareLink.svelte
@@ -6,3 +6,10 @@
 <a href="javascript:void(0)">
   <PencilSquare />
 </a>
+
+<style>
+  a {
+    position: relative;
+    top: var(--pencil-square-icon-top, 2px);
+  }
+</style>
--- packages/frontend/src/practice/presc-example/presc-example-data.ts
+++ packages/frontend/src/practice/presc-example/presc-example-data.ts
@@ -3,12 +3,17 @@ import type { PrescExample } from "@/lib/presc-example";
 export interface PrescExampleData {
   data: PrescExample;
   id: number;
+  isEditingComment: boolean;
+  commentInput: string;
 };
 
 let serialId = 1;
 
 export function createPrescExampleData(data: PrescExample): PrescExampleData {
   return {
-    data, id: serialId++,
+    data, 
+    id: serialId++,
+    isEditingComment: false,
+    commentInput: "",
   }
 }
