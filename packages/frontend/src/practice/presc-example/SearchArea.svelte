0a0bbb03233179f6d285dee5056ca7a9948edf21
Hangil Chang <hangil@chang.jp>
2025-08-06 14:09:21 +0900

    added presc example

--- packages/frontend/src/practice/presc-example/SearchArea.svelte
+++ packages/frontend/src/practice/presc-example/SearchArea.svelte
@@ -1,26 +1,48 @@
 <script lang="ts">
   import Link from "../ui/Link.svelte";
-  import { cache } from "@/lib/cache";
   import {
-    createPrescExampleData,
     type PrescExampleData,
   } from "./presc-example-data";
   import DrugGroupRep from "./components/DrugGroupRep.svelte";
-  import type { RP剤情報 } from "@/lib/denshi-shohou/presc-info";
   import CommentField from "./components/CommentField.svelte";
 
   export let onSelect: (data: PrescExampleData, drugIndex: number) => void;
   export let list: PrescExampleData[] = [];
   let selected: PrescExampleData[] = [];
+  let searchText = "";
+
+  $: selected = updateSelected(list);
+
+  function updateSelected(list: PrescExampleData[]): PrescExampleData[] {
+    if (searchText === "") {
+      return list;
+    } else {
+      return list.filter((e) => {
+        return e.data.薬品情報グループ.some((d) =>
+          d.薬品レコード.薬品名称.includes(searchText),
+        );
+      });
+    }
+  }
 
   function doShowAll() {
     selected = list;
   }
+
+  function doSearch() {
+    selected = updateSelected(list);
+  }
 </script>
 
-<Link onClick={doShowAll}>全例</Link>
+<div class="search-form">
+  <form on:submit|preventDefault={doSearch}>
+    <input type="text" bind:value={searchText} />
+    <button type="submit">検索</button>
+  </form>
+  <Link onClick={doShowAll}>全例</Link>
+</div>
 <div class="list">
-  {#each list as data (data.id)}
+  {#each selected as data (data.id)}
     <div class="group">
       <div>
         <DrugGroupRep
@@ -36,6 +58,14 @@
 </div>
 
 <style>
+  .search-form {
+    margin-bottom: 10px;
+  }
+
+  .search-form form {
+    display: inline-block;
+  }
+
   .list {
     max-height: 500px;
     overflow-y: auto;
@@ -45,6 +75,11 @@
     margin: 10px 10px 10px 0;
     border: 1px solid gray;
     padding: 6px;
+    background-color: #f8f8f8;
+  }
+
+  .group:nth-child(1) {
+    margin-top: 0;
   }
 
   .comment {
