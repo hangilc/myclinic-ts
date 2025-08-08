5de279e6ee3fe02513e7cb6fbd567641c5718828
Hangil Chang <hangil@chang.jp>
2025-08-08 13:50:19 +0900

    temporary saving

---
+++ packages/frontend/src/lib/ui/Confirm.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import Dialog2 from "../Dialog2.svelte";
+
+  export let destroy: () => void;
+  export let title = "確認";
+</script>
+
+<Dialog2 title={title} {destroy}>
+
+</Dialog2>
