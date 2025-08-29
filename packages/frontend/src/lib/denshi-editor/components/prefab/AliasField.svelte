93ad364073c4af2b975ca67518d5dfc651b14866
Hangil Chang <hangil@chang.jp>
2025-08-29 13:42:44 +0900

    adding AliasField

--- packages/frontend/src/lib/denshi-editor/components/prefab/AliasField.svelte
+++ packages/frontend/src/lib/denshi-editor/components/prefab/AliasField.svelte
@@ -1,14 +1,11 @@
 <script lang="ts">
-  import Field from "./workarea/Field.svelte";
-  import FieldTitle from "./workarea/FieldTitle.svelte";
-  import FieldForm from "./workarea/FieldForm.svelte";
+  import Field from "../workarea/Field.svelte";
+  import FieldTitle from "../workarea/FieldTitle.svelte";
+  import FieldForm from "../workarea/FieldForm.svelte";
   import { tick } from "svelte";
-  import SubmitLink from "../icons/SubmitLink.svelte";
-  import CancelLink from "../icons/CancelLink.svelte";
-  import type { 薬品情報Edit } from "../denshi-edit";
-  import { toHankaku } from "@/lib/zenkaku";
-  import type { 薬品情報 } from "@/lib/denshi-shohou/presc-info";
-
+  import SubmitLink from "../../icons/SubmitLink.svelte";
+  import CancelLink from "../../icons/CancelLink.svelte";
+  
   export let alias: string[];
   export let onFieldChange: () => void;
   let inputText: string = "";
@@ -23,7 +20,7 @@
   };
 
   function updateInputText(value: string[]) {
-    inputText = value.join("|");
+    inputText = value.join(" ");
   }
 
   function doRepClick() {
@@ -41,16 +38,24 @@
     isEditing = false;
     alias = orig;
   }
+
+  function rep(alias: string[]): string {
+    if( alias.length === 0 ){
+      return "（未設定）";
+    } else {
+      return alias.join(" ");
+    }
+  }
 </script>
 
   <Field>
-    <FieldTitle>薬品分量</FieldTitle>
+    <FieldTitle>薬品別名</FieldTitle>
     <FieldForm>
       {#if !isEditing}
         <!-- svelte-ignore a11y-no-static-element-interactions -->
         <!-- svelte-ignore a11y-click-events-have-key-events -->
         <div class="rep" on:click={doRepClick}>
-          {drug.薬品レコード.分量 || "（未設定）"}{drug.薬品レコード.単位名}
+          {rep(alias)}
         </div>
       {:else}
         <form on:submit|preventDefault={doEnter} class="with-icons">
@@ -59,7 +64,7 @@
             bind:value={inputText}
             bind:this={inputElement}
             class="input"
-          />{drug.薬品レコード.単位名}
+          />
           <SubmitLink onClick={doEnter} />
           <CancelLink onClick={doCancel} />
         </form>
