da6f1e2b73b347f2341922369c4ef127ff95f2d4
Hangil Chang <hangil@chang.jp>
2025-09-09 13:22:05 +0900

    added batch delete

--- packages/frontend/src/lib/denshi-editor/components/Batch.svelte
+++ packages/frontend/src/lib/denshi-editor/components/Batch.svelte
@@ -2,7 +2,7 @@
   import Title from "./workarea/Title.svelte";
   import Workarea from "./workarea/Workarea.svelte";
   import Commands from "./workarea/Commands.svelte";
-  import type { RP剤情報Edit } from "../denshi-edit";
+  import { RP剤情報Edit } from "../denshi-edit";
   import SelectDrugForm from "./SelectDrugForm.svelte";
   import { toHankaku } from "@/lib/zenkaku";
   import Link from "@/practice/ui/Link.svelte";
@@ -10,8 +10,15 @@
   export let groups: RP剤情報Edit[];
   export let onCancel: () => void;
   export let onEnter: (groups: RP剤情報Edit[]) => void;
-  
-  let working: RP剤情報Edit[] = groups.map(g => g.clone());
+
+  let working: RP剤情報Edit[] = groups.map((g) => g.clone());
+
+  function clearSelection() {
+    working.forEach(group => {
+      group.isSelected = false;
+      group.薬品情報グループ.forEach(drug => drug.isSelected = false)
+    });
+  }
 
   function doCancel() {
     onCancel();
@@ -22,38 +29,39 @@
   }
 
   function doChangeDays() {
-    const gs: RP剤情報Edit[] = working.filter(group => {
-      return group.isSelected && group.剤形レコード.剤形区分 === "内服"
+    const gs: RP剤情報Edit[] = working.filter((group) => {
+      return group.isSelected && group.剤形レコード.剤形区分 === "内服";
     });
-    if( gs.length === 0 ){
+    if (gs.length === 0) {
       return;
     }
     let input = prompt("新しい日数");
-    if( input == null ){
+    if (input == null) {
       return;
     }
     input = toHankaku(input);
     const newDays = parseInt(input);
-    if( isNaN(newDays) || newDays <= 0 ){
-      alert("日数が正の整数でありません。")
-      return 
+    if (isNaN(newDays) || newDays <= 0) {
+      alert("日数が正の整数でありません。");
+      return;
     }
-    gs.forEach(group => {
+    gs.forEach((group) => {
       group.剤形レコード.調剤数量 = newDays;
-    })
+    });
     working = working;
   }
 
   function doDelete() {
-    working.filter(group => group.isSelected)
-      .forEach(group => {
-        if( group.薬品情報グループ.length === 1){
-          group.薬品情報グループ = [];
-        } else {
-          group.薬品情報グループ = group.薬品情報グループ.filter(drug => !drug.isSelected);
-        }
+    working
+      .filter((group) => group.isSelected)
+      .forEach((group) => {
+        group.薬品情報グループ = group.薬品情報グループ.filter(
+          (drug) => !drug.isSelected,
+        );
       });
-    working = working.filter(group => group.薬品情報グループ.length > 0);
+    working = working.filter((group) => group.薬品情報グループ.length > 0);
+    clearSelection();
+    working = working;
   }
 </script>
 
--- packages/frontend/src/lib/denshi-editor/components/SelectDrugForm.svelte
+++ packages/frontend/src/lib/denshi-editor/components/SelectDrugForm.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { toZenkaku } from "@/lib/zenkaku";
-  import type { RP剤情報Edit } from "../denshi-edit";
+  import type { RP剤情報Edit, 薬品情報Edit } from "../denshi-edit";
   import { drugRep } from "../helper";
   import { daysTimesDisp } from "@/lib/denshi-shohou/disp/disp-util";
   import Link from "@/practice/ui/Link.svelte";
@@ -22,14 +22,36 @@
     });
     groups = groups;
   }
+
+  function doGroupChange(group: RP剤情報Edit) {
+    if( group.isSelected ){
+      group.薬品情報グループ.forEach(drug => drug.isSelected = true );
+    } else {
+      group.薬品情報グループ.forEach(drug => drug.isSelected = false );
+    }
+    groups = groups;
+  }
+
+  function doDrugChange(group: RP剤情報Edit, drug: 薬品情報Edit) {
+    if( drug.isSelected ){
+      group.isSelected = true;
+    } else {
+      if( group.薬品情報グループ.every(drug => !drug.isSelected) ){
+        group.isSelected = false;
+      }
+    }
+    groups = groups;
+  }
 </script>
 
 <div class="groups">
   {#each groups as group, index (group.id)}
     <div>
-      <input type="checkbox" bind:checked={group.isSelected} />{toZenkaku(
-        `${index + 1})`,
-      )}
+      <input
+        type="checkbox"
+        bind:checked={group.isSelected}
+        on:change={() => doGroupChange(group)}
+      />{toZenkaku(`${index + 1})`)}
     </div>
     <div class="drug-list">
       {#each group.薬品情報グループ as drug (drug.id)}
@@ -38,6 +60,7 @@
             <input
               type="checkbox"
               bind:checked={drug.isSelected}
+              on:change={() => doDrugChange(group, drug)}
             />{/if}{@html drugRep(drug)}
         </div>
       {/each}
