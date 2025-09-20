ed3c06357fc3531a9ff572ec15529a9506984e83
Hangil Chang <hangil@chang.jp>
2025-09-20 14:20:50 +0900

    modified RcptCheck.svelte

--- packages/frontend/src/practice/RcptCheck.svelte
+++ packages/frontend/src/practice/RcptCheck.svelte
@@ -4,14 +4,17 @@
   import { pad } from "@/lib/pad";
   import { hasHokenOrKouhi, loadVisits } from "@/lib/rezept-adapter";
   import type { Patient, Visit, VisitEx } from "myclinic-model";
-  import { checkForRcpt, type CheckError, type CheckResult } from "./rcpt-check/check";
+  import {
+    checkForRcpt,
+    type CheckErrorWithFixers,
+    type CheckResult,
+  } from "./rcpt-check/check";
 
   export let isVisible = false;
   let shinryouYearMonth: string = defaultShinryouYearMonth();
   let patientTotal = 0;
   let current = 0;
-  // let serial = 1;
-  let errors: { patient: Patient, checkErrors: CheckError[] }[] = [];
+  let errors: { patient: Patient; checkErrors: CheckErrorWithFixers[] }[] = [];
 
   function defaultShinryouYearMonth(): string {
     let now = new Date();
@@ -41,46 +44,60 @@
     patientTotal = visitsList.length;
     errors = [];
     // serial = 1;
-    for(const visits of visitsList) {
-      if( visits.length === 0 ){
+    for (const visits of visitsList) {
+      if (visits.length === 0) {
         continue;
       }
       current += 1;
-      let patientVisits: VisitEx[] = await Promise.all(visits.map(async visit => await api.getVisitEx(visit.visitId)));
+      let patientVisits: VisitEx[] = await Promise.all(
+        visits.map(async (visit) => await api.getVisitEx(visit.visitId)),
+      );
       const errs = await checkForRcpt(patientVisits);
-      if( errs !== "ok" && errs !== "no-visit" ){
-        errors = [...errors, { patient: patientVisits[0].patient, checkErrors: errs}];
-        // const patient = patientVisits[0].patient;
+      if (errs !== "ok" && errs !== "no-visit") {
+        console.log("errs", errs);
+        errors = [
+          ...errors,
+          { patient: patientVisits[0].patient, checkErrors: errs },
+        ];
       }
     }
   }
 
   async function recheck(patientId: number): Promise<CheckResult> {
-    const visitIds = await api.listVisitIdByPatientAndMonth(patientId, getYear(), getMonth());
-    let visits = await Promise.all(visitIds.map(async visitId => await api.getVisitEx(visitId)));
-    visits = visits.filter(visit => hasHokenOrKouhi(visit.asVisit));
+    const visitIds = await api.listVisitIdByPatientAndMonth(
+      patientId,
+      getYear(),
+      getMonth(),
+    );
+    let visits = await Promise.all(
+      visitIds.map(async (visitId) => await api.getVisitEx(visitId)),
+    );
+    visits = visits.filter((visit) => hasHokenOrKouhi(visit.asVisit));
     return checkForRcpt(visits);
   }
 
-  async function doFix(fix: (() => Promise<boolean>) | undefined, patientId: number) {
-    if( !fix ){
+  async function doFix(
+    fix: (() => Promise<boolean>) | undefined,
+    patientId: number,
+  ) {
+    if (!fix) {
       return;
     }
     const ok = await fix();
-    if( ok ){
+    if (ok) {
       const errs = await recheck(patientId);
-      if( errs === "ok" ){
-        errors = errors.filter(e => e.patient.patientId !== patientId);
-      } else if( errs === "no-visit" ){
+      if (errs === "ok") {
+        errors = errors.filter((e) => e.patient.patientId !== patientId);
+      } else if (errs === "no-visit") {
         alert("No visits");
       } else {
-        errors = errors.map(e => {
-          if( e.patient.patientId === patientId ){
+        errors = errors.map((e) => {
+          if (e.patient.patientId === patientId) {
             return { patient: e.patient, checkErrors: errs };
           } else {
             return e;
           }
-        })
+        });
       }
     }
   }
@@ -106,10 +123,14 @@
         {#each error.checkErrors as ce}
           <div class="error-code-wrapper">
             <div class="error-code">{ce.code}</div>
-            {#if ce.hint}<div class="hint-wrapper">| {ce.hint}</div>{/if}
-            {#if ce.fix}
-            <div class="fix-wrapper"><button on:click={() => doFix(ce.fix, error.patient.patientId)}>Fix</button></div>
-            {/if}
+            {#each ce.fixers as fixer}
+              <div class="hint-wrapper">| {fixer.hint}</div>
+              <div class="fix-wrapper">
+                <button on:click={() => doFix(fixer.fix, error.patient.patientId)}
+                  >Fix</button
+                >
+              </div>
+            {/each}
           </div>
         {/each}
       </div>
@@ -129,7 +150,7 @@
     margin-bottom: 6px;
   }
 
-  .error-code-wrapper + .error-code-wrapper{
+  .error-code-wrapper + .error-code-wrapper {
     margin-top: 6px;
   }
 
