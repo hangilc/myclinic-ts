9a0df7ab88fc23cdf2676372016408c25eacb0a7
Hangil Chang <hangil@chang.jp>
2025-09-20 14:01:38 +0900

    modified RcptChecker type

--- packages/frontend/src/practice/rcpt-check/check.ts
+++ packages/frontend/src/practice/rcpt-check/check.ts
@@ -17,36 +17,56 @@ import { checkNonEmptyMeisai } from "./checkers/check-non-empty-meisai";
 import { checkIryouJohoShutokuShoshinSaishin } from "./checkers/check-iryou-joho-shutoku-shosin-saishin";
 import { checkGairaiSeikactsuConflict } from "./checkers/check-gairai-seikatsu-conflict";
 
-export type Fixer = () => Promise<boolean>;
-export type CheckError = { code: string, fix?: Fixer, hint?: string };
-export type CheckResult = "ok" | "no-visit" | CheckError[];
-export type RcptChecker = (visits: VisitEx[]) => Promise<CheckError[]>;
+export type CheckError = {
+  code: string;
+  fix?: () => Promise<boolean>;
+  hint?: string;
+};
+export interface Fixer {
+  fix: () => Promise<boolean>;
+  hint: string;
+}
+export type CheckErrorWithFixers = { code: string; fixers: Fixer[] };
+export type CheckResult = "ok" | "no-visit" | CheckErrorWithFixers[];
+export type RcptChecker = (visits: VisitEx[]) => Promise<CheckErrorWithFixers[]>;
+
+function convSingle(
+  checker: (visits: VisitEx[]) => Promise<CheckError[]>
+): RcptChecker {
+  return async (visits) => {
+    let err = await checker(visits);
+    return err.map((e) => ({
+      code: e.code,
+      fixers: e.fix ? [{ fix: e.fix, hint: e.hint ?? "" }] : [],
+    }));
+  };
+}
 
 const checkers: RcptChecker[] = [
-  checkGenericOne,
-  checkXpSatsuei,
-  checkXpShindan,
-  checkOnshi,
-  checkMainDisease,
-  checkGlucoseSelfMeasuring,
-  checkDuplicate,
-  checkHandanryou,
-  checkShoshinSaishin,
-  checkRyouyouDouisho,
-  checkIryoujouhouShutokuKasan,
-  checkShoshinByoumei,
-  checkSaishinByoumei,
-  checkByoumei,
-  checkNonEmptyMeisai,
-  checkIryouJohoShutokuShoshinSaishin,
-  checkGairaiSeikactsuConflict,
+  convSingle(checkGenericOne),
+  convSingle(checkXpSatsuei),
+  convSingle(checkXpShindan),
+  convSingle(checkOnshi),
+  convSingle(checkMainDisease),
+  convSingle(checkGlucoseSelfMeasuring),
+  convSingle(checkDuplicate),
+  convSingle(checkHandanryou),
+  convSingle(checkShoshinSaishin),
+  convSingle(checkRyouyouDouisho),
+  convSingle(checkIryoujouhouShutokuKasan),
+  convSingle(checkShoshinByoumei),
+  convSingle(checkSaishinByoumei),
+  convSingle(checkByoumei),
+  convSingle(checkNonEmptyMeisai),
+  convSingle(checkIryouJohoShutokuShoshinSaishin),
+  convSingle(checkGairaiSeikactsuConflict),
 ];
 
 export async function checkForRcpt(visits: VisitEx[]): Promise<CheckResult> {
   if (visits.length === 0) {
     return "no-visit";
   }
-  const errors: CheckError[] = [];
+  const errors: CheckErrorWithFixers[] = [];
   for (let checker of checkers) {
     const errs = await checker(visits);
     errors.push(...errs);
