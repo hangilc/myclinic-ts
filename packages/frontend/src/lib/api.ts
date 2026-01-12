0828b450b115458d415b86d8ec01ead74ea6b1c1
Hangil Chang <hangil@chang.jp>
2026-01-12 17:27:33 +0900

    added .env.development

--- packages/frontend/src/lib/api.ts
+++ packages/frontend/src/lib/api.ts
@@ -75,7 +75,7 @@ export function getBackend(): string {
     console.log("getBackend", result);
     return result;
   } else {
-    return "http://localhost:8080";
+    return import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
   }
 }
 
